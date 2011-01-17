a7ec635853d7d2db440ce772b6d09c9338ede30d
Stephen Billard <stephen@sbillard.org>
2011-01-17 19:46:25 +0000

    Missed files

---
+++ themes/garland/main.php
@@ -0,0 +1,65 @@
+<?php
+if (!defined('WEBPATH')) die();
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<?php zp_apply_filter('theme_head'); ?>
+<title><?php printGalleryTitle(); ?></title>
+<link rel="stylesheet" href="<?php echo $_zp_themeroot ?>/zen.css"
+	type="text/css" />
+<?php printRSSHeaderLink('Gallery',gettext('Gallery RSS')); ?>
+</head>
+<body class="sidebars">
+<?php zp_apply_filter('theme_body_open'); ?>
+<div id="navigation"></div>
+<div id="wrapper">
+	<div id="container">
+		<div id="header">
+			<div id="logo-floater">
+				<div>
+					<h1 class="title"><a href="<?php echo html_encode(getGalleryIndexURL(false)); ?>" title="<?php echo gettext('Gallery Index'); ?>"><?php echo html_encode(getGalleryTitle()); ?></a></h1>
+				</div>
+			</div>
+		</div><!-- header -->
+	<div class="sidebar">
+		 <div id="leftsidebar">
+		<?php include("sidebar.php"); ?>
+		</div>
+	</div>
+	<div id="center">
+		<div id="squeeze">
+			<div class="right-corner">
+				<div class="left-corner"><!-- begin content -->
+					<div class="main section" id="main">
+						<h3 id="gallerytitle"><?php echo getGalleryTitle(); ?></h3>
+						<?php commonNewsLoop(false); ?>
+						<div style="clear: both;"></div>
+						<?php footer(); ?>
+					</div><!-- main -->
+				<span class="clear"></span>
+				</div><!-- left corner -->
+			</div><!-- right corner -->
+		</div><!-- squeeze -->
+	</div><!-- center -->
+	<div class="sidebar">
+		<div id="rightsidebar">
+			<?php
+			if (function_exists('printLatestImages')) {
+				?>
+				<h2><?php  echo gettext('Latest Images'); ?></h2>
+				<?php
+				printLatestImages(7);
+			}
+			?>
+		</div><!-- right sidebar -->
+	</div><!-- sidebar -->
+	<span class="clear"></span>
+	</div><!-- container -->
+</div><!-- wrapper -->
+<?php
+printAdminToolbox();
+zp_apply_filter('theme_body_close');
+?>
+</body>
+</html>
---
+++ zp-core/zp-extensions/GoogleMap/m.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * google_maps -- map server
+ *
+ * @package plugins
+ */
+
+// force UTF-8 Ø
+
+define ('OFFSET_PATH', 4);
+require_once(dirname(dirname(dirname(__FILE__))).'/functions.php');
+require_once(dirname(dirname(__FILE__)).'/GoogleMap.php');
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s').' GMT');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/2002/REC-xhtml1-20020801/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<?php googlemap_js(); ?>
+</head>
+<body>
+<?php
+$map = unserialize($_GET['mapobject']);
+echo $map->getMapJS();
+echo $map->printOnLoad();
+echo $map->printMap();
+?>
+</body>
+</html>
