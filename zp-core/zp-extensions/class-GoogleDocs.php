b82f3c44e84ffe025f73624a204280cc2d2f9e36
Stephen Billard <stephen@sbillard.org>
2011-01-18 00:40:00 +0000

    Two new non-image objects:class-GoogleDocs for handling png and pps files using googleDocs and class-AnyFile for arbitrary file suffixes. Both are descendants of class-TextObject which no longer handles the googledocs suffixes

--- zp-core/zp-extensions/class-GoogleDocs.php
+++ zp-core/zp-extensions/class-GoogleDocs.php
@@ -1,58 +1,8 @@
 <?php
 /**
- * Plugin handler for "text" files
- * Text type files can be displayed in place of an image in themes
- *
- * supports files of the following types:
- * 	.txt
- * 	.htm
- * 	.html
- * 		The contents of these files are "dumpped" into a SPAN sized to a 24x36 ratioed box based on your
- * 		theme	"image size" option. This has a class of "textobject" so it can be styled.
- *
- * What this plugin really is for is to serve as a model of how a plugin can be made to handle file types
- * that zenphoto does not handle natively.
- *
- * Some key points to note:
- * 1. The naming convention for these plugins is class-«handler class».php.
- * 2. The statement setting the plugin_is_filter variable must be near the front of the file. This is important
- * as it is the indicator to the Zenphoto plugin loader to load the script at the same point that other
- * object modules are loaded.
- * 3. These objects are extension to the zenphoto "Image" class. This means they have all the properties of
- * an image plus whatever you add. Of course you will need to override some of the image class functions to
- * implement the functionality of your new class.
- * 4. There is one VERY IMPORTANT method that you must provide which is not part of the "Image" base class. That
- * getBody() method. This method is called by template-functions.php in place of where it would normally put a URL
- * to the image to show. This method must do everything needed to cause your image object to be viewable by the
- * browser.
- *
- * So, briefly, the first four lines of code below are the standard plugin interface to Admin. There is one small
- * wrinkle you might notice--the code for 'plugin_description' includes a test which sets the variable $disable.
- * As you might expect, there were some changes needed to zenphoto in order to get this concept to work.  $disable
- * is set to true if the revision of zenphoto that is attempting to load this plugin is lower than the one where the
- * implementation first appeared. The interface variable 'plugin_disable' is set to this value telling Admin not to
- * allow enabling of the plugin if the release level is too low.
- *
- * The line that follows insures that the plugin will not load when it should be disabled--just in case.
- *
- * Then there is a call on addPlginType(«file extension», «Object Name»); This function registers the plugin as the
- * handler for files with the specified extension. If the plugin can handle more than one file extension, make a call
- * to the registration function for each extension that it handles.
- *
- * The rest is the object class for handling these files.
- *
- * The code of the object instantiation function is mostly required. Plugin "images" follow the lead of videos in that
- * if there is a real image file with the same name save the suffix, it will be considered the thumb image of the object.
- * This image is fetched by the call on checkObjectsThumb(). There is also code in the getThumb() method to deal with
- * this property.
- *
- * Since text files have no natural height and width, we set them based on the image size option. This happens after the call
- * PersistentObject(). The rest of the code there sets up the default title.
- *
- * getThumb() is responsible for generating the thumbnail image for the object. As above, if there is a similar named real
- * image, it will be used. Otherwise [for this object implementation] we will use a thumbnail image provided with the plugin.
- * The particular form of the file name used when there is no thumb stand-in image allows zenphoto to choose an image in the
- * plugin folder.
+ * Plugin handler for: .pdf, .pps documents
+ * These are displayed Google Docs viewer. The item is displayed in an iFrame sized as above. Of course, your site
+ * must be accessable by Google and your viewer must have a google account for this to work.
  *
  * @author Stephen Billard (sbillard)
  * @package plugins
@@ -60,20 +10,19 @@
  */
 
 $plugin_is_filter = 9|CLASS_PLUGIN;
-$plugin_description = gettext('Provides a means for showing text type documents (.txt, .html, .htm) where zenphoto would normally display images. For documentation, see the script file.');
+$plugin_description = gettext('Provides a means for showing .pdf, .pps documents using googledocs for the document rendering');
 $plugin_author = "Stephen Billard (sbillard)";
-$plugin_version = '1.4.0';
+$plugin_version = '1.4.1';
 
-addPluginType('htm', 'TextObject');
-addPluginType('html', 'TextObject');
-addPluginType('txt', 'TextObject');
-$option_interface = 'textObject_Options';
+addPluginType('pdf', 'GoogleDocs');
+addPluginType('pps', 'GoogleDocs');
+$option_interface = 'GoogleDocs_Options';
 
 /**
  * Option class for textobjects objects
  *
  */
-class TextObject_Options {
+class GoogleDocs_Options {
 
 	/**
 	 * Standard option interface
@@ -81,22 +30,24 @@ class TextObject_Options {
 	 * @return array
 	 */
 	function getOptionsSupported() {
-		return array(gettext('Watermark default images') => array ('key' => 'textobject_watermark_default_images', 'type' => OPTION_TYPE_CHECKBOX,
+		return array(gettext('Watermark default images') => array ('key' => 'GoogleDocs_watermark_default_images', 'type' => OPTION_TYPE_CHECKBOX,
 																	'desc' => gettext('Check to place watermark image on default thumbnail images.')));
 	}
 
 }
 
-class TextObject extends _Image {
+require_once(dirname(__FILE__).'/class-TextObject.php');
+
+class GoogleDocs extends TextObject {
 
 	/**
-	 * creates a textobject (image standin)
+	 * creates a GoogleDocs (image standin)
 	 *
 	 * @param object $album the owner album
 	 * @param string $filename the filename of the text file
 	 * @return TextObject
 	 */
-	function TextObject($album, $filename) {
+	function GoogleDocs($album, $filename) {
 		global $_zp_supported_images;
 
 		// $album is an Album object; it should already be created.
@@ -139,8 +90,14 @@ class TextObject extends _Image {
 			$imgfile = getAlbumFolder().$this->album->name.'/'.$this->objectsThumb;
 		} else {
 			switch(getSuffix($this->filename)) {
+				case "pdf":
+					$img = '/pdfDefault.png';
+					break;
+				case 'pps':
+					$img = '/ppsDefault.png';
+					break;
 				default: // just in case we extend and are lazy...
-					$img = '/textDefault.png';
+					$img = '/googleDocsDefault.png';
 					break;
 			}
 			$imgfile = $path . '/' . THEMEFOLDER . '/' . internalToFilesystem($this->album->gallery->getCurrentTheme()) . '/images/'.$img;
@@ -159,11 +116,11 @@ class TextObject extends _Image {
 	 */
 	function getThumb($type='image') {
 		list($custom, $sw, $sh, $cw, $ch, $cx, $cy) = $this->getThumbCropping($type);
-		$wmt = getOption('TextObject_watermark');
+		$wmt = getOption('GoogleDocs_watermark');
 		if ($this->objectsThumb == NULL) {
 			$cx = $cy = NULL;
 			$filename = makeSpecialImageName($this->getThumbImageFile());
-			if (!getOption('textobject_watermark_default_images')) {
+			if (!getOption('GoogleDocs_watermark_default_images')) {
 				$wmt = '!';
 			}
 		} else {
@@ -189,10 +146,9 @@ class TextObject extends _Image {
 		if (is_null($w)) $w = $this->getWidth();
 		if (is_null($h)) $h = $this->getHeight();
 		switch(getSuffix($this->filename)) {
-			case 'txt':
-			case 'htm':
-			case 'html':
-				return '<span style="display:block;width:'.$w.'px;height:'.$h.'px;" class="textobject">'.@file_get_contents($this->localpath).'</span>';
+			case 'pps':
+			case 'pdf':
+				return '<iframe src="http://docs.google.com/gview?url='.$this->getFullImage(FULLWEBPATH).'&amp;embedded=true" style="width:'.$w.'px;height:'.$h.'px;" frameborder="0"></iframe>';
 			default: // just in case we extend and are lazy...
 				return '<img src="'.$this->getThumb().'">';
 		}
@@ -217,7 +173,7 @@ class TextObject extends _Image {
 	 */
 	function getCustomImage($size, $width, $height, $cropw, $croph, $cropx, $cropy, $thumbStandin=false, $effects=NULL) {
 		if ($thumbStandin) {
-			$wmt = getOption('TextObject_watermark');
+			$wmt = getOption('GoogleDocs_watermark');
 		} else {
 			$wmt = NULL;
 		}
@@ -225,7 +181,7 @@ class TextObject extends _Image {
 		if ($thumbStandin) {
 			if ($this->objectsThumb == NULL) {
 				$filename = makeSpecialImageName($this->getThumbImageFile());
-				if (!getOption('textobject_watermark_default_images')) {
+				if (!getOption('GoogleDocs_watermark_default_images')) {
 					$args[11] = '!';
 				}
 				return getImageProcessorURI($args, $this->album->name, $filename);
@@ -264,9 +220,8 @@ class TextObject extends _Image {
 	 */
 	function updateDimensions() {
 		switch(getSuffix($this->filename)) {
-			case 'txt':
-			case 'htm':
-			case 'html':
+			case 'pdf':
+			case 'pps':
 				$this->set('width', getOption('image_size'));
 				$this->set('height', floor((getOption('image_size') * 24) / 36));
 				break;
