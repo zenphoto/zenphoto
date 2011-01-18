19e284c5025030c6eff0ef858e3027c4e36f8b3e
Malte Müller <wartgart@gmx.net>
2011-01-18 10:27:19 +0000

    Sitemap fix (Thanks to frankm again)

--- zp-core/zp-extensions/sitemap-extended.php
+++ zp-core/zp-extensions/sitemap-extended.php
@@ -165,7 +165,7 @@ if(isset($_GET['sitemap'])) {
 	// Output XML file headers, and plug the plugin :)
 	sitemap_echonl('<?xml version="1.0" encoding="UTF-8"?>');
 	if(getOption('sitemap_google')) {
-		sitemap_echonl('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">');
+		sitemap_echonl('<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">');
 	} else {
 		sitemap_echonl('<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">');
 	}
