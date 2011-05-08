9f0c399578aa353ea13492520edf3c0005875163
Stephen Billard <stephen@sbillard.org>
2011-05-08 18:36:09 +0000

    check before requiring zenpage template functions

--- zp-core/zp-extensions/menu_manager/menu_tab.php
+++ zp-core/zp-extensions/menu_manager/menu_tab.php
@@ -3,7 +3,9 @@ define ('OFFSET_PATH', 4);
 require_once(dirname(dirname(dirname(__FILE__))).'/admin-functions.php');
 require_once(dirname(dirname(dirname(__FILE__))).'/admin-globals.php');
 require_once(dirname(dirname(dirname(__FILE__))).'/template-functions.php');
-require_once(dirname(dirname(dirname(__FILE__))).'/'.PLUGIN_FOLDER.'/zenpage/zenpage-admin-functions.php');
+if (getOption('zp_plugin_zenpage')) {
+	require_once(dirname(dirname(dirname(__FILE__))).'/'.PLUGIN_FOLDER.'/zenpage/zenpage-admin-functions.php');
+}
 require_once(dirname(dirname(dirname(__FILE__))).'/'.PLUGIN_FOLDER.'/menu_manager/menu_manager-admin-functions.php');
 
 admin_securityChecks(NULL, currentRelativeURL(__FILE__));
--- zp-core/zp-extensions/menu_manager/menu_tab_edit.php
+++ zp-core/zp-extensions/menu_manager/menu_tab_edit.php
@@ -2,7 +2,9 @@
 define ('OFFSET_PATH', 4);
 require_once(dirname(dirname(dirname(__FILE__))).'/admin-functions.php');
 require_once(dirname(dirname(dirname(__FILE__))).'/admin-globals.php');
-require_once(dirname(dirname(dirname(__FILE__))).'/'.PLUGIN_FOLDER.'/zenpage/zenpage-admin-functions.php');
+if (getOption('zp_plugin_zenpage')) {
+	require_once(dirname(dirname(dirname(__FILE__))).'/'.PLUGIN_FOLDER.'/zenpage/zenpage-admin-functions.php');
+}
 require_once(dirname(__FILE__).'/menu_manager-admin-functions.php');
 
 admin_securityChecks(NULL, currentRelativeURL(__FILE__));
