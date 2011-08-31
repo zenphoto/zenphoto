4e0b16802df1c205e4406515a07416d10423771c
Malte Müller <wartgart@gmx.net>
2011-08-31 17:34:52 +0000

    Updates options text for slideshow

--- zp-core/zp-extensions/slideshow.php
+++ zp-core/zp-extensions/slideshow.php
@@ -53,7 +53,7 @@ class slideshowOptions {
 
 	function getOptionsSupported() {
 		return array(	gettext('Slide width') => array('key' => 'slideshow_width', 'type' => OPTION_TYPE_TEXTBOX,
-										'desc' => gettext("Width of the images in the slideshow. <em>[jQuery Cycle mode option]</em><br />If empty the theme options <em>image size</em> is used.")),
+										'desc' => gettext("Width of the images in the slideshow. <em>[jQuery Cycle/Colorbox sizedimage mode option]</em><br />If empty the theme options <em>image size</em> is used.")),
 									gettext('Slide height') => array('key' => 'slideshow_height', 'type' => OPTION_TYPE_TEXTBOX,
 										'desc' => gettext("Height of the images in the slideshow. <em>[jQuery Cycle mode option]</em><br />If empty the theme options <em>image size</em> is used.")),
 									gettext('Watermark') => array('key' => 'slideshow_watermark', 'type' => OPTION_TYPE_CHECKBOX,
