a8588f5d86bd20e8814979124d2289da3df34a3f
buildscript <buildscript@zenphoto.org>
2011-09-01 06:03:19 +0000

    Nightly script: build tag updated to 7980

--- zp-core/version.php
+++ zp-core/version.php
@@ -1,4 +1,4 @@
 <?php // This file contains version info only and is automatically updated. DO NOT EDIT.
 define('ZENPHOTO_VERSION', '1.4.2-DEV');
-define('ZENPHOTO_RELEASE', 7973);
+define('ZENPHOTO_RELEASE', 7980);
 ?>
