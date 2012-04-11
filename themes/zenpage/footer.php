8026ec4539d88fe20639db8e4c856efc3ec35101
Stephen Billard <stephen@sbillard.org>
2012-04-11 23:03:12 +0000

    consistent zenphoto logo

--- themes/zenpage/footer.php
+++ themes/zenpage/footer.php
@@ -3,4 +3,4 @@
 // force UTF-8 Ø
 
 echo gettext("Zenpage theme by <a href=\"http://www.maltem.de\">Malte Müller</a>");
-?> | <?php echo gettext("Powered by <a href=\"http://www.zenphoto.org\" title=\"A simpler web album\"><span id=\"zen-part\">zen</span><span id=\"photo-part\">PHOTO</span></a>"); ?>
+?> | <?php printZenphotoLink(); ?>
