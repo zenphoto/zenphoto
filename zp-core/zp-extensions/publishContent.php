a446573bca2d17afb3de4832599c4b31c14541f1
Stephen Billard <stephen@sbillard.org>
2012-04-15 02:15:17 +0000

    fix some documentation

--- zp-core/zp-extensions/publishContent.php
+++ zp-core/zp-extensions/publishContent.php
@@ -1,24 +1,31 @@
 <?php
 /**
  *
- * There you can also change the default setting of the albums: published and
- * the images: visible fields.
- *
- * You will be presented a list un-published albums and  a list of not visible images from before a
- * specific data and time. You can select albums and images from these lists to be published.
- * <b>NOTE:</b> there is no record of when albums were first encountered, so all un-published
- * albums are show.
- *
+ * You will be presented a list un-published albums and a list of not visible images. You can select albums and
+ * images from these lists to be published. (
  * So you can freely upload albums and images then on a periodic basis review which ones to make available
  * to visitors of your gallery.
+ * Only images that are older than specific date and time will be shown. (You can select that benchmark making it easy
+ * to allow new images to <i>age</i> before you decide to publish them.)
+ * There is no record of when albums were first encountered, so all un-published
+ * albums are show.
+ *
+ * If you have the <var>Zenpage</var> content management plugin enabled you will also have lists of
+ * unpublished <i>categories</i>, <i>news articles</i>, and <i>pages</i>.
+ *
+ * <b>NOTE:</b>  The field sets for each of these displays is <i>collapsed</i> by default. Click on the legend to
+ * <i>open</i> the view.
+ *
+ * You can also change the default setting of the albums <i>published</i> and
+ * the images <i>visible</i> fields. (These are the same options provided in the <i>Gallery behavior</i> section of
+ * the gallery options tab.)
  *
  * @package admin
  */
 
 $plugin_is_filter = 9|ADMIN_PLUGIN;
-$plugin_description = gettext('Allows bulk management of image and album content.');
+$plugin_description = gettext('A single place to quickly review your unpublished content.');
 $plugin_author = "Stephen Billard (sbillard)";
-$plugin_version = '1.4.3';
 
 zp_register_filter('admin_utilities_buttons', 'publishContent::button');
 
