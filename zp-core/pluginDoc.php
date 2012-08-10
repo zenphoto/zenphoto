521362e8374c65a27a36a6ca9328f0e1a64765df
Stephen Billard <stephen@sbillard.org>
2012-08-10 17:28:19 +0000

    fix issues with bad image and album instantiation in pluginDoc.php

--- zp-core/pluginDoc.php
+++ zp-core/pluginDoc.php
@@ -55,8 +55,8 @@ if (!defined('OFFSET_PATH')) {
 
 	@require_once($path);
 	$buttonlist = zp_apply_filter('admin_utilities_buttons', array());
-	$album = new Album(NULL, '', false, true);
-	$image = new _Image($album, '');
+	$album = new ALbumbase(NULL, '', false);
+	$image = new Transientimage($album, '');
 	$imagebuttons = zp_apply_filter('edit_image_utilities', '', $image, 0, '', ''); //pass space as HTML because there is already a button shown for cropimage
 	$albumbuttons = zp_apply_filter('edit_album_utilities', '', $album, '');
 
