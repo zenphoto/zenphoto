a44673ed2952c2c43fc88668286b4b0f2b16726f
Stephen Billard <stephen@sbillard.org>
2012-10-01 19:54:13 +0000

    turn on debug options in DEV branch even in nightlies

--- zp-core/global-definitions.php
+++ zp-core/global-definitions.php
@@ -19,12 +19,14 @@ define('ADMIN_PLUGIN',2048);
 define('THEME_PLUGIN',1024);
 define('PLUGIN_PRIORITY',1023);
 
+define('TEST_RELEASE',!defined('RELEASE')||strpos(ZENPHOTO_VERSION, '-')!==false);
+
 define('DEBUG_LOGIN', false); // set to true to log admin saves and login attempts
-define('DEBUG_ERROR', !defined('RELEASE')); // set to true to supplies the calling sequence with zp_error messages
+define('DEBUG_ERROR', TEST_RELEASE); // set to true to supplies the calling sequence with zp_error messages
 define('DEBUG_IMAGE', false); // set to true to log image processing debug information.
-define('DEBUG_IMAGE_ERR', !defined('RELEASE')); // set to true to flag image processing errors.
-define('DEBUG_ALBUM_ERR', !defined('RELEASE')); // set to true to flag album instantiation errors.
-define('DEBUG_404', !defined('RELEASE')); // set to true to log 404 error processing debug information.
+define('DEBUG_IMAGE_ERR', TEST_RELEASE); // set to true to flag image processing errors.
+define('DEBUG_ALBUM_ERR', TEST_RELEASE); // set to true to flag album instantiation errors.
+define('DEBUG_404', TEST_RELEASE); // set to true to log 404 error processing debug information.
 define('DEBUG_EXIF', false); // set to true to log start/finish of exif processing. Useful to find problematic images.
 define('DEBUG_PLUGINS', false); // set to true to log plugin load sequence.
 define('DEBUG_FILTERS', false); // set to true to log filter application sequence.
