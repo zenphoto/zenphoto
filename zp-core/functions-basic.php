79a4748fd271fa6e2eb710beedcaa30e158dfb53
Stephen Billard <stephen@sbillard.org>
2012-10-29 15:44:12 -0700

    Fix to 15d8828

--- zp-core/functions-basic.php
+++ zp-core/functions-basic.php
@@ -150,7 +150,7 @@ define('FILE_MOD', CHMOD_VALUE & 0666);
 if (!isset($_zp_conf_vars['server_protocol'])) $_zp_conf_vars['server_protocol'] = 'http';
 
 require_once(dirname(__FILE__).'/functions-db-'.(isset($_zp_conf_vars['db_software'])?$_zp_conf_vars['db_software']:'MySQL').'.php');
-if (OFFSET_PATH != 2 && !db_connect(false)) {
+if (!db_connect(false) && OFFSET_PATH != 2) {
 	require_once(dirname(__FILE__).'/reconfigure.php');
 	reconfigureAction(true);
 }
