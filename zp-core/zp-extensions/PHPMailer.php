1a0b7ce493790596a6bfd1481aa5c660b00cbc62
Stephen Billard <stephen@sbillard.org>
2012-11-06 16:31:46 -0800

    Reply-to added to mail from contact form
    Adds "reply-to" handling to the zp mailing system. Adds the sender's
    name as a reply-to on contact form contact mails
    Fix #96

--- zp-core/zp-extensions/PHPMailer.php
+++ zp-core/zp-extensions/PHPMailer.php
@@ -83,7 +83,7 @@ class zp_PHPMailer {
 
 }
 
-function zenphoto_PHPMailer($msg, $email_list, $subject, $message, $from_mail, $from_name, $cc_addresses) {
+function zenphoto_PHPMailer($msg, $email_list, $subject, $message, $from_mail, $from_name, $cc_addresses, $replyTo) {
 	require_once(dirname(__FILE__).'/PHPMailer/class.phpmailer.php');
 	switch (getOption('PHPMailer_mail_protocol')) {
 		case 'pop3':
@@ -129,6 +129,10 @@ function zenphoto_PHPMailer($msg, $email_list, $subject, $message, $from_mail, $
 		foreach ($cc_addresses as $cc_name=>$cc_mail) {
 			$mail->AddCC($cc_mail);
 		}
+	}
+	if ($replyTo) {
+		$names = array_keys($replyTo)
+		$mail->AddReplyTo(array_shift($replyTo), array_shift($names)));
 	}
 	if (!$mail->Send()) {
 		if (!empty($msg)) $msg .= '<br />';
