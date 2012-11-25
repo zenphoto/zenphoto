dc368c9d338371bde1fffb7dd62e91c4326e3610
Stephen Billard <stephen@sbillard.org>
2012-11-25 12:51:16 -0800

    release number simplification

--- zp-core/version.php
+++ zp-core/version.php
@@ -1,8 +1,5 @@
 <?php // This file contains version info only and is automatically updated. DO NOT EDIT.
 define('ZENPHOTO_VERSION', '1.4.4-DEV');
-list($_release, $_full_release) = explode("\n",file_get_contents(dirname(__FILE__).'/githead'));
-define('ZENPHOTO_RELEASE', trim($_release));
-define('ZENPHOTO_FULL_RELEASE', trim($_full_release));
-unset($_release);
-unset($_full_release);
+define('ZENPHOTO_FULL_RELEASE', trim(file_get_contents(dirname(__FILE__).'/githead')));
+define('ZENPHOTO_RELEASE', substr(ZENPHOTO_FULL_RELEASE,0,7));
 ?>
