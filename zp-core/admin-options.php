08b0443bb92f6f24cceb9423550a9f4f66bf2ee1
acrylian <wartgart@gmx.net>
2012-11-25 11:11:10 +0100

    Some css fixes for plugin options

--- zp-core/admin-options.php
+++ zp-core/admin-options.php
@@ -2946,7 +2946,7 @@ if ($subtab == 'plugin' && zp_loggedin(ADMIN_RIGHTS)) {
 								$v= 0;
 							}
 							?>
-							<th style="text-align:left">
+							<th style="text-align:left; width: 200px;">
 								<span id="<?php echo $extension; ?>" ></span>
 								<input type="hidden" name="show-<?php echo $extension;?>" id="show-<?php echo $extension;?>" value="<?php echo $v; ?>" />
 								<span style="display:<?php echo $show_show; ?>;" class="pluginextrashow">
@@ -2963,8 +2963,8 @@ if ($subtab == 'plugin' && zp_loggedin(ADMIN_RIGHTS)) {
 									<a href="javascript:$('#show-<?php echo $extension;?>').val(0);toggleExtraInfo('<?php echo $extension;?>','plugin',false);"><?php echo $extension; ?></a>
 								</span>
 							</th>
-							<th colspan="2" style="text-align:left">
-								<?php echo $plugin_description; //TODO: this needs CSS help!!!!?>
+							<th style="text-align:left; font-weight: normal;">
+								<?php echo $plugin_description; //TODO: this needs CSS help!!!! ?>
 							</th>
 						</tr>
 						<?php
