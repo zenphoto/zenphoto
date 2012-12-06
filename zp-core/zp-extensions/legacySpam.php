407357fcd46445a231a4693d585456382df53887
Stephen Billard <stephen@sbillard.org>
2012-12-06 15:45:25 -0800

    Provide transparency to the target spam filter
    Allows options of the target spam filter to be set.
    Allows selection of the target spam filter.

--- zp-core/zp-extensions/legacySpam.php
+++ zp-core/zp-extensions/legacySpam.php
@@ -2,8 +2,9 @@
 /**
  * This is a shell plugin for legacy external SPAM filters.
  *
- * It assumes that there is only one SPAM filter in the <var>%USER_PLUGIN_FOLDER%/spamfilters</var> folder.
- * It will "load" that SPAM filter for use by the <var>comment_form</var> plugin.
+ * The plugin will load the spam filter defined by the <i>spam_filter</i> option if it is found in the
+ * <var>%USER_PLUGIN_FOLDER%/spamfilter</var> folder. This allows older third-party spam filters to continue to
+ * be used.
  *
  * @author Stephen Billard (sbillard)
  * @package plugins
@@ -11,41 +12,79 @@
  */
 
 $plugin_is_filter = 5|CLASS_PLUGIN;
-$plugin_description = gettext("Plugin to enable using legacy third party SPAM filters.");
+$plugin_description = gettext("Use this plugin is to enable using older third party spam filters.");
 $plugin_author = "Stephen Billard (sbillard)";
 $plugin_disable = (isset($_zp_spamFilter) && !getoption('zp_plugin_legacySpam'))?sprintf(gettext('Only one SPAM handler plugin may be enalbed. <a href="#%1$s"><code>%1$s</code></a> is already enabled.'),$_zp_spamFilter->name):'';
-$plugin_notice = gettext('This plugin is to enable using older third party spam filters. Please contact the author of your SPAM filter and request it be updated to work with the <i>plugin</i> model of SPAM filters. This plugin will be removed from future Zenphoto releases.');
+$plugin_notice = gettext('Please contact the author of your SPAM filter and request it be updated to work with the <i>plugin</i> model of SPAM filters. This plugin will be removed from future Zenphoto releases.');
 
-if ($plugin_disable) {
-	setOption('zp_plugin_legacySpam', 0);
-} else {
-	$filters = getPluginFiles('*.php','spamfilters');
-	if (!empty($filters)) {
-		foreach ($filters as $name=>$path) {
+$option_interface = 'zpLegacySpam';
 
-			require_once($path);
+$filters = getPluginFiles('*.php','spamfilters');
+$actingfor = getOption('spam_filter');
+foreach ($filters as $_legacyFilter=>$path) {
+	if ($actingfor == $_legacyFilter) {
+		require_once($path);
+		break;
+	}
+}
 
-			class zpLegacySpam extends SpamFilter {
+if (class_exists('SpamFilter')) {
 
-				var $name = 'legacySpam';
-				var $actingFor = NULL;
+	class zpLegacySpam extends SpamFilter {
 
-				function __construct($for) {
-					$this->actingFor = $for;
-					parent::__construct();
-				}
+		var $name = 'legacySpam';
+		var $actingFor = NULL;
 
-				function displayName() {
-					return $this->actingFor;
-				}
+		function __construct() {
+			global $_legacyFilter;
+			$this->actingFor = $_legacyFilter;
+			parent::SpamFilter();
+		}
 
+		function displayName() {
+			return 'zpLegacySpam::'.$this->actingFor;
+		}
+
+		function getOptionsSupported() {
+			$list = array();
+			foreach (getPluginFiles('*.php','spamfilters') as $filter=>$path) {
+				$list[$filter] = $filter;
 			}
-
-			$_zp_spamFilter = new zpLegacySpam($name);
-			break;
+			$options[gettext('Associated Spam Filter')] = array('key' => 'spam_filter', 'type' => OPTION_TYPE_SELECTOR,
+					'selections' => $list,
+					'order' => 0,
+					'desc' => gettext('Select the legacy spam filter to be used.'));
+			return array_merge($options,parent::getOptionsSupported());
 		}
 
 	}
+
+} else {
+
+	class zpLegacySpam {
+
+		function getOptionsSupported() {
+			$list = array();
+			foreach (getPluginFiles('*.php','spamfilters') as $filter=>$path) {
+				$list[$filter] = $filter;
+			}
+			$options[gettext('Associated Spam Filter')] = array('key' => 'spam_filter', 'type' => OPTION_TYPE_SELECTOR,
+					'selections' => $list,
+					'order' => 0,
+					'desc' => gettext('Select the legacy spam filter to be used.'));
+			return $options;
+		}
+
+		function displayName() {
+			return 'zpLegacySpam::<em>not found</em>';
+		}
+
+	}
+
+}
+
+if (isset($_legacyFilter) && !$plugin_disable) {
+	$_zp_spamFilter = new zpLegacySpam();
 }
 
 ?>
