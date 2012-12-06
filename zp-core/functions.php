ddd7f0e6ecb6824026575ce51bf299ed016816be
Stephen Billard <stephen@sbillard.org>
2012-12-06 15:59:31 -0800

    fix #127

--- zp-core/functions.php
+++ zp-core/functions.php
@@ -2339,6 +2339,12 @@ class zpFunctions {
 	function checkCaptcha($s1, $s2) {
 		return true;
 	}
+	function getOptionsSupported() {
+		$option['note'] = array('key' => 'captcha_note',
+														'type' => OPTION_TYPE_NOTE,
+														'desc' => gettext('<p class="notebox">'.gettext('No captcha handler is enabled.').'</p>'));
+		return $option;
+	}
 
 	/**
 	 * Searches out i.php image links and replaces them with cache links if image is cached
