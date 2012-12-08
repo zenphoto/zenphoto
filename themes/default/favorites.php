59ccadfbff26bba01746a958b7f4a3b9e51e1c65
fretzl <f.sondaar@wxs.nl>
2012-12-08 21:57:32 +0100

    And one more...

--- themes/default/favorites.php
+++ themes/default/favorites.php
@@ -6,7 +6,7 @@ if (!defined('WEBPATH')) die();
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<?php zp_apply_filter('theme_head'); ?> zp_apply_filter('theme_head'); ?>
+		<?php zp_apply_filter('theme_head'); ?>
 		<title><?php printBareGalleryTitle(); ?> | <?php printBareAlbumTitle(); if ($_zp_page>1) echo "[$_zp_page]"; ?></title>
 		<meta http-equiv="content-type" content="text/html; charset=<?php echo LOCAL_CHARSET; ?>" />
 		<link rel="stylesheet" href="<?php echo pathurlencode($zenCSS); ?>" type="text/css" />
