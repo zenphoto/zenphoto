1b2e9bf1f07604f7b90114487397e702f7214f3d
SubJunk <subjunk@gmail.com>
2012-12-28 13:41:21 +1300

    Removed extra semicolon

--- zp-core/zp-extensions/image_album_statistics.php
+++ zp-core/zp-extensions/image_album_statistics.php
@@ -89,7 +89,7 @@ function getAlbumStatistic($number=5, $option, $albumfolder='', $threshold=0) {
 			$sortorder = "total_votes";
 			break;
 		case "toprated":
-			$sortorder = "(total_value/total_votes) DESC, total_value";;
+			$sortorder = "(total_value/total_votes) DESC, total_value";
 			break;
 		case "latestupdated":
 			$sortorder = 'updateddate';
