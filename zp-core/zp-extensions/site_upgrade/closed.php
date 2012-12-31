f24c48040aa71a88e5fd6abc95b3511bec4fcb75
Stephen Billard <stephen@sbillard.org>
2012-12-31 11:31:28 -0800

    fix #147
    Note: you will need to remove the flawed closte.php file from your
    plugins/site_upgrade folder as the site_upgrade plugin does not override
    an existing script.

--- zp-core/zp-extensions/site_upgrade/closed.php
+++ zp-core/zp-extensions/site_upgrade/closed.php
@@ -1,6 +1,6 @@
 <?php
-header('Last-Modified: ' . ZP_LAST_MODIFIED);
-header('Content-Type: text/html; charset=' . LOCAL_CHARSET);
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s').' GMT');
+header('Content-Type: text/html; charset=UTF-8');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
