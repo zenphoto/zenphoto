1389d09751752185447d2e1472ce8b2c80fbab2c
Stephen Billard <stephen@sbillard.org>
2013-01-31 13:02:36 -0800

    Revert "Insure that hidden rights are set for a user"
    This reverts commit 03b2cf0249e25abfb8bcac2fa5c9d70cbbbd913c.

--- zp-core/lib-auth.php
+++ zp-core/lib-auth.php
@@ -1271,41 +1271,27 @@ class Zenphoto_Administrator extends PersistentObject {
 			$this->set('id', -1);
 		}
 		if ($valid) {
-			$rights = $this->getRights();
-			$new_rights = 0;
 			if ($user == $_zp_authority->master_user) {
-				$new_rights = ALL_RIGHTS;
+				$this->setRights($this->getRights() | ADMIN_RIGHTS);
 				$this->master = true;
 			} else {
 				// make sure that the "hidden" gateway rights are set for managing objects
-				if ($rights & MANAGE_ALL_ALBUM_RIGHTS) {
-					$new_rights = $new_rights | ALBUM_RIGHTS;
-				}
-				if ($rights & MANAGE_ALL_NEWS_RIGHTS) {
-					$new_rights = $new_rights | ZENPAGE_PAGES_RIGHTS;
-				}
-				if ($rights & MANAGE_ALL_PAGES_RIGHTS) {
-					$new_rights = $new_rights | ZENPAGE_NEWS_RIGHTS;
-				}
 				$this->getObjects();
 				foreach ($this->objects as $object) {
 					switch ($object['type']) {
 						case 'album':
 							if ($object['edit'] && MANAGED_OBJECT_RIGHTS_EDIT) {
-								$new_rights = $new_rights | ALBUM_RIGHTS;
+								$this->setRights($this->getRights() | ALBUM_RIGHTS);
 							}
 							break;
 						case 'pages':
-							$new_rights = $new_rights | ZENPAGE_PAGES_RIGHTS;
+							$this->setRights($this->getRights() | ZENPAGE_PAGES_RIGHTS);
 							break;
 						case 'news':
-							$new_rights = $new_rights | ZENPAGE_NEWS_RIGHTS;
+							$this->setRights($this->getRights() | ZENPAGE_NEWS_RIGHTS);
 							break;
 					}
 				}
-			}
-			if ($new_rights) {
-				$this->setRights($rights | $new_rights);
 			}
 		}
 	}
