08599d107f04fb67e487b0313bf9bc1c55a130f1
Stephen Billard <stephen@sbillard.org>
2013-02-05 11:45:46 -0800

    Correct merge conflicts

--- zp-core/zp-extensions/jcarousel_thumb_nav.php
+++ zp-core/zp-extensions/jcarousel_thumb_nav.php
@@ -117,12 +117,6 @@ if (!OFFSET_PATH && getOption('jcarousel_'.$_zp_gallery->getCurrentTheme().'_'.s
 		} else {
 			$thumbscroll = sanitize_numeric($thumbscroll);
 		}
-<<<<<<< HEAD
-	if(count($jcarousel_items) >= 2) {
-		foreach($jcarousel_items as $item) {
-			if(is_array($item)) {
-				$imgobj = newImage(newAlbum($item['folder']),$item['filename']);
-=======
 		if(is_null($width)) {
 			$width = getOption('jcarousel_width');
 		} else {
@@ -164,7 +158,6 @@ if (!OFFSET_PATH && getOption('jcarousel_'.$_zp_gallery->getCurrentTheme().'_'.s
 				}	else {
 					$searchimages = true;
 				}
->>>>>>> local-fixes
 			} else {
 				$searchimages = false;
 			}
@@ -176,7 +169,7 @@ if (!OFFSET_PATH && getOption('jcarousel_'.$_zp_gallery->getCurrentTheme().'_'.s
 		if(count($jcarousel_items) >= 2) {
 			foreach($jcarousel_items as $item) {
 				if(is_array($item)) {
-					$imgobj = newImage(new Album($_zp_gallery,$item['folder']),$item['filename']);
+				$imgobj = newImage(newAlbum($item['folder']),$item['filename']);
 				} else {
 					$imgobj = newImage($_zp_current_album,$item);
 				}
