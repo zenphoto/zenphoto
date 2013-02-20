403fce6f7f0f5aa945e3c12067e9d6df8d55a071
Stephen Billard <stephen@sbillard.org>
2013-02-20 15:40:28 -0800

    restore sort order to cache items

--- zp-core/zp-extensions/cacheManager.php
+++ zp-core/zp-extensions/cacheManager.php
@@ -133,10 +133,11 @@ class cacheManager {
 		$result = query('SELECT * FROM '.prefix('plugin_storage').' WHERE `type`="cacheManager" ORDER BY `aux`');
 		$key = 0;
 		while ($row = db_fetch_assoc($result)) {
+			$theme = $row['aux'];
 			$data = unserialize($row['data']);
-			$custom[$data['theme']][] = $data;
+			$custom[$theme][] = $data;
 		}
-		$custom = sortMultiArray($custom, array('theme','thumb','image_size','image_width','image_height'));
+		ksort($custom);
 		$custom[''] = array(array());
 		$c = 0;
 		?>
@@ -159,6 +160,7 @@ class cacheManager {
 		</script>
 		<?php
 		foreach($custom as $theme=>$themedata) {
+			$themedata = sortMultiArray($themedata, array('thumb','image_size','image_width','image_height'));
 			if ($theme) {
 				?>
 				<span class="icons" id="<?php echo $theme; ?>_arrow">
