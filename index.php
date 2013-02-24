3cade6d6fad592fd57674e601469d3495ac9dd74
Stephen Billard <stephen@sbillard.org>
2013-02-24 13:23:11 -0800

    Missed a file for c4b60c3c2605b20b3419479bcbcf040ea4b66188

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 eval(@file_get_contents(dirname(__FILE__).'/zp-data/zenphoto.cfg'));
 if (@$_zp_conf_vars['site_upgrade_state']=='closed') {
-	header('location: plugins/site_upgrade/closed.php');
-	exit();
+	if (!preg_match('~page/setup_set-mod_rewrite\?z=setup$~', $_SERVER['REQUEST_URI'])) {
+		header('location: plugins/site_upgrade/closed.php');
+		exit();
+	}
 }
 include (dirname(__FILE__).'/zp-core/index.php');
 ?>
