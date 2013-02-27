893b73bbb96e97f2304dcf947eee47b308a54792
Stephen Billard <stephen@sbillard.org>
2013-02-26 18:13:06 -0800

    Link to the rewriteTokens plugin options  plugin o
    Place a link to where you can change the rewrite tokens in the
    description of the URL options.

--- zp-core/admin-options.php
+++ zp-core/admin-options.php
@@ -617,9 +617,9 @@ if ($subtab == 'general' && zp_loggedin(OPTIONS_RIGHTS)) {
 					</td>
 					<td>
 						<p>
-							<?php
-							echo gettext("If you have Apache <em>mod rewrite</em> (or equivalent), put a checkmark on the <em>mod rewrite</em> option and you'll get nice cruft-free URLs.");
-							if (!getOption('mod_rewrite_detected')) echo '<p class="notebox">'.gettext('Setup did not detect a working <em>mod_rewrite</em> facility.'),'</p>';
+							<?php echo gettext("If you have Apache <em>mod rewrite</em> (or equivalent), put a checkmark on the <em>mod rewrite</em> option and you'll get nice cruft-free URLs."); ?>
+							<?php echo sprintf(gettext('The <em>tokens</em> used in rewritten URIs may be altered to your taste. See the <a href="%s">plugin options</a> for <code>rewriteTokens</code>.'),WEBPATH.'/'.ZENFOLDER.'/admin-options.php?page=options&tab=plugin&single=rewriteTokens'); ?>
+							<?php if (!getOption('mod_rewrite_detected')) echo '<p class="notebox">'.gettext('Setup did not detect a working <em>mod_rewrite</em> facility.'),'</p>';
 							?>
 						</p>
 						<p><?php echo gettext("If you are having problems with images whose names contain characters with diacritical marks try changing the <em>UTF8 image URIs</em> setting."); ?></p>
