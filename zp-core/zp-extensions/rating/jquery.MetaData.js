39ee5933aafab598841f6f4340b8328aef1fea47
Stephen Billard <stephen@sbillard.org>
2013-03-18 18:26:06 -0700

    update jquery rating
    Ratings were broken by the js updates

--- zp-core/zp-extensions/rating/jquery.MetaData.js
+++ zp-core/zp-extensions/rating/jquery.MetaData.js
@@ -3,11 +3,8 @@
  *
  * Copyright (c) 2006 John Resig, Yehuda Katz, Jörn Zaefferer, Paul McLanahan
  *
- * Dual licensed under the MIT and GPL licenses:
- *   http://www.opensource.org/licenses/mit-license.php
- *   http://www.gnu.org/licenses/gpl.html
+	* Licensed under http://en.wikipedia.org/wiki/MIT_License
  *
- * Revision: $Id$
  *
  */
 
