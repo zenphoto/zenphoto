3d40b5f3e42972248d2c1f666b4673be646c18fd
Stephen Billard <stephen@sbillard.org>
2013-03-24 17:27:48 -0700

    Usaved changes that are part of [86ff4be]

--- zp-core/functions.php
+++ zp-core/functions.php
@@ -2065,44 +2065,7 @@ function reveal($content, $visible=false) {
 /**
  * Deals with the [macro parameters] substitutions
  *
- * Plugins can add to the macro list by registering the content_macro filter. They add to the
- * array an element for each macro. The array key is the macro, the element is an array with the
- * class of macro, the regular expression for parsing parameters and the "value" of the macro
- *
- * macro classes:
- * 	<ol>
- * 		<li>
- * 			<var>procedure</var> calls a script function that produces output. The output is captured and inserted in place of the macro instance
- * 		</li>
- * 		<li>
- * 			<var>function</var> calls a script function that returns a result. The result is inserted in place of the macro instance
- * 		</li>
- * 		<li>
- * 			<var>constant</var> replaces the macro instances with the constant provided
- * 		</li>
- * 		<li>
- * 			<var>expression</var> evaluates the expression provided and replaces the instance with the result of the evaluation. If a regex is supplied for an expression
- * 														the values provided will replace placeholders in the expression. The first parameter replaces $1, the second $2, etc.
- * 		</li>
- * 	</ol>
- *
- * useage examples:
- * 	<ol>
- * 		<li>
- * 			[CODEBLOCK 3]
- * 		</li>
- * 		<li>
- * 			[PAGE]
- * 		</li>
- * 		<li>
- * 			[ZENPHOTO_VERSION]
- * 		</li>
- * 		<li>
- * 			[pageLink register]
- * 		</li>
- * 	</ol>
- *
- *
+ * See the macroList plugin for details
  *
  * @param string $text
  * @return string
