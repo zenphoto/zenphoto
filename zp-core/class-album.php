afaf1c6c4ab888ffaff6342ece34b430dd1ff4c2
Stephen Billard <stephen@sbillard.org>
2013-05-15 19:37:51 -0700

    fix dynamic album list fetching
    Sometimes calling album->getAlbums would improperly set the subalbum
    list to a slice of the real list.

--- zp-core/class-album.php
+++ zp-core/class-album.php
@@ -863,7 +863,7 @@ class Album extends AlbumBase {
 			}
 			if ($this->isDynamic()) {
 				$search = $this->getSearchEngine();
-				$subalbums = $search->getAlbums($page,NULL,NULL,false);
+				$subalbums = $search->getAlbums(0,NULL,NULL,false);
 			} else {
 				$dirs = $this->loadFileNames(true);
 				$subalbums = array();
