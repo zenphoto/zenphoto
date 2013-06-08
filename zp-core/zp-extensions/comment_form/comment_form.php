93e20872d7324f5b8dacb31fbbafaf4042866317
acrylian <wartgart@gmx.net>
2013-06-08 11:34:23 +0200

    Form cleanup regarding the required stars Fixes contact_form not checking for required fields at all (all were required always); Make all required stars enclosed in strong tags without space (Set via the theme using "form label strong { … } if really needed - in German for example it is not)

--- zp-core/zp-extensions/comment_form/comment_form.php
+++ zp-core/zp-extensions/comment_form/comment_form.php
@@ -2,6 +2,7 @@
 	<input type="hidden" name="comment" value="1" />
 	<input type="hidden" name="remember" value="1" />
 	<?php
+	$star = '<strong>*</strong>';
 	printCommentErrors();
 	$required = false;
 	?>
@@ -11,15 +12,9 @@
 	</p>
 	<?php
 	if ($req = getOption('comment_name_required')) {
-		if ($req == 'required') {
-			$star = " <strong>*</strong>";
-			$required = true;
-		} else {
-			$star = '';
-		}
 		?>
 		<p>
-			<label for="name"><?php printf(gettext("Name%s"),$star); ?></label>
+			<label for="name"><?php printf(gettext("Name%s"),($req == 'required' ? $star : '')); ?></label>
 			<input<?php if($disabled['name']) echo ' READONLY '; ?> type="text" id="name" name="name" size="22" value="<?php echo html_encode($stored['name']);?>" class="inputbox" />
 		</p>
 		<?php
@@ -33,65 +28,47 @@
 			}
 	}
 	if ($req = getOption('comment_email_required')) {
-		if ($req == 'required') {
-			$star = "*";
-			$required = true;
-		} else {
-			$star = '';
-		}
 		?>
 		<p>
-		<label for="email"><?php printf(gettext("E-Mail%s"),$star); ?></label>
+		<label for="email"><?php printf(gettext("E-Mail%s"),($req == 'required' ? $star : '')); ?></label>
 		<input <?php if($disabled['email']) echo 'READONLY'; ?> type="text" id="email" name="email" size="22" value="<?php echo html_encode($stored['email']);?>" class="inputbox" />
 		</p>
 		<?php
 		}
 		if ($req = getOption('comment_web_required')) {
-			if ($req == 'required') {
-				$star = "<strong>*</strong>";
-				$required = true;
-			} else {
-				$star = '';
-			}
 			?>
 			<p>
-				<label for="website"><?php printf(gettext("Site%s"),$star); ?></label>
+				<label for="website"><?php printf(gettext("Site%s"),($req == 'required' ? $star : '')); ?></label>
 				<input <?php if($disabled['website']) echo 'READONLY'; ?> type="text" id="website" name="website" size="22" value="<?php echo html_encode($stored['website']);?>" class="inputbox" />
 			</p>
 	<?php
 	}
 	if ($req = getOption('comment_form_addresses')) {
-		if ($req == 'required') {
-			$star = '<strong>*</strong>';
-			$required = true;
-		} else {
-			$star = '';
-		}
 		?>
 		<p>
-			<label for="0-comment_form_street"><?php printf(gettext('Street%s'),$star); ?></label>
+			<label for="0-comment_form_street"><?php printf(gettext('Street%s'),($req == 'required' ? $star : '')); ?></label>
 			<input <?php if($disabled['street']) echo 'READONLY'; ?> type="text" name="0-comment_form_street" id="0-comment_form_street" class="inputbox" size="22" value="<?php echo html_encode($stored['street']); ?>" />
 		</p>
 		<p>
-			<label for="0-comment_form_city"><?php printf(gettext('City%s'),$star); ?></label>
+			<label for="0-comment_form_city"><?php printf(gettext('City%s'),($req == 'required' ? $star : '')); ?></label>
 			<input <?php if($disabled['city']) echo 'READONLY'; ?> type="text" name="0-comment_form_city" id="0-comment_form_city" class="inputbox" size="22" value="<?php echo html_encode($stored['city']); ?>" />
 		</p>
 		<p>
-			<label for="comment_form_state"><?php printf(gettext('State%s'),$star); ?></label>
+			<label for="comment_form_state"><?php printf(gettext('State%s'),($req == 'required' ? $star : '')); ?></label>
 			<input <?php if($disabled['state']) echo 'READONLY'; ?> type="text" name="0-comment_form_state" id="comment_form_state" class="inputbox" size="22" value="<?php echo html_encode($stored['state']); ?>" />
 		</p>
 		<p>
-			<label for="comment_form_country"><?php printf(gettext('Country%s'),$star); ?></label>
+			<label for="comment_form_country"><?php printf(gettext('Country%s'),($req == 'required' ? $star : '')); ?></label>
 			<input <?php if($disabled['country']) echo 'READONLY'; ?> type="text" id="comment_form_country" name="0-comment_form_country" class="inputbox" size="22" value="<?php echo html_encode($stored['country']); ?>" />
 		</p>
 		<p>
-			<label for="comment_form_postal"><?php printf(gettext('Postal code%s'),$star); ?></label>
+			<label for="comment_form_postal"><?php printf(gettext('Postal code%s'),($req == 'required' ? $star : '')); ?></label>
 			<input <?php if($disabled['postal']) echo 'READONLY'; ?> type="text" id="comment_form_postal" name="0-comment_form_postal" class="inputbox" size="22" value="<?php echo html_encode($stored['postal']); ?>" />
 		</p>
 	<?php
 	}
 	if (commentFormUseCaptcha()) {
-		$captcha = $_zp_captcha->getCaptcha(gettext("Enter CAPTCHA <strong>*</strong>"));
+		$captcha = $_zp_captcha->getCaptcha(gettext("Enter CAPTCHA<strong>*</strong>"));
 		?>
 		<p>
 			<?php
