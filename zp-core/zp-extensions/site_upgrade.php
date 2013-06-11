89c89080a952f2ec99526d9c68dc76ce66880e40
Stephen Billard <stephen@sbillard.org>
2013-06-11 16:34:16 -0700

    fix #431

--- zp-core/zp-extensions/site_upgrade.php
+++ zp-core/zp-extensions/site_upgrade.php
@@ -36,7 +36,7 @@
 $plugin_is_filter = 97|ADMIN_PLUGIN|THEME_PLUGIN;
 $plugin_description = gettext('Utility to divert access to the gallery to a screen saying the site is upgrading.');
 $plugin_author = "Stephen Billard (sbillard)";
-$plugin_notice = (MOD_REWRITE) ? false : gettext('<em>mod_rewrite</em> is not enabled. This plugin may not work without without rewrite redirection if the upgrade is significantly different than the running release.');
+$plugin_notice = (MOD_REWRITE) ? false : gettext('<em>mod_rewrite</em> is not enabled. This plugin may not work without rewrite redirection if the upgrade is significantly different than the running release.');
 
 switch (OFFSET_PATH) {
 	case 0:
