9f53df46c6548420813829dfeec2c4b4b43ba8ff
APAdmin <admin@animepapers.org>
2013-07-22 01:23:41 -0400

    ZenPapers first release

---
+++ themes/zenpapers/404.php
@@ -0,0 +1,47 @@
+<?php
+// force UTF-8 Ø
+
+if (!defined('WEBPATH')) die();
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php zp_apply_filter('theme_head'); ?>
+		<title><?php printBareGalleryTitle(); ?> | <?php echo gettext("Object not found"); ?></title>
+		<meta http-equiv="content-type" content="text/html; charset=<?php echo LOCAL_CHARSET; ?>" />
+		<link rel="stylesheet" href="<?php echo pathurlencode($zenCSS); ?>" type="text/css" />
+		<link rel="stylesheet" href="<?php echo WEBPATH.'/'.THEMEFOLDER; ?>/default/common.css" type="text/css" />
+	</head>
+	<body>
+		<?php zp_apply_filter('theme_body_open'); ?>
+		<div id="main">
+			<div id="gallerytitle">
+				<h2>
+					<span>
+						<?php printHomeLink('', ' | '); ?><a href="<?php echo html_encode(getGalleryIndexURL()); ?>" title="<?php echo gettext('Gallery Index'); ?>"><?php printGalleryTitle(); ?></a>
+					</span> | <?php echo gettext("Object not found"); ?>
+				</h2>
+			</div>
+			<div id="padbox">
+				<?php
+				echo gettext("The Zenphoto object you are requesting cannot be found.");
+				if (isset($album)) {
+					echo '<br />' . sprintf(gettext('Album: %s'), html_encode($album));
+				}
+				if (isset($image)) {
+					echo '<br />' . sprintf(gettext('Image: %s'), html_encode($image));
+				}
+				if (isset($obj)) {
+					echo '<br />' . sprintf(gettext('Page: %s'), html_encode(substr(basename($obj), 0, -4)));
+				}
+				?>
+			</div>
+		</div>
+		<div id="credit">
+			<?php printZenphotoLink(); ?>
+		</div>
+		<?php
+		zp_apply_filter('theme_body_close');
+		?>
+	</body>
+</html>
