f988438a9e75fb09588adb3ec8972c48e5eacb01
Stephen Billard <stephen@sbillard.org>
2013-10-03 11:01:59 -0700

    fix detection of closed site

--- index.php
+++ index.php
@@ -1,16 +1,23 @@
 <?php
+
 define('OFFSET_PATH', 0);
-eval(@file_get_contents(dirname(__FILE__).'/zp-data/zenphoto.cfg'));
-if (@$_zp_conf_vars['site_upgrade_state']=='closed') {
-	if (isset($_zp_conf_vars['special_pages']['page']['rewrite'])) {
-		$page = $_zp_conf_vars['special_pages']['page']['rewrite'];
-	} else {
-		$page = 'page';
-	}
-	if (!preg_match('~'.preg_quote($page).'/setup_set-mod_rewrite\?z=setup$~', $_SERVER['REQUEST_URI'])) {
-		header('location: plugins/site_upgrade/closed.php');
-		exit();
+$_contents = @file_get_contents(dirname(__FILE__) . '/zp-data/zenphoto.cfg');
+if ($_contents) {
+	if (strpos($_contents, '<?php') !== false)
+		$_contents = '?>' . $_contents;
+	@eval($_contents);
+	if (@$_zp_conf_vars['site_upgrade_state'] == 'closed') {
+		if (isset($_zp_conf_vars['special_pages']['page']['rewrite'])) {
+			$page = $_zp_conf_vars['special_pages']['page']['rewrite'];
+		} else {
+			$page = 'page';
+		}
+		if (!preg_match('~' . preg_quote($page) . '/setup_set-mod_rewrite\?z=setup$~', $_SERVER['REQUEST_URI'])) {
+			header('location: plugins/site_upgrade/closed.php');
+			exit();
+		}
 	}
 }
-include (dirname(__FILE__).'/zp-core/index.php');
+unset($_contents);
+include (dirname(__FILE__) . '/zp-core/index.php');
 ?>
