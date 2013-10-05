dc5632c1f50361d3c8bc4ab72941bdbb3201ae87
Stephen Billard <stephen@sbillard.org>
2013-10-05 10:08:22 -0700

    Handle sub-locales
    NOTE: it appears that the TinyMCE configuration files will override this
    code. That needs to be changed.

--- zp-core/zp-extensions/tinymce4.php
+++ zp-core/zp-extensions/tinymce4.php
@@ -1,23 +1,24 @@
 <?php
+
 /**
- * Back-end <i>WYSIWYG</i> editor TinyMCE 4.x. 
+ * Back-end <i>WYSIWYG</i> editor TinyMCE 4.x.
  *
  * You can place your own additional custom configuration files within
  * <var>%USER_PLUGIN_FOLDER%/tiny_mce/config</var>
  * There is a naming convention since there is a difference between Zenphoto (gallery) and Zenpag (news/pages) editor configurations
  * - zenphoto-<yourcustomname>.js.php
  * - zenpagae-<yourcustomname>.js.php
- * 
+ *
  * @author Malte Müller (acrylian)
  * @package plugins
  * @subpackage tools
  */
-$plugin_is_filter = 5|ADMIN_PLUGIN;
+$plugin_is_filter = 5 | ADMIN_PLUGIN;
 $plugin_description = gettext("Text editor TinyMCE 4.x");
 $plugin_author = "Malte Müller (acrylian)";
 $option_interface = 'tinymce4Options';
 
-zp_register_filter('texteditor_config','tinymce4ConfigJS');
+zp_register_filter('texteditor_config', 'tinymce4ConfigJS');
 
 /**
  * Plugin option handling class
@@ -33,17 +34,17 @@ class tinymce4Options {
 	function getOptionsSupported() {
 		$configs_zenpage = getTinyMCE4ConfigFiles('zenpage');
 		$configs_zenphoto = getTinyMCE4ConfigFiles('zenphoto');
-		$options = array(gettext('Text editor configuration - Zenphoto') => array('key' => 'tinymce4_zenphoto', 'type' => OPTION_TYPE_SELECTOR,
-										'order'=>0,
-										'selections' => $configs_zenphoto,
+		$options = array(gettext('Text editor configuration - Zenphoto')	 => array('key'						 => 'tinymce4_zenphoto', 'type'					 => OPTION_TYPE_SELECTOR,
+										'order'					 => 0,
+										'selections'		 => $configs_zenphoto,
 										'null_selection' => gettext('Disabled'),
-										'desc' => gettext('Applies to <em>admin</em> editable text other than for Zenpage pages and news articles.')),
-										gettext('Text editor configuration - Zenpage') => array('key' => 'tinymce4_zenpage', 'type' => OPTION_TYPE_SELECTOR,
-										'order'=>0,
-										'selections' => $configs_zenpage,
+										'desc'					 => gettext('Applies to <em>admin</em> editable text other than for Zenpage pages and news articles.')),
+						gettext('Text editor configuration - Zenpage')	 => array('key'						 => 'tinymce4_zenpage', 'type'					 => OPTION_TYPE_SELECTOR,
+										'order'					 => 0,
+										'selections'		 => $configs_zenpage,
 										'null_selection' => gettext('Disabled'),
-										'desc' => gettext('Applies to editing on the Zenpage <em>pages</em> and <em>news</em> tabs.')),
-										gettext('Custom image size')	=> array('key'		 => 'tinymce_tinyzenpage_customimagesize', 'type'	 => OPTION_TYPE_TEXTBOX,
+										'desc'					 => gettext('Applies to editing on the Zenpage <em>pages</em> and <em>news</em> tabs.')),
+						gettext('Custom image size')										 => array('key'		 => 'tinymce_tinyzenpage_customimagesize', 'type'	 => OPTION_TYPE_TEXTBOX,
 										'order'	 => 2,
 										'desc'	 => gettext("Predefined size (px) for custom size images included using tinyZenpage.")),
 						gettext('Custom image size')										 => array('key'		 => 'tinymce_tinyzenpage_customimagesize', 'type'	 => OPTION_TYPE_TEXTBOX,
@@ -72,19 +73,28 @@ class tinymce4Options {
 	}
 
 	function handleOption($option, $currentValue) {
+
 	}
+
 }
 
-
-function tinymce4ConfigJS($editorconfig,$mode) {
-	if (empty($editorconfig)) {	// only if we get here first!
-		$locale = substr(getOption("locale"),0,2);
-		if (empty($locale)) {
-			$locale = 'en';
+function tinymce4ConfigJS($editorconfig, $mode) {
+	if (empty($editorconfig)) { // only if we get here first!
+		$locale = 'en';
+		$loc = str_replace('_', '-', getOption("locale"));
+		if ($loc) {
+			if (file_exists(SERVERPATH . '/' . PLUGIN_FOLDER . '/tiny_mce/langs/' . $loc . '.js')) {
+				$locale = $loc;
+			} else {
+				$loc = substr($loc, 0, 2);
+				if (file_exists(SERVERPATH . '/' . PLUGIN_FOLDER . '/tiny_mce/langs/' . $loc . '.js')) {
+					$locale = $loc;
+				}
+			}
 		}
-		$editorconfig = getOption('tinymce4_'.$mode);
+		$editorconfig = getOption('tinymce4_' . $mode);
 		if (!empty($editorconfig)) {
-			$editorconfig = getPlugin('/tinymce4/config/'.$editorconfig);
+			$editorconfig = getPlugin('/tinymce4/config/' . $editorconfig);
 			if (!empty($editorconfig)) {
 				require_once($editorconfig);
 			}
@@ -93,24 +103,23 @@ function tinymce4ConfigJS($editorconfig,$mode) {
 	return $editorconfig;
 }
 
-
 function getTinyMCE4ConfigFiles($mode) {
 	// get only those that work!
-	switch($mode) {
+	switch ($mode) {
 		case 'zenphoto':
-			$files = getPluginFiles('zenphoto-*.js.php','tinymce4/config/');
+			$files = getPluginFiles('zenphoto-*.js.php', 'tinymce4/config/');
 			break;
 		case 'zenpage':
-			$files = getPluginFiles('zenpage-*.js.php','tinymce4/config/');
+			$files = getPluginFiles('zenpage-*.js.php', 'tinymce4/config/');
 			break;
 	}
 	$array = array();
-	foreach($files as $file) {
-		$filename = strrchr($file,'/');
+	foreach ($files as $file) {
+		$filename = strrchr($file, '/');
 		$filename = substr($filename, 1);
-		$array[ucfirst(substr($filename,0,strpos($filename, '.js.php')))] = $filename;
+		$array[ucfirst(substr($filename, 0, strpos($filename, '.js.php')))] = $filename;
 	}
-	return $array;	
+	return $array;
 }
 
 ?>
