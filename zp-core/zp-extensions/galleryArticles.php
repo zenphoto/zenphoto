800b1504679208d3e5605cec152ca40606080a3e
Stephen Billard <stephen@sbillard.org>
2013-11-07 17:15:21 -0800

    Plugin to create news articles when an album or image is published
    #574

---
+++ zp-core/zp-extensions/galleryArticles.php
@@ -0,0 +1,183 @@
+<?php
+
+/**
+ *
+ * @package plugins
+ * @author Stephen Billard (sbillard)
+ * @subpackage tools
+ */
+$plugin_is_filter = 9 | THEME_PLUGIN | ADMIN_PLUGIN;
+$plugin_description = gettext('Create news articles when a gallery item is published.');
+$plugin_author = "Stephen Billard (sbillard)";
+
+$option_interface = 'galleryArticles';
+
+zp_register_filter('show_change', 'galleryArticles::published');
+if (getOption('galleryArticles_albums'))
+	zp_register_filter('new_album', 'galleryArticles::published');
+if (getOption('galleryArticles_images'))
+	zp_register_filter('new_image', 'galleryArticles::published');
+zp_register_filter('admin_head', 'galleryArticles::scan');
+zp_register_filter('load_theme_script', 'galleryArticles::scan');
+
+/**
+ *
+ * Standard options interface
+ * @author Stephen
+ *
+ */
+class galleryArticles {
+
+	function __construct() {
+		setOptionDefault('galleryArticles_images', NULL);
+		setOptionDefault('galleryArticles_albums', NULL);
+		setOptionDefault('galleryArticles_category', NULL);
+	}
+
+	/**
+	 *
+	 * supported options
+	 */
+	function getOptionsSupported() {
+		global $_zp_zenpage;
+		$categories = array();
+		$list = $_zp_zenpage->getAllCategories();
+		foreach ($list as $cat) {
+			$categories[$cat['title']] = $cat['titlelink'];
+		}
+
+		$list = array('<em>' . gettext('Albums') . '</em>' => 'galleryArticles_albums', '<em>' . gettext('Images') . '</em>' => 'galleryArticles_images');
+
+		$options = array(gettext('Publish for') => array('key'				 => 'galleryArticles_items', 'type'			 => OPTION_TYPE_CHECKBOX_ARRAY,
+										'checkboxes' => $list,
+										'desc'			 => gettext('If a <em>type</em> is checked, a news article will be made when an object of that <em>type</em> is published.')),
+						gettext('Category')		 => array('key'				 => 'galleryArticles_category', 'type'			 => OPTION_TYPE_SELECTOR,
+										'selections' => $categories,
+										'desc'			 => gettext('Select a category for the generated articles'))
+		);
+
+		return $options;
+	}
+
+	/**
+	 *
+	 * place holder
+	 * @param string $option
+	 * @param mixed $currentValue
+	 */
+	function handleOption($option, $currentValue) {
+
+	}
+
+	/**
+	 *
+	 * filter for the setShow() methods
+	 * @param object $obj
+	 */
+	static function published($obj) {
+		self::publishArticlesWithCheck($obj);
+		return $obj;
+	}
+
+	/**
+	 *
+	 * used by the filters to decide if to publish gallery Articles
+	 * @param object $obj
+	 */
+	private static function publishArticlesWithCheck($obj) {
+		$type = $obj->table;
+		if (getOption('galleryArticles_' . $type)) {
+			if ($obj->getShow()) {
+				if (getOption('galleryArticles_protected') || !$obj->isProtected()) {
+					switch ($type = $obj->table) {
+
+						case 'albums':
+							$dt = $obj->getPublishDate();
+							if ($dt > date('Y-m-d H:i:s')) {
+								$result = query_single_row('SELECT * FROM ' . prefix('plugin_storage') . ' WHERE `type`="galleryArticles" AND `aux`="pending_albums" AND `data`=' . db_quote($obj->name));
+								if (!$result) {
+									query('INSERT INTO ' . prefix('plugin_storage') . ' (`type`,`aux`,`data`) VALUES ("galleryArticles","pending_albums",' . db_quote($obj->name) . ')');
+								}
+							} else {
+								self::publishArticle($obj);
+							}
+							break;
+						case 'images':
+							$dt = $obj->getPublishDate();
+							if ($dt > date('Y-m-d H:i:s')) {
+								$result = query_single_row('SELECT * FROM ' . prefix('plugin_storage') . ' WHERE `type`="galleryArticles" AND `aux`="pending_images" AND `data`=' . db_quote($obj->imagefolder . '/' . $obj->filename));
+								if (!$result) {
+									query('INSERT INTO ' . prefix('plugin_storage') . ' (`type`,`aux`,`data`) VALUES ("galleryArticles","pending_images",' . db_quote($obj->imagefolder . '/' . $obj->filename) . ')');
+								}
+							} else {
+								self::publishArticle($obj);
+							}
+							break;
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 *
+	 * Formats the message and calls sendTweet() on an object
+	 * @param object $obj
+	 */
+	private static function publishArticle($obj) {
+		switch ($type = $obj->table) {
+			case 'albums':
+				$text = sprintf(gettext('New album: %1$s'), $item = $obj->getTitle());
+				$link = $obj->getAlbumLink();
+				$thumb = $obj->getAlbumThumb();
+				break;
+			case 'images':
+				$text = sprintf(gettext('New image: [%2$s]%1$s'), $item = $obj->getTitle(), $obj->imagefolder);
+				$link = $obj->getImageLink();
+				$thumb = $obj->getThumb();
+				break;
+		}
+		$article = new ZenpageNews(seoFriendly($text));
+		$article->setTitle($text);
+		$article->setContent('<p><a href="' . $link . '"><img src="' . $thumb . '"></p>');
+		$article->setShow(true);
+		$article->setDateTime(date('Y-m-d H:i:s'));
+		$article->setAuthor('galleryArticles');
+		$article->save();
+		$article->setCategories(array(getOption('galleryArticles_category')));
+	}
+
+	/**
+	 *
+	 * filter which checks if there are any matured items to be sent
+	 * @param string $script
+	 * @param bool $valid will be false if the object is not found (e.g. there will be a 404 error);
+	 * @return string
+	 */
+	static function scan($script, $valid = true) {
+		if ($script && $valid) {
+
+			$result = query_full_array('SELECT * FROM ' . prefix('albums') . ' AS album,' . prefix('plugin_storage') . ' AS store WHERE store.type="galleryArticles" AND store.aux="pending_albums" AND store.data = album.folder AND album.date <= ' . db_quote(date('Y-m-d H:i:s')));
+			if ($result) {
+				foreach ($result as $album) {
+					query('DELETE FROM ' . prefix('plugin_storage') . ' WHERE `id`=' . $album['id']);
+					$album = newAlbum($album['folder']);
+					self::publishArticle($album);
+				}
+			}
+			$result = query_full_array('SELECT * FROM ' . prefix('images') . ' AS image,' . prefix('plugin_storage') . ' AS store WHERE store.type="galleryArticles" AND store.aux="pending_images" AND store.data LIKE image.filename AND image.date <= ' . db_quote(date('Y-m-d H:i:s')));
+			if ($result) {
+				foreach ($result as $image) {
+					query('DELETE FROM ' . prefix('plugin_storage') . ' WHERE `id`=' . $image['id']);
+					$album = query_single_row('SELECT * FROM ' . prefix('albums') . ' WHERE `id`=' . $image['albumid']);
+					$album = newAlbum($album['folder']);
+					$image = newImage($album, $image['filename']);
+					self::publishArticle($image);
+				}
+			}
+		}
+		return $script;
+	}
+
+}
+?>
--- zp-core/zp-extensions/zenpage/zenpage-class-news.php
+++ zp-core/zp-extensions/zenpage/zenpage-class-news.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * zenpage news class
  *
@@ -6,11 +7,10 @@
  * @package plugins
  * @subpackage zenpage
  */
-
-if (!defined('NEWS_POSITION_NORMAL')) {	// No idea why this is needed, but clones get already defined errors.
-	define('NEWS_POSITION_NORMAL',0);
-	define('NEWS_POSITION_STICKY',1);
-	define('NEWS_POSITION_STICK_TO_TOP',9);
+if (!defined('NEWS_POSITION_NORMAL')) { // No idea why this is needed, but clones get already defined errors.
+	define('NEWS_POSITION_NORMAL', 0);
+	define('NEWS_POSITION_STICKY', 1);
+	define('NEWS_POSITION_STICK_TO_TOP', 9);
 }
 
 class ZenpageNews extends ZenpageItems {
@@ -21,11 +21,11 @@ class ZenpageNews extends ZenpageItems {
 	var $categories = NULL;
 	var $index = NULL;
 
-	function __construct($titlelink, $allowCreate=NULL) {
+	function __construct($titlelink, $allowCreate = NULL) {
 		if (is_array($titlelink)) {
 			$titlelink = $titlelink['titlelink'];
 		}
-		$new = parent::PersistentObject('news', array('titlelink'=>$titlelink), 'titlelink', true, empty($titlelink), $allowCreate);
+		$new = parent::PersistentObject('news', array('titlelink' => $titlelink), 'titlelink', true, empty($titlelink), $allowCreate);
 	}
 
 	/**
@@ -36,23 +36,25 @@ class ZenpageNews extends ZenpageItems {
 	 */
 	function getCategories() {
 		if (is_null($this->categories)) {
-			$this->categories = query_full_array("SELECT * FROM ".prefix('news_categories')." as cat,".prefix('news2cat')." as newscat WHERE newscat.cat_id = cat.id AND newscat.news_id = ".$this->getID()." ORDER BY cat.titlelink",false,'title');
+			$this->categories = query_full_array("SELECT * FROM " . prefix('news_categories') . " as cat," . prefix('news2cat') . " as newscat WHERE newscat.cat_id = cat.id AND newscat.news_id = " . $this->getID() . " ORDER BY cat.titlelink", false, 'title');
 			if (!$this->categories) {
 				$this->categories = array();
 			}
 		}
 		return $this->categories;
 	}
+
 	function setCategories($categories) {
-		$result = query('DELETE FROM '.prefix('news2cat').' WHERE `news_id`='.$this->getID());
-		$result = query_full_array("SELECT * FROM ".prefix('news_categories')." ORDER BY titlelink");
+		$result = query('DELETE FROM ' . prefix('news2cat') . ' WHERE `news_id`=' . $this->getID());
+		$result = query_full_array("SELECT * FROM " . prefix('news_categories') . " ORDER BY titlelink");
 		foreach ($result as $cat) {
-			if (in_array($cat['titlelink'],$categories)) {
-				query("INSERT INTO ".prefix('news2cat')." (cat_id, news_id) VALUES ('".$cat['id']."', '".$this->getID()."')");
+			if (in_array($cat['titlelink'], $categories)) {
+				query("INSERT INTO " . prefix('news2cat') . " (cat_id, news_id) VALUES ('" . $cat['id'] . "', '" . $this->getID() . "')");
 			}
 		}
 	}
-/**
+
+	/**
 	 * Returns true if the article is sticky
 	 *
 	 * @return bool
@@ -60,14 +62,17 @@ class ZenpageNews extends ZenpageItems {
 	function getSticky() {
 		return $this->get('sticky');
 	}
+
 	function setSticky($v) {
-		$this->set('sticky',$v);
+		$this->set('sticky', $v);
 	}
+
 	function getTruncation() {
 		return $this->get('truncation');
 	}
+
 	function setTruncation($v) {
-		$this->set('truncation',$v);
+		$this->set('truncation', $v);
 	}
 
 	/**
@@ -76,10 +81,10 @@ class ZenpageNews extends ZenpageItems {
 	 */
 	function copy($newtitle) {
 		$newID = $newtitle;
-		$id = parent::copy(array('titlelink'=>$newID));
+		$id = parent::copy(array('titlelink' => $newID));
 		if (!$id) {
-			$newID = $newtitle.':'.seoFriendly(date('Y-m-d_H-i-s'));
-			$id = parent::copy(array('titlelink'=>$newID));
+			$newID = $newtitle . ':' . seoFriendly(date('Y-m-d_H-i-s'));
+			$id = parent::copy(array('titlelink' => $newID));
 		}
 		if ($id) {
 			$newobj = new ZenpageNews($newID);
@@ -92,10 +97,10 @@ class ZenpageNews extends ZenpageItems {
 			foreach ($this->getCategories() as $cat) {
 				$categories[] = $cat['cat_id'];
 			}
-			$result = query_full_array("SELECT * FROM ".prefix('news_categories')." ORDER BY titlelink");
+			$result = query_full_array("SELECT * FROM " . prefix('news_categories') . " ORDER BY titlelink");
 			foreach ($result as $cat) {
-				if (in_array($cat['id'],$categories)) {
-					query("INSERT INTO ".prefix('news2cat')." (cat_id, news_id) VALUES ('".$cat['id']."', '".$id."')");
+				if (in_array($cat['id'], $categories)) {
+					query("INSERT INTO " . prefix('news2cat') . " (cat_id, news_id) VALUES ('" . $cat['id'] . "', '" . $id . "')");
 				}
 			}
 			return $newobj;
@@ -103,39 +108,41 @@ class ZenpageNews extends ZenpageItems {
 		return false;
 	}
 
-/**
- * Deletes an news article from the database
- *
- */
+	/**
+	 * Deletes an news article from the database
+	 *
+	 */
 	function remove() {
 		if ($success = parent::remove()) {
 			if ($this->id) {
 				$success = query("DELETE FROM " . prefix('obj_to_tag') . "WHERE `type`='news' AND `objectid`=" . $this->getID());
-				$success = $success && query("DELETE FROM ".prefix('news2cat')." WHERE news_id = ".$this->getID()); // delete the category association
-				$success = $success && query("DELETE FROM ".prefix('comments')." WHERE ownerid = ".$this->getID().' AND type="news"'); // delete any comments
+				$success = $success && query("DELETE FROM " . prefix('news2cat') . " WHERE news_id = " . $this->getID()); // delete the category association
+				$success = $success && query("DELETE FROM " . prefix('comments') . " WHERE ownerid = " . $this->getID() . ' AND type="news"'); // delete any comments
 			}
 		}
 		return $success;
 	}
 
-/**
- * Checks if an article (not CombiNews gallery items!) is in a password protected category and returns TRUE or FALSE
- * NOTE: This function does not check if the password has been entered! Use checkAccess() for that.
- *
- * @param bool $only set to true to know if the news article belongs only to protected categories (i.e. it is protected)
- *
- * @return array
- */
-	function inProtectedCategory($only=false) {
+	/**
+	 * Checks if an article (not CombiNews gallery items!) is in a password protected category and returns TRUE or FALSE
+	 * NOTE: This function does not check if the password has been entered! Use checkAccess() for that.
+	 *
+	 * @param bool $only set to true to know if the news article belongs only to protected categories (i.e. it is protected)
+	 *
+	 * @return array
+	 */
+	function inProtectedCategory($only = false) {
 		$categories = $this->getCategories();
-		if(!empty($categories)) {
-			foreach($categories as $cat) {
+		if (!empty($categories)) {
+			foreach ($categories as $cat) {
 				$catobj = new ZenpageCategory($cat['titlelink']);
 				$password = $catobj->getPassword();
-				if(!empty($password)) {
-					if (!$only) return true;
+				if (!empty($password)) {
+					if (!$only)
+						return true;
 				} else {
-					if ($only) return false;
+					if ($only)
+						return false;
 				}
 			}
 			return $only;
@@ -155,11 +162,12 @@ class ZenpageNews extends ZenpageItems {
 	 * returns true if the article exists in any published category (or in no categories)
 	 */
 	function categoryIsVisible() {
-		if (zp_loggedin(ALL_NEWS_RIGHTS)) return true;
+		if (zp_loggedin(ALL_NEWS_RIGHTS))
+			return true;
 		global $_zp_zenpage;
 		$categories = $this->getCategories(false);
-		if(count($categories) > 0) {
-			foreach($categories as $cat) {
+		if (count($categories) > 0) {
+			foreach ($categories as $cat) {
 				if ($_zp_zenpage->visibleCategory($cat)) {
 					return true;
 				}
@@ -175,12 +183,12 @@ class ZenpageNews extends ZenpageItems {
 	 * @param $hint
 	 * @param $show
 	 */
-	function checkforGuest(&$hint=NULL, &$show=NULL) {
+	function checkforGuest(&$hint = NULL, &$show = NULL) {
 		if (!parent::checkForGuest()) {
 			return false;
 		}
 		$categories = $this->getCategories();
-		if (empty($categories)) {	//	cannot be protected!
+		if (empty($categories)) { //	cannot be protected!
 			return 'zp_public_access';
 		} else {
 			foreach ($categories as $cat) {
@@ -210,7 +218,7 @@ class ZenpageNews extends ZenpageItems {
 				return LIST_RIGHTS;
 			}
 			if ($_zp_current_admin_obj->getUser() == $this->getAuthor()) {
-				return true;	//	he is the author
+				return true; //	he is the author
 			}
 			if ($this->getShow() && $action == LIST_RIGHTS) {
 				return true;
@@ -219,7 +227,7 @@ class ZenpageNews extends ZenpageItems {
 			if (!empty($mycategories)) {
 				foreach ($this->getCategories() as $category) {
 					$cat = new ZenpageCategory($category['titlelink']);
-					if ($cat->isMyItem(ZENPAGE_NEWS_RIGHTS)) {	// only override item visibility if we "own" the category
+					if ($cat->isMyItem(ZENPAGE_NEWS_RIGHTS)) { // only override item visibility if we "own" the category
 						return true;
 					}
 				}
@@ -235,11 +243,11 @@ class ZenpageNews extends ZenpageItems {
 	 * @return bool
 	 */
 	function inNewsCategory($catlink) {
-		if(!empty($catlink)) {
+		if (!empty($catlink)) {
 			$categories = $this->getCategories();
 			$count = 0;
-			foreach($categories as $cat) {
-				if($catlink == $cat['titlelink']) {
+			foreach ($categories as $cat) {
+				if ($catlink == $cat['titlelink']) {
 					$count = 1;
 					break;
 				}
@@ -257,15 +265,15 @@ class ZenpageNews extends ZenpageItems {
 	 * @return bool
 	 */
 	function inSubNewsCategoryOf($catlink) {
-		if(!empty($catlink)) {
+		if (!empty($catlink)) {
 			$categories = $this->getCategories();
 			$count = 0;
-			foreach($categories as $cat) {
+			foreach ($categories as $cat) {
 				$catobj = new ZenpageCategory($cat['titlelink']);
 				$parentid = $catobj->getParentID();
 				$parentcats = $catobj->getParents();
-				foreach($parentcats as $parentcat) {
-					if($catlink == $parentcat) {
+				foreach ($parentcats as $parentcat) {
+					if ($catlink == $parentcat) {
 						$count = 1;
 						break;
 					}
@@ -278,29 +286,28 @@ class ZenpageNews extends ZenpageItems {
 	}
 
 	/**
-	* Returns the url to a news article
-	*
-	*
-	* @return string
-	*/
+	 * Returns the url to a news article
+	 *
+	 *
+	 * @return string
+	 */
 	function getNewsLink() {
 		global $_zp_zenpage;
 		return $_zp_zenpage->getNewsTitlePath($this->getTitlelink());
 	}
-
 
 	/**
 	 * Get the index of this article
 	 *
 	 * @return int
 	 */
-	function getIndex($sortorder,$sortdirection,$sticky) {
+	function getIndex($sortorder, $sortdirection, $sticky) {
 		global $_zp_zenpage, $_zp_current_zenpage_news;
-		if($this->index == NULL) {
-			$articles = $_zp_zenpage->getArticles(0,NULL,true,$sortorder,$sortdirection,$sticky);
-			for ($i=0; $i < count($articles); $i++) {
+		if ($this->index == NULL) {
+			$articles = $_zp_zenpage->getArticles(0, NULL, true, $sortorder, $sortdirection, $sticky);
+			for ($i = 0; $i < count($articles); $i++) {
 				$article = $articles[$i];
-				if($this->getTitlelink() == $article['titlelink']) {
+				if ($this->getTitlelink() == $article['titlelink']) {
 					$this->index = $i;
 					break;
 				}
@@ -314,10 +321,10 @@ class ZenpageNews extends ZenpageItems {
 	 *
 	 * @return object
 	 */
-	function getPrevArticle($sortorder='date',$sortdirection='desc',$sticky=true) {
+	function getPrevArticle($sortorder = 'date', $sortdirection = 'desc', $sticky = true) {
 		global $_zp_zenpage, $_zp_current_zenpage_news;
-		$index = $this->getIndex($sortorder,$sortdirection,$sticky);
-		$article = $_zp_zenpage->getArticle($index-1);
+		$index = $this->getIndex($sortorder, $sortdirection, $sticky);
+		$article = $_zp_zenpage->getArticle($index - 1);
 		return $article;
 	}
 
@@ -326,14 +333,14 @@ class ZenpageNews extends ZenpageItems {
 	 *
 	 * @return object
 	 */
-	function getNextArticle($sortorder='date',$sortdirection='desc',$sticky=true) {
+	function getNextArticle($sortorder = 'date', $sortdirection = 'desc', $sticky = true) {
 		global $_zp_zenpage, $_zp_current_zenpage_news;
-		$index = $this->getIndex($sortorder,$sortdirection,$sticky);
-		$article = $_zp_zenpage->getArticle($index+1);
+		$index = $this->getIndex($sortorder, $sortdirection, $sticky);
+		$article = $_zp_zenpage->getArticle($index + 1);
 		return $article;
 	}
 
-} // zenpage news class end
+}
 
-
+// zenpage news class end
 ?>
