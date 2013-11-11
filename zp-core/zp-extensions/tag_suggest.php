750beb2781102eb7056f52ff4520c7b9d31ef968
acrylian <wartgart@gmx.net>
2013-11-11 14:21:20 +0100

    tag_suggest now also covers the item edit page’s tag selectors

--- zp-core/zp-extensions/tag_suggest.php
+++ zp-core/zp-extensions/tag_suggest.php
@@ -3,15 +3,17 @@
  * Provide javaScript tag "suggestions" based on Remy Sharp's {@link http://remysharp.com/2007/12/28/jquery-tag-suggestion/ jQuery Tag Suggestion} plugin.
  *
  * Activate the plugin and the feature is available on the theme's search field.
+ * It is also available on each item edit page's tag selector.
  *
  * @author Malte Müller (acrylian), Stephen Billard (sbillard) - an adaption of Remy Sharp's jQuery Tag Suggestion
  * @package plugins
  */
-
+$plugin_is_filter = 9 | THEME_PLUGIN | ADMIN_PLUGIN;
 $plugin_description = gettext("Enables jQuery tag suggestions on the search field.");
 $plugin_author = "Malte Müller (acrylian), Stephen Billard (sbillard)";
 
 zp_register_filter('theme_head','tagSuggestJS');
+zp_register_filter('admin_head','tagSuggestJS');
 
 function tagSuggestJS() {
 	// the scripts needed
@@ -24,6 +26,7 @@ function tagSuggestJS() {
 	<link type="text/css" rel="stylesheet" href="<?php echo pathurlencode($css); ?>" />
 	<?php
 	$taglist = getAllTagsUnique();
+	
 	$c = 0;
 	$list = '';
 	foreach ($taglist AS $tag) {
@@ -31,12 +34,21 @@ function tagSuggestJS() {
 		$c++;
 		$list .= '"'.addslashes($tag).'"';
 	}
+	if(OFFSET_PATH == 4) {
+		$tagseparator = ' ';
+	} else {
+		if(getOption('search_space_is') == 'OR') {
+			$tagseparator = ' '; 
+		} else {
+			$tagseparator = ',';
+		}
+	}
 	?>
 	<script type="text/javascript">
 		// <!-- <![CDATA[
 		var _tagList = [<?php  echo $list; ?>];
 		$(function () {
-			$('#search_input, #edit-editable_4').tagSuggest({ separator:'<?php  echo (getOption('search_space_is')=='OR'?' ':','); ?>', tags: _tagList })
+			$('#search_input, #edit-editable_4, #newtag_tags_').tagSuggest({ separator:'<?php echo $tagseparator; ?>', tags: _tagList })
 			});
 		// ]]> -->
 	</script>
