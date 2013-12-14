66d76e636f9b384bf717eb9c1ef0902e8693d98e
Stephen Billard <stephen@sbillard.org>
2013-12-14 09:33:18 -0800

    fix #599

--- zp-core/zp-extensions/elFinder.php
+++ zp-core/zp-extensions/elFinder.php
@@ -21,8 +21,7 @@
  * @package plugins
  * @subpackage uploader
  */
-
-$plugin_is_filter = 5|ADMIN_PLUGIN;
+$plugin_is_filter = 5 | ADMIN_PLUGIN;
 $plugin_description = gettext('Provides file handling for the <code>upload/files</code> tab and the <em>TinyMCE</em> file browser.');
 $plugin_author = "Stephen Billard (sbillard)";
 
@@ -33,6 +32,7 @@ $option_interface = 'elFinder_options';
  *
  */
 class elFinder_options {
+
 	/**
 	 * class instantiation function
 	 *
@@ -42,22 +42,22 @@ class elFinder_options {
 		setOptionDefault('elFinder_tinymce', 0);
 	}
 
-
 	/**
 	 * Reports the supported options
 	 *
 	 * @return array
 	 */
 	function getOptionsSupported() {
-		$options = array(	gettext('Files tab') => array('key' => 'elFinder_files', 'type' => OPTION_TYPE_CHECKBOX,
-																											'desc' => gettext('Use as the upload <em>files</em> subtab.')),
-											gettext('TinyMCE plugin') => array('key' => 'elFinder_tinymce', 'type' => OPTION_TYPE_CHECKBOX,
-																											'desc' => gettext('Enable plugin for TinyMCE.'))
+		$options = array(gettext('Files tab')			 => array('key'	 => 'elFinder_files', 'type' => OPTION_TYPE_CHECKBOX,
+										'desc' => gettext('Use as the upload <em>files</em> subtab.')),
+						gettext('TinyMCE plugin')	 => array('key'	 => 'elFinder_tinymce', 'type' => OPTION_TYPE_CHECKBOX,
+										'desc' => gettext('Enable plugin for TinyMCE.'))
 		);
 		return $options;
 	}
 
 	function handleOption($option, $currentValue) {
+
 	}
 
 }
@@ -70,19 +70,19 @@ if (getOption('elFinder_tinymce')) {
 }
 
 function elFinder_admin_tabs($tabs) {
-	$me = sprintf(gettext('files (%s)'),'elFinder');
-	$mylink = PLUGIN_FOLDER.'/'.'elFinder/filemanager.php?page=upload&amp;tab='.$me;
+	$me = sprintf(gettext('files (%s)'), 'elFinder');
+	$mylink = PLUGIN_FOLDER . '/' . 'elFinder/filemanager.php?page=upload&amp;tab=' . $me;
 	if (is_null($tabs['upload'])) {
-		$tabs['upload'] =  array('text'=>gettext("upload"),
-				'link'=>WEBPATH."/".ZENFOLDER.'/'.$mylink,
-				'subtabs'=>NULL);
+		$tabs['upload'] = array('text'		 => gettext("upload"),
+						'link'		 => WEBPATH . "/" . ZENFOLDER . '/' . $mylink,
+						'subtabs'	 => NULL);
 	} else {
-		$default = str_replace(WEBPATH.'/'.ZENFOLDER.'/','',$tabs['upload']['link']);
+		$default = str_replace(WEBPATH . '/' . ZENFOLDER . '/', '', $tabs['upload']['link']);
 		preg_match('|&amp;tab=([^&]*)|', $default, $matches);
 		$tabs['upload']['subtabs'][$matches[1]] = $default;
 		$tabs['upload']['subtabs'][$me] = $mylink;
-		$tabs['upload']['default']= $me;
-		$tabs['upload']['link'] = WEBPATH."/".ZENFOLDER.'/'.$mylink;
+		$tabs['upload']['default'] = $me;
+		$tabs['upload']['link'] = WEBPATH . "/" . ZENFOLDER . '/' . $mylink;
 	}
 	return $tabs;
 }
@@ -90,28 +90,27 @@ function elFinder_admin_tabs($tabs) {
 function elFinder_tinymce($discard) {
 	?>
 	<script type="text/javascript">
-	// <!-- <![CDATA[
-	function elFinderBrowser (field_name, url, type, win) {
-	  var elfinder_url = '<?php echo FULLWEBPATH.'/'.ZENFOLDER.'/'.PLUGIN_FOLDER; ?>/elfinder/elfinder.php?XSRFToken=<?php echo getXSRFToken('elFinder'); ?>';    // use an absolute path!
-	  tinyMCE.activeEditor.windowManager.open({
-	    file: elfinder_url,
-	    title: 'elFinder 2.0',
-	    width: 900,
-	    height: 450,
-	    resizable: 'yes',
-	    inline: 'yes',    // This parameter only has an effect if you use the inlinepopups plugin!
-	    popup_css: false, // Disable TinyMCE's default popup CSS
-	    close_previous: 'no'
-	  }, {
-	    window: win,
-	    input: field_name
-	  });
-	  return false;
-	}
-	// ]]> -->
+		// <!-- <![CDATA[
+		function elFinderBrowser(field_name, url, type, win) {
+			var elfinder_url = '<?php echo FULLWEBPATH . '/' . ZENFOLDER . '/' . PLUGIN_FOLDER; ?>/elFinder/elfinder.php?XSRFToken=<?php echo getXSRFToken('elFinder'); ?>';    // use an absolute path!
+					tinyMCE.activeEditor.windowManager.open({
+						file: elfinder_url,
+						title: 'elFinder 2.0',
+						width: 900,
+						height: 450,
+						resizable: 'yes',
+						inline: 'yes', // This parameter only has an effect if you use the inlinepopups plugin!
+						popup_css: false, // Disable TinyMCE's default popup CSS
+						close_previous: 'no'
+					}, {
+						window: win,
+						input: field_name
+					});
+					return false;
+				}
+				// ]]> -->
 	</script>
 	<?php
 	return 'elFinderBrowser';
 }
-
 ?>
