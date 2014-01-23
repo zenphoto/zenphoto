12ad1f92fadb2cc73c3b240e2468023af4a17157
Stephen Billard <stephen@sbillard.org>
2014-01-23 15:01:35 -0800

    Version=>1.4.5.9

--- zp-core/version.php
+++ zp-core/version.php
@@ -1,6 +1,6 @@
 <?php
 
-define('ZENPHOTO_VERSION', '1.4.5.8');
+define('ZENPHOTO_VERSION', '1.4.5.9');
 define('ZENPHOTO_FULL_RELEASE', trim(file_get_contents(dirname(__FILE__) . '/githead')));
 define('ZENPHOTO_RELEASE', substr(ZENPHOTO_FULL_RELEASE, 0, 10));
 ?>
