77d6c3ec3516f8c655dbfc8d4108ec2c9b51ddcb
fretzl <f.sondaar@kpnmail.nl>
2014-01-26 19:50:36 +0100

    bxslider_thumb_nav some typo’s and cleanup

--- zp-core/zp-extensions/bxslider_thumb_nav.php
+++ zp-core/zp-extensions/bxslider_thumb_nav.php
@@ -42,36 +42,44 @@ class bxslider {
 	function getOptionsSupported() {
 		global $_zp_gallery;
 		$options = array(
-						gettext('Minimum items')	 => array('key'	 => 'bxslider_minitems', 'type' => OPTION_TYPE_TEXTBOX,
-										'desc' => gettext("The minimum number of slides to be shown. Slides will be sized down if carousel becomes smaller than the original size.")),
-						gettext('Maximum items')	 => array('key'	 => 'bxslider_maxitems', 'type' => OPTION_TYPE_TEXTBOX,
-										'desc' => gettext("The maximum number of slides to be shown. Slides will be sized up if carousel becomes larger than the original size.")),
-						gettext('width')					 => array('key'	 => 'bxslider_width', 'type' => OPTION_TYPE_TEXTBOX,
-										'desc' => gettext("Width of the thumb. Note that the CSS might need to be adjusted.")),
-						gettext('height')					 => array('key'	 => 'bxslider_height', 'type' => OPTION_TYPE_TEXTBOX,
-										'desc' => gettext("Height of the thumb. Note that the CSS might need to be adjusted.")),
-						gettext('Crop width')			 => array('key'	 => 'bxslider_cropw', 'type' => OPTION_TYPE_TEXTBOX,
-										'desc' => ""),
-						gettext('Crop height')		 => array('key'	 => 'bxslider_croph', 'type' => OPTION_TYPE_TEXTBOX,
-										'desc' => ""),
-						gettext('Speed')					 => array('key'	 => 'bxslider_speed', 'type' => OPTION_TYPE_TEXTBOX,
-										'desc' => gettext("The speed in miliseconds the slides advance when clicked.)")),
-						gettext('Full image link') => array('key'	 => 'bxslider_fullimagelink', 'type' => OPTION_TYPE_CHECKBOX,
-										'desc' => gettext("If checked the thumbs link to the full image instead of the image page.")),
-						gettext('Mode')						 => array('key'				 => 'bxslider_mode', 'type'			 => OPTION_TYPE_SELECTOR,
-										'order'			 => 2,
+						gettext('Minimum items') => array('key' => 'bxslider_minitems', 'type' => OPTION_TYPE_TEXTBOX,
+										'desc' => gettext("The minimum number of slides to be shown. Slides will be sized down if carousel becomes smaller than the original size."),
+										'order'	=> 1),
+						gettext('Maximum items') => array('key' => 'bxslider_maxitems', 'type' => OPTION_TYPE_TEXTBOX,
+										'desc' => gettext("The maximum number of slides to be shown. Slides will be sized up if carousel becomes larger than the original size."),
+										'order'	=> 2),
+						gettext('Width')		=> array('key' => 'bxslider_width', 'type' => OPTION_TYPE_TEXTBOX,
+										'desc' => gettext("Width of the thumb. Note that the CSS might need to be adjusted."),
+										'order'	=> 3),
+						gettext('Height')		=> array('key' => 'bxslider_height', 'type' => OPTION_TYPE_TEXTBOX,
+										'desc' => gettext("Height of the thumb. Note that the CSS might need to be adjusted."),
+										'order'	=> 4),
+						gettext('Crop width')	=> array('key' => 'bxslider_cropw', 'type' => OPTION_TYPE_TEXTBOX,
+										'desc' => "",
+										'order'	=> 5),
+						gettext('Crop height')	=> array('key' => 'bxslider_croph', 'type' => OPTION_TYPE_TEXTBOX,
+										'desc' => "",
+										'order'	=> 6),
+						gettext('Speed')		=> array('key' => 'bxslider_speed', 'type' => OPTION_TYPE_TEXTBOX,
+										'desc' => gettext("The speed in miliseconds the slides advance when clicked.)"),
+										'order'	=> 7),
+						gettext('Full image link')	=> array('key' => 'bxslider_fullimagelink', 'type' => OPTION_TYPE_CHECKBOX,
+										'desc' => gettext("If checked the thumbs link to the full image instead of the image page."),
+										'order'	=> 8),
+						gettext('Mode')			=> array('key' => 'bxslider_mode', 'type' => OPTION_TYPE_SELECTOR,
 										'selections' => array(
-														gettext('Horizontal')	 => "horizontal",
-														gettext('Vertical')		 => "vertical",
-														gettext('Fade')				 => "fade"),
-										'desc'			 => gettext("The Mide of the thumb nav. Note this might require theme changes.)"))
+														gettext('Horizontal')	=> "horizontal",
+														gettext('Vertical')		=> "vertical",
+														gettext('Fade')			=> "fade"),
+										'desc'		=> gettext("The mode of the thumb nav. Note this might require theme changes."),
+										'order'		=> 9)
 		);
 		foreach (getThemeFiles(array('404.php', 'themeoptions.php', 'theme_description.php')) as $theme => $scripts) {
 			$list = array();
 			foreach ($scripts as $script) {
 				$list[$script] = 'bxslider_' . $theme . '_' . stripSuffix($script);
 			}
-			$options[$theme] = array('key'				 => 'bxslider_' . $theme . '_scripts', 'type'			 => OPTION_TYPE_CHECKBOX_ARRAY,
+			$options[$theme] = array('key'				 => 'bxslider_' . $theme . '_scripts', 'type' => OPTION_TYPE_CHECKBOX_ARRAY,
 							'checkboxes' => $list,
 							'desc'			 => gettext('The scripts for which BxSlider is enabled. {If themes require it they might set this, otherwise you need to do it manually!}')
 			);
