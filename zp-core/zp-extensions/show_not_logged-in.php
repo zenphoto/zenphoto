f9e8691c18a3173da52c7f8e2b4fad21175a6f51
Stephen Billard <stephen@sbillard.org>
2014-02-15 17:08:00 -0800

    fix #670

--- zp-core/zp-extensions/show_not_logged-in.php
+++ zp-core/zp-extensions/show_not_logged-in.php
@@ -7,17 +7,18 @@
  * @package plugins
  * @subpackage development
  */
-$plugin_is_filter = 95 | FEATURE_PLUGIN;
+$plugin_is_filter = 9999 | FEATURE_PLUGIN;
 $plugin_description = sprintf(gettext("Treats users as not logged in for gallery pages."), DATA_FOLDER);
 $plugin_author = "Stephen Billard (sbillard)";
 
 
 zp_register_filter('guest_login_attempt', 'show_not_loggedin::adminLoginAttempt');
-zp_register_filter('load_theme_script', 'show_not_loggedin::hideAdmin');
+if (!OFFSET_PATH)
+	show_not_loggedin::hideAdmin();
 
 class show_not_loggedin {
 
-	static function hideAdmin($param) {
+	static function hideAdmin() {
 		global $_zp_loggedin, $_zp_current_admin_obj, $_showNotLoggedin_real_auth;
 		if (!OFFSET_PATH && is_object($_zp_current_admin_obj)) {
 			$_showNotLoggedin_real_auth = $_zp_current_admin_obj;
@@ -29,7 +30,6 @@ class show_not_loggedin {
 			}
 			$_zp_current_admin_obj = $_zp_loggedin = NULL;
 		}
-		return $param;
 	}
 
 	static function adminLoginAttempt($success, $user, $pass, $athority) {
