0c596eb2a9f0a3ff085bed5f899c8ff7f0957f16
Stephen Billard <stephen@sbillard.org>
2014-03-06 12:28:15 -0800

    fix #724

--- zp-core/zp-extensions/tinymce4/config/zenphoto-classic.js.php
+++ zp-core/zp-extensions/tinymce4/config/zenphoto-classic.js.php
@@ -24,9 +24,9 @@ if ($filehandler) {
 					plugins: [
 									"advlist autolink autosave link image lists charmap print preview hr anchor pagebreak spellchecker",
 									"searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
-									"table contextmenu directionality emoticons template textcolor paste fullpage textcolor tinyzenpage"
+									"table contextmenu directionality emoticons template textcolor paste textcolor tinyzenpage"
 					],
-									toolbar1: "newdocument fullpage | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | styleselect formatselect fontselect fontsizeselect",
+									toolbar1: "newdocument | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | styleselect formatselect fontselect fontsizeselect",
 									toolbar2: "cut copy paste | searchreplace | bullist numlist | outdent indent blockquote | undo redo | link unlink anchor image media code | inserttime preview | forecolor backcolor",
 									toolbar3: "table | hr removeformat | subscript superscript | charmap emoticons | print fullscreen | ltr rtl | spellchecker | visualchars visualblocks nonbreaking template pagebreak restoredraft tinyzenpage",
 									menubar: false,
