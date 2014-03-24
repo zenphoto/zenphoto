0b0f1a1f1663a807cb0b9c8bfe7f5b87bd7bd83e
Stephen Billard <stephen@sbillard.org>
2014-03-24 16:24:47 -0700

    Flag release as Beta

--- zp-core/version.php
+++ zp-core/version.php
@@ -1,5 +1,5 @@
 <?php // This file contains version info only and is automatically updated. DO NOT EDIT.
-define('ZENPHOTO_VERSION', '1.4.6-DEV');
+define('ZENPHOTO_VERSION', '1.4.6-BETA');
 define('ZENPHOTO_FULL_RELEASE', trim(file_get_contents(dirname(__FILE__).'/githead')));
 define('ZENPHOTO_RELEASE', substr(ZENPHOTO_FULL_RELEASE,0,10));
 ?>
