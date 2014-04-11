3afd0faea8b64ce728af331730471e5af098c534
Stephen Billard <stephen@sbillard.org>
2014-04-10 17:19:06 -0700

    set of "dark" comment config files

---
+++ zp-core/zp-extensions/tinymce4/config/comment-basic_dark.js.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The configuration functions for TinyMCE 4.x.
+ *
+ * Zenphoto plugin default light configuration
+ */
+?>
+<script type="text/javascript" src="<?php echo WEBPATH . "/" . ZENFOLDER . "/" . PLUGIN_FOLDER; ?>/tinymce4/tinymce.min.js"></script>
+<script type="text/javascript">
+// <!-- <![CDATA[
+	tinymce.init({
+		skin: "tundora",
+		selector: "textarea.textarea_inputbox",
+		language: "<?php echo $locale; ?>",
+		relative_urls: false,
+		plugins: [
+			"advlist autolink lists link image charmap print preview anchor",
+			"searchreplace visualblocks code fullscreen",
+			"insertdatetime media table contextmenu paste tinyzenpage"
+		],
+		content_css: "<?php echo FULLWEBPATH . '/' . ZENFOLDER . '/' . PLUGIN_FOLDER; ?>/tinymce4/config/content_dark.css",
+		toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | fullscreen"
+	});
+// ]]> -->
+</script>
---
+++ zp-core/zp-extensions/tinymce4/config/comment-full_dark.js.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The configuration functions for TinyMCE
+ *
+ * Zenpage plugin default light configuration
+ */
+?>
+<script type="text/javascript" src="<?php echo WEBPATH . "/" . ZENFOLDER . "/" . PLUGIN_FOLDER; ?>/tinymce4/tinymce.min.js"></script>
+<script type="text/javascript">
+// <!-- <![CDATA[
+	tinymce.init({
+		skin: "tundora",
+		selector: "textarea.textarea_inputbox",
+		language: "<?php echo $locale; ?>",
+		relative_urls: false,
+		plugins: [
+			"advlist autolink lists link image charmap print preview hr anchor pagebreak",
+			"searchreplace wordcount visualblocks visualchars code fullscreen",
+			"insertdatetime media nonbreaking save table contextmenu directionality",
+			"emoticons template paste tinyzenpage"
+		],
+		content_css: "<?php echo FULLWEBPATH . '/' . ZENFOLDER . '/' . PLUGIN_FOLDER; ?>/tinymce4/config/content_dark.css",
+		toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+		toolbar2: "print preview media | forecolor backcolor emoticons | code | fullscreen",
+	});
+// ]]> -->
+</script>
