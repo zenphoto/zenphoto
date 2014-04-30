6047f0c051a3e33a1ef365dc0d58940c2f8de700
acrylian <wartgart@gmx.net>
2014-04-30 13:39:01 +0200

    The mergerdRSS plugin should maybe tell how you actually call the feed…

--- zp-core/zp-extensions/mergedRSS.php
+++ zp-core/zp-extensions/mergedRSS.php
@@ -6,10 +6,11 @@
  * Based on David Stinemetze's {@link http://www.widgetsandburritos.com/technical/programming/merge-rss-feeds-php-cache/ MergedRSS Class}
  *
  * If you ever wanted to have a Zenphoto RSS that for example returns latest images for <i>album x</i>
- * but latest albums for <i>album y</i> this is the tool to use.
- * Just enter the urls of the seperate feeds and it will return the combined feed.
+ * but latest albums for <i>album y</i> this is the tool to use. Just enter the urls of the seperate feeds and it will return the combined feed.
  *
- * While this plugin is mean for Zenphoto's RSS, you could also use even external RSS feeds.
+ * The merged feed is called by yourdomain.com/index.php?mergedrss
+ *
+ * While this plugin is meant for Zenphoto's RSS feeds, you could also use even external RSS feeds.
  * But be aware that hijacking content may be a vialation of applicable laws!
  *
  * @author Malte Müller (acrylian)
