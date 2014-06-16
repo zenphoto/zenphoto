bd1341a35bdfe5bbc84525246758e475bc11fbf0
Stephen Billard <stephen@sbillard.org>
2014-06-16 16:43:20 -0700

    theme part of 55d1b1004c2a7f05f66b675bf1c195b308b5ea7c

--- themes/zpminimal/theme_description.php
+++ themes/zpminimal/theme_description.php
@@ -6,5 +6,5 @@ $theme_description['author'] = 'gjr';
 $theme_description['version'] = '1.4.4.3b';
 $theme_description['date'] = '4/9/13';
 $theme_description['desc'] = 'A ZenPhoto/ZenPage minimalistic theme.';
-$theme_description['distribution'] = true;
+$theme_description['distribution'] = 'ZenPhoto20';
 ?>
--- themes/zpskeleton/theme_description.php
+++ themes/zpskeleton/theme_description.php
@@ -6,5 +6,5 @@ $theme_description['author'] = 'ZEPsikopat';
 $theme_description['version'] = '1.5.0';
 $theme_description['date'] = '28/4/14';
 $theme_description['desc'] = 'A ZenPhoto/ZenPage responsive theme ready for desktop to mobile device viewing.';
-$theme_description['distribution'] = true;
+$theme_description['distribution'] = 'ZenPhoto20';
 ?>
