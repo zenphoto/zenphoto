5bdb5d7cbd90bb4582062691bdfb7cc45159460b
Stephen Billard <stephen@sbillard.org>
2014-07-29 13:49:14 -0700

    cron runner issues

--- zp-core/admin-functions.php
+++ zp-core/admin-functions.php
@@ -1469,7 +1469,7 @@ function printAdminHeader($tab, $subtab = NULL) {
 									<label><input type="checkbox" name="disclose_password<?php echo $suffix; ?>"
 																id="disclose_password<?php echo $suffix; ?>"
 																onclick="passwordClear('<?php echo $suffix; ?>');
-																		togglePassword('<?php echo $suffix; ?>');" /><?php echo addslashes(gettext('Show password')); ?></label>
+																				togglePassword('<?php echo $suffix; ?>');" /><?php echo addslashes(gettext('Show password')); ?></label>
 								</td>
 								<td>
 									<input type="text" size="<?php echo TEXT_INPUT_SIZE; ?>"
@@ -1986,7 +1986,7 @@ function printAdminHeader($tab, $subtab = NULL) {
 										 } else {
 											 ?>
 											 onclick="toggleAlbumMCR('<?php echo $prefix; ?>', '');
-													 deleteConfirm('Delete-<?php echo $prefix; ?>', '<?php echo $prefix; ?>', deleteAlbum1);"
+															 deleteConfirm('Delete-<?php echo $prefix; ?>', '<?php echo $prefix; ?>', deleteAlbum1);"
 											 <?php
 										 }
 										 ?> />
@@ -3452,6 +3452,7 @@ function processManagedObjects($i, &$rights) {
 				$pages[] = array('data' => $key, 'type' => 'pages');
 			}
 		}
+
 		if (substr($key, 0, $l_n) == $prefix_n) {
 			if ($value) {
 				$key = postIndexDecode(substr($key, $l_n));
@@ -3483,6 +3484,7 @@ function processManagedObjects($i, &$rights) {
 			$pages = array();
 		}
 	}
+
 	if (empty($news)) {
 		if (!($rights & MANAGE_ALL_NEWS_RIGHTS)) {
 			$rights = $rights & ~ZENPAGE_NEWS_RIGHTS;
@@ -4330,8 +4332,20 @@ function admin_securityChecks($rights, $return) {
 	checkInstall();
 	httpsRedirect();
 
-	if ($_zp_current_admin_obj && $_zp_current_admin_obj->reset) {
-		$_zp_loggedin = USER_RIGHTS;
+	if ($_zp_current_admin_obj) {
+		if ($_zp_current_admin_obj->reset) {
+			$_zp_loggedin = USER_RIGHTS;
+		}
+	} else {
+		if (isset($_SESSION['admin'])) {
+			$user = unserialize($_SESSION['admin']);
+			$user2 = Zenphoto_Authority::getAnAdmin(array('`user`=' => $user->getUser(), '`valid`=' => 1));
+			if ($user2 && $user->getPass() == $user2->getPass()) {
+				Zenphoto_Authority::logUser($user2);
+				$_zp_current_admin_obj = $user2;
+				$_zp_loggedin = $_zp_current_admin_obj->getRights();
+			}
+		}
 	}
 	if (!zp_loggedin($rights)) {
 // prevent nefarious access to this page.
@@ -4519,7 +4533,7 @@ function printPageSelector($subpage, $rangeset, $script, $queryParams) {
 		}
 		?>
 		<select name="subpage" class="ignoredirty" id="subpage<?php echo $instances; ?>" onchange="launchScript('<?php echo WEBPATH . '/' . ZENFOLDER . '/' . $script; ?>',
-								[<?php echo $jump; ?>'subpage=' + $('#subpage<?php echo $instances; ?>').val()]);" >
+										[<?php echo $jump; ?>'subpage=' + $('#subpage<?php echo $instances; ?>').val()]);" >
 							<?php
 							foreach ($rangeset as $page => $range) {
 								?>
@@ -5002,12 +5016,12 @@ function linkPickerIcon($obj, $id = NULL, $extra = NULL) {
 	<a onclick="<?php
 	if ($id) {
 		?>
-				$('#<?php echo $id; ?>').select();
+						$('#<?php echo $id; ?>').select();
 		<?php
 	}
 	?>
-			$('.pickedObject').removeClass('pickedObject');
-			$('#<?php echo $iconid; ?>').addClass('pickedObject');
+					$('.pickedObject').removeClass('pickedObject');
+					$('#<?php echo $iconid; ?>').addClass('pickedObject');
 	<?php linkPickerPick($obj, $id, $extra); ?>"
 		 title="<?php echo gettext('pick source'); ?>">
 		<img src="<?php echo WEBPATH . '/' . ZENFOLDER; ?>/images/add.png" alt="" id="<?php echo $iconid; ?>">
