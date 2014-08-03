b2dab858c0c3a452fcdba1d2dbb6e73b616d9003
Stephen Billard <stephen@sbillard.org>
2014-08-02 17:49:01 -0700

    some improvements to the debug plugin
    now shows _SESSION variable because I found that useful

--- zp-core/zp-extensions/debug.php
+++ zp-core/zp-extensions/debug.php
@@ -20,16 +20,23 @@ zp_register_filter('admin_tabs', 'debug::tabs');
 class debug {
 
 	static function tabs($tabs) {
-		if (!isset($tabs['debug'])) {
-			$tabs['debug'] = array('text'		 => gettext("debug"),
-							'link'		 => WEBPATH . "/" . ZENFOLDER . '/' . PLUGIN_FOLDER . '/debug/admin_tab.php',
-							'rights'	 => DEBUG_RIGHTS,
-							'default'	 => 'phpinfo');
+		if (zp_loggedin(DEBUG_RIGHTS)) {
+			if (!isset($tabs['debug'])) {
+				$tabs['debug'] = array('text'	 => gettext("debug"),
+								'link'	 => WEBPATH . "/" . ZENFOLDER . '/' . PLUGIN_FOLDER . '/debug/admin_tab.php',
+								'rights' => DEBUG_RIGHTS);
+			}
+			if (zp_loggedin(ADMIN_RIGHTS)) {
+				$tabs['debug']['default'] = 'phpinfo';
+				$tabs['debug']['subtabs'][gettext("phpinfo")] = PLUGIN_FOLDER . '/debug/admin_tab.php?page=debug&tab=phpinfo';
+				$tabs['debug']['subtabs'][gettext("Locales")] = PLUGIN_FOLDER . '/debug/admin_tab.php?page=debug&tab=locale';
+				$tabs['debug']['subtabs'][gettext("Session")] = PLUGIN_FOLDER . '/debug/admin_tab.php?page=debug&tab=session';
+			} else {
+				$tabs['debug']['default'] = 'cookie';
+			}
+			$tabs['debug']['subtabs'][gettext("HTTP accept")] = PLUGIN_FOLDER . '/debug/admin_tab.php?page=debug&tab=http';
+			$tabs['debug']['subtabs'][gettext("Cookies")] = PLUGIN_FOLDER . '/debug/admin_tab.php?page=debug&tab=cookie';
 		}
-		$tabs['debug']['subtabs'][gettext("phpinfo")] = PLUGIN_FOLDER . '/debug/admin_tab.php?page=debug&tab=phpinfo';
-		$tabs['debug']['subtabs'][gettext("HTTP accept")] = PLUGIN_FOLDER . '/debug/admin_tab.php?page=debug&tab=http';
-		$tabs['debug']['subtabs'][gettext("Locales")] = PLUGIN_FOLDER . '/debug/admin_tab.php?page=debug&tab=locale';
-		$tabs['debug']['subtabs'][gettext("Cookies")] = PLUGIN_FOLDER . '/debug/admin_tab.php?page=debug&tab=cookie';
 		return $tabs;
 	}
 
