a61dd2fd61a55b8378036a8cbef329357921da0e
Stephen Billard <stephen@sbillard.org>
2014-08-17 16:45:31 -0700

    unimportant differences

--- themes/basic/index.php
+++ themes/basic/index.php
@@ -1,6 +1,5 @@
 <?php
 // force UTF-8 Ø
-
 if (!defined('WEBPATH'))
 	die();
 ?>
@@ -8,30 +7,30 @@ if (!defined('WEBPATH'))
 <html>
 	<head>
 
-		<?php zp_apply_filter('theme_head'); ?>
+<?php zp_apply_filter('theme_head'); ?>
 
 		<link rel="stylesheet" href="<?php echo pathurlencode($zenCSS); ?>" type="text/css" />
 		<link rel="stylesheet" href="<?php echo pathurlencode(dirname(dirname($zenCSS))); ?>/common.css" type="text/css" />
-		<?php if (class_exists('RSS')) printRSSHeaderLink('Gallery', gettext('Gallery RSS')); ?>
+<?php if (class_exists('RSS')) printRSSHeaderLink('Gallery', gettext('Gallery RSS')); ?>
 	</head>
 	<body>
-		<?php zp_apply_filter('theme_body_open'); ?>
+<?php zp_apply_filter('theme_body_open'); ?>
 		<div id="main">
 			<div id="gallerytitle">
-				<?php
-				if (getOption('Allow_search')) {
-					printSearchForm('');
-				}
-				?>
+<?php
+if (getOption('Allow_search')) {
+	printSearchForm('');
+}
+?>
 				<h2><?php
-					printHomeLink('', ' | ');
-					printGalleryTitle();
-					?></h2>
+				printHomeLink('', ' | ');
+				printGalleryTitle();
+				?></h2>
 			</div>
 			<div id="padbox">
-				<?php printGalleryDesc(); ?>
+<?php printGalleryDesc(); ?>
 				<div id="albums">
-					<?php while (next_album()): ?>
+				<?php while (next_album()): ?>
 						<div class="album">
 							<div class="thumb">
 								<a href="<?php echo html_encode(getAlbumURL()); ?>" title="<?php echo gettext('View album:'); ?> <?php printAnnotatedAlbumTitle(); ?>"><?php printAlbumThumbImage(getAnnotatedAlbumTitle()); ?></a>
@@ -43,18 +42,18 @@ if (!defined('WEBPATH'))
 							</div>
 							<p style="clear: both; "></p>
 						</div>
-					<?php endwhile; ?>
+<?php endwhile; ?>
 				</div>
 				<br class="clearall" />
-				<?php printPageListWithNav("« " . gettext("prev"), gettext("next") . " »"); ?>
+<?php printPageListWithNav("« " . gettext("prev"), gettext("next") . " »"); ?>
 			</div>
 		</div>
 		<div id="credit">
-			<?php
-			if (function_exists('printFavoritesURL')) {
-				printFavoritesURL(NULL, '', ' | ', '<br />');
-			}
-			?>
+<?php
+if (function_exists('printFavoritesURL')) {
+	printFavoritesURL(NULL, '', ' | ', '<br />');
+}
+?>
 			<?php @call_user_func('printUserLogin_out', '', ' | '); ?>
 			<?php if (class_exists('RSS')) printRSSLink('Gallery', '', 'RSS', ' | '); ?>
 			<?php printCustomPageURL(gettext("Archive View"), "archive"); ?> |
@@ -70,7 +69,7 @@ if (!defined('WEBPATH'))
 			?>
 			<?php printSoftwareLink(); ?>
 		</div>
-		<?php @call_user_func('mobileTheme::controlLink'); ?>
+			<?php @call_user_func('mobileTheme::controlLink'); ?>
 		<?php @call_user_func('printLanguageSelector'); ?>
 		<?php
 		zp_apply_filter('theme_body_close');
