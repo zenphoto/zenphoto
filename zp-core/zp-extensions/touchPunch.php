d152953755aebbef6780c4fcc15bb4d4f6ca6de8
Stephen Billard <stephen@sbillard.org>
2014-09-16 11:28:10 -0700

    typo

--- zp-core/zp-extensions/touchPunch.php
+++ zp-core/zp-extensions/touchPunch.php
@@ -9,7 +9,7 @@
  */
 
 $plugin_is_filter = 5 | ADMIN_PLUGIN;
-$plugin_description = gettext("enables Touch Event Support for jQuery UI.");
+$plugin_description = gettext("Enables Touch Event Support for jQuery UI.");
 $plugin_notice = gettext('This plugin may not work for you. Use at your own discretion. It has been tested and works on an iPad with the Safari browser and on Android with the default browser. Drag and drop works but clicking on links does not on Chrome on Android. It does not work for Opera on Android.');
 
 $plugin_author = "Stephen Billard (sbillard)";
