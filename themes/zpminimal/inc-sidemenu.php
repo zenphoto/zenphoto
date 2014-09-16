57c123146933ca8c16d8a62de8a169db95e81224
Stephen Billard <stephen@sbillard.org>
2014-09-16 11:21:11 -0700

    #156
    Clears the line after third album thumb. This does mean that the thumb
    real estate may be un-even, so maybe better would be to truncate the
    titles. But this is useful anyway.

--- themes/zpminimal/inc-sidemenu.php
+++ themes/zpminimal/inc-sidemenu.php
@@ -14,14 +14,14 @@
 			<li><a href="<?php echo html_encode(getGalleryIndexURL()); ?>" title="<?php echo ('Home'); ?>"><?php echo ('Home'); ?></a></li>
 			<li <?php if ($galleryactive) { ?>class="active" <?php } ?>><a href="<?php echo getCustomPageURL('gallery'); ?>" title="<?php echo gettext('Gallery'); ?>"><?php echo gettext('Gallery'); ?></a></li>
 			<li <?php if ($_zp_gallery_page == "archive.php") { ?>class="active" <?php } ?>><a href="<?php echo getCustomPageURL('archive'); ?>" title="<?php echo gettext('Archive View'); ?>"><?php echo gettext('Archive'); ?></a></li>
-				<?php if (function_exists('getNewsIndexURL')) { ?>
+			<?php if (function_exists('getNewsIndexURL')) { ?>
 				<li <?php if ($_zp_gallery_page == "news.php") { ?>class="active" <?php } ?>>
 					<a href="<?php echo getNewsIndexURL(); ?>"><?php echo gettext('News'); ?></a>
-				<?php
-				if ($_zp_gallery_page == "news.php") {
-					printAllNewsCategories('', true, '', 'active', true, '', 'active', 'list', true);
-				}
-				?>
+					<?php
+					if ($_zp_gallery_page == "news.php") {
+						printAllNewsCategories('', true, '', 'active', true, '', 'active', 'list', true);
+					}
+					?>
 				</li>
 			<?php } ?>
 			<?php
@@ -35,18 +35,18 @@
 			}
 			?>
 			<?php if (function_exists('printContactForm')) { ?><li <?php if ($_zp_gallery_page == "contact.php") { ?>class="active" <?php } ?>><?php printCustomPageURL(gettext('Contact'), "contact"); ?></li><?php } ?>
-				<?php if (!zp_loggedin() && function_exists('printRegistrationForm')) { ?>
+			<?php if (!zp_loggedin() && function_exists('printRegistrationForm')) { ?>
 				<li <?php if ($_zp_gallery_page == "register.php") { ?>class="active" <?php } ?>><a href="<?php echo getCustomPageURL('register'); ?>" title="<?php echo gettext('Register'); ?>"><?php echo gettext('Register'); ?></a></li>
-				<?php } ?>
-		<?php
-		if (function_exists("printUserLogin_out")) {
-			if (zp_loggedin()) {
-				?>
+			<?php } ?>
+			<?php
+			if (function_exists("printUserLogin_out")) {
+				if (zp_loggedin()) {
+					?>
 					<li><?php printUserLogin_out('', ''); ?></li>
-		<?php } else { ?>
+				<?php } else { ?>
 					<li <?php if ($_zp_gallery_page == "login.php") { ?>class="active" <?php } ?>><a href="<?php echo getCustomPageURL('login'); ?>" title="<?php echo gettext('Login'); ?>"><?php echo gettext('Login'); ?></a></li>
-		<?php } ?>
-	<?php } ?>
+					<?php } ?>
+				<?php } ?>
 		</ul>
-<?php } ?>
+	<?php } ?>
 </div>
