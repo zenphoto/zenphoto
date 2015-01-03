08d3d9d91f29d3ae971617bdd3c5f4e331586fd2
Stephen Billard <stephen@sbillard.org>
2015-01-02 17:05:06 -0800

    Add fine-tuning to tags suggested
    This change adds an option to tag suggestion to filter out less used
    tags from the suggestions. The option specifies how many uses of the tag
    there must be for it to be part of the suggestion list.

--- zp-core/zp-extensions/tag_suggest.php
+++ zp-core/zp-extensions/tag_suggest.php
@@ -1,51 +1,75 @@
 <?php
 /**
- * Provide javaScript tag "suggestions" based on Remy Sharp's {@link http://remysharp.com/2007/12/28/jquery-tag-suggestion/ jQuery Tag Suggestion} plugin.
+ * Provide javaScript tag "suggestions"
+ * Based on Remy Sharp's {@link http://remysharp.com/2007/12/28/jquery-tag-suggestion/ jQuery Tag Suggestion}
+ * plugin as modified for ZenPhoto20 to enhance performance.
  *
- * Activate the plugin and the feature is available on the theme's search field.
- * It is also available on each item edit page's tag selector.
+ * This plugin provides suggestions for tag fields such as the search form. It is
+ * automatically enabled for administration fields. The plugin must be enabled for
+ * the suggestions to appear on theme pages.
  *
- * @author Malte Müller (acrylian), Stephen Billard (sbillard) - an adaption of Remy Sharp's jQuery Tag Suggestion
+ * copyright 2015 by Stephen Billard for use in ZenPhoto20
+ *
+ * @author Stephen Billard (sbillard)
  * @package plugins
  * @subpackage theme
  */
 $plugin_is_filter = defaultExtension(9 | THEME_PLUGIN);
 $plugin_description = gettext("Enables jQuery tag suggestions on the search field.");
-$plugin_author = "Malte Müller (acrylian), Stephen Billard (sbillard)";
+$plugin_author = "Stephen Billard";
 
-zp_register_filter('theme_head', 'tagSuggestJS');
-zp_register_filter('admin_head', 'tagSuggestJS');
+$option_interface = 'tag_suggest';
 
-function tagSuggestJS() {
-	// the scripts needed
-	?>
-	<script type="text/javascript" src="<?php echo WEBPATH . '/' . ZENFOLDER . '/' . PLUGIN_FOLDER; ?>/tag_suggest/encoder.js"></script>
-	<script type="text/javascript" src="<?php echo WEBPATH . '/' . ZENFOLDER . '/' . PLUGIN_FOLDER; ?>/tag_suggest/tag.js"></script>
-	<?php
-	$css = getPlugin('tag_suggest/tag.css', true, true);
-	?>
-	<link type="text/css" rel="stylesheet" href="<?php echo pathurlencode($css); ?>" />
-	<?php
-	$taglist = getAllTagsUnique(OFFSET_PATH ? false : NULL, !OFFSET_PATH);
-	$tags = array();
-	foreach ($taglist as $tag) {
-		$tags[] = addslashes($tag);
+zp_register_filter('theme_head', 'tag_suggest::JS');
+zp_register_filter('admin_head', 'tag_suggest::JS');
+
+class tag_suggest {
+
+	function __construct() {
+		setOptionDefault('tag_suggest_threshold', 1);
 	}
 
-	if (OFFSET_PATH || getOption('search_space_is') == 'OR') {
-		$tagseparator = ' ';
-	} else {
-		$tagseparator = ',';
+	function getOptionsSupported() {
+		$options = array(gettext('threshold') => array('key'		 => 'tag_suggest_threshold', 'type'	 => OPTION_TYPE_NUMBER,
+										'order'	 => 1,
+										'limits' => array('min' => 1),
+										'desc'	 => gettext('Only tags with at least this number of uses will be suggested.'))
+		);
+		return $options;
 	}
-	?>
-	<script type="text/javascript">
-		// <!-- <![CDATA[
-		var _tagList = ["<?php echo implode($tags, '","'); ?>"];
-		$(function () {
-			$('#search_input, #edit-editable_4, .tagsuggest').tagSuggest({separator: '<?php echo $tagseparator; ?>', tags: _tagList})
-		});
-		// ]]> -->
-	</script>
-	<?php
+
+	static function JS() {
+		// the scripts needed
+		?>
+		<script type="text/javascript" src="<?php echo WEBPATH . '/' . ZENFOLDER . '/' . PLUGIN_FOLDER; ?>/tag_suggest/encoder.js"></script>
+		<script type="text/javascript" src="<?php echo WEBPATH . '/' . ZENFOLDER . '/' . PLUGIN_FOLDER; ?>/tag_suggest/tag.js"></script>
+		<?php
+		$css = getPlugin('tag_suggest/tag.css', true, true);
+		?>
+		<link type="text/css" rel="stylesheet" href="<?php echo pathurlencode($css); ?>" />
+		<?php
+		$taglist = getAllTagsUnique(OFFSET_PATH ? false : NULL, OFFSET_PATH ? 0 : getOption('tag_suggest_threshold'));
+		$tags = array();
+		foreach ($taglist as $tag) {
+			$tags[] = addslashes($tag);
+		}
+
+		if (OFFSET_PATH || getOption('search_space_is') == 'OR') {
+			$tagseparator = ' ';
+		} else {
+			$tagseparator = ',';
+		}
+		?>
+		<script type="text/javascript">
+			// <!-- <![CDATA[
+			var _tagList = ["<?php echo implode($tags, '","'); ?>"];
+			$(function () {
+				$('#search_input, #edit-editable_4, .tagsuggest').tagSuggest({separator: '<?php echo $tagseparator; ?>', tags: _tagList})
+			});
+			// ]]> -->
+		</script>
+		<?php
+	}
+
 }
 ?>
