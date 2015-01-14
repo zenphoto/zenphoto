9b63f574a7c761830f27257a0d077b7f05761714
acrylian <wartgart@gmx.net>
2015-01-14 13:08:42 +0100

    Should fix the unwanted future publishded / expired items in randomImages

--- zp-core/classes.php
+++ zp-core/classes.php
@@ -727,11 +727,42 @@ class ThemeObject extends PersistentObject {
 	 * @param string $show
 	 */
 	function checkAccess(&$hint = NULL, &$show = NULL) {
-		if ($this->isMyItem(LIST_RIGHTS)) {
-			return true;
-		}
-		return $this->checkforGuest($hint, $show);
-	}
+    if (!$this->checkPublishDates()) {
+      $this->setShow(0);
+    }
+    if ($this->isMyItem(LIST_RIGHTS)) {
+      return true;
+    }
+    return $this->checkforGuest($hint, $show);
+  }
+  
+  /**
+   * Checks if the item is either expired or in scheduled publishing
+   * A class method wrapper of the functions.php function of the same name
+   * @return boolean
+   */
+  function checkPublishDates() {
+    $row = array();
+    if (isAlbumClass($this) || isImageClass($this)) {
+      $row = array(
+          'show' => $this->getShow(),
+          'expiredate' => $this->getExpireDate(),
+          'publishdate' => $this->getPublishDate()
+      );
+    } else if ($this->table == 'news' || $this->table == 'pages') {
+      $row = array(
+          'show' => $this->getShow(),
+          'expiredate' => $this->getExpireDate(),
+          'publishdate' => $this->getDateTime()
+      );
+    }
+    $check = checkPublishDates($row);
+    if ($check == 1 || $check == 2) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 
 }
 
