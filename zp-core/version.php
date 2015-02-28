a72e97f9127beb56df5e92725e8d9e3f5745a46e
Stephen Billard <stephen@sbillard.org>
2015-02-27 17:03:15 -0800

    1.1.1.0 release
    Feature updates--see release notes
    fix #904
    fix #895

--- zp-core/version.php
+++ zp-core/version.php
@@ -1,5 +1,5 @@
 <?php
 // This file contains version info only and is automatically updated. DO NOT EDIT. 
-define('ZENPHOTO_VERSION', '1.1.0.12'); 
+define('ZENPHOTO_VERSION', '1.1.1.0'); 
 define('ZENPHOTO_RELEASE', '33f57a25aa'); 
 ?>
