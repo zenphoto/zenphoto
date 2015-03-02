97bf202bf731533e5ae3b478df6b622f8d2a94bf
Stephen Billard <stephen@sbillard.org>
2015-03-01 18:07:43 -0800

    merge 1.1.1.1

--- zp-core/version.php
+++ zp-core/version.php
@@ -1,5 +1,6 @@
 <?php
+
 // This file contains version info only and is automatically updated. DO NOT EDIT.
-define('ZENPHOTO_VERSION', '1.1.1.0');
-define('ZENPHOTO_RELEASE', '33f57a25aa');
+define('ZENPHOTO_VERSION', '1.1.1.1');
+define('ZENPHOTO_RELEASE', 'dd503bb31b');
 ?>
