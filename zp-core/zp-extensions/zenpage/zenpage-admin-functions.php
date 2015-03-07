95aef2097de7b2ff57fa34de0e4b04a01d72d8bc
acrylian <wartgart@gmx.net>
2015-03-07 15:02:55 +0100

    Fix for date selector for Zenpage articles if no date is set

--- zp-core/zp-extensions/zenpage/zenpage-admin-functions.php
+++ zp-core/zp-extensions/zenpage/zenpage-admin-functions.php
@@ -556,7 +556,7 @@ function printArticleDatesDropdown() {
 	<form name="AutoListBox1" id="articledatesdropdown" style="float:left; margin-left: 10px;" action="#" >
 		<select name="ListBoxURL" size="1" onchange="gotoLink(this.form)">
 			<?php
-			echo "<option $selected value='admin-news-articles.php" . getNewsAdminOptionPath(array_merge(array('date' => 'all'), $option)) . "'>" . gettext("View all months") . "</option>";
+			echo "<option $selected value='admin-news-articles.php" . getNewsAdminOptionPath(array_merge(array('' => ''), $option)) . "'>" . gettext("View all months") . "</option>";
 			while (list($key, $val) = each($datecount)) {
 				$nr++;
 				if ($key == '0000-00-01') {
