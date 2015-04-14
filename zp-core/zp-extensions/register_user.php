01a31b8b938ea0aef82ba00fe0df4dca8af5aa5f
acrylian <wartgart@gmx.net>
2015-04-14 12:24:08 +0200

    One more for #919

--- zp-core/zp-extensions/register_user.php
+++ zp-core/zp-extensions/register_user.php
@@ -220,6 +220,12 @@ class register_user {
 			$_notify = 'incomplete';
 		}
 		$user = trim(sanitize($_POST['user']));
+		if (getOption('register_user_email_is_id')) {
+			$mail_duplicate = $_zp_authority->checkUniqueMailaddress($user, $user);
+			if ($mail_duplicate) {
+				$_notify = 'exists';
+			}
+		} 
 		if (isset($_POST['admin_email'])) {
 			$admin_e = trim(sanitize($_POST['admin_email']));
 			$mail_duplicate = $_zp_authority->checkUniqueMailaddress($admin_e, $user);
