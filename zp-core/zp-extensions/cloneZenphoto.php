bfc7b205daa398116386ca7282107241cbf7fdb5
Stephen Billard <stephen@sbillard.org>
2015-06-06 13:45:35 -0700

    Move Cloning function to an admin tab
    And provide links to existing clones

--- zp-core/zp-extensions/cloneZenphoto.php
+++ zp-core/zp-extensions/cloneZenphoto.php
@@ -4,10 +4,15 @@
  * "Clones" the currrent installation to a new location using symlinks. The <i>zp-core</i>, <i>themes</i>, <i>user plugins</i>
  * folders and the root <i>index.php</i> file are symlinked. Setup will create the other needed folders.
  *
- * The <i>Clone installation</i> button will take you to the cloning page where you can select a folder destination
- * for the cloned installation. Upon successful cloning there will be a link to <var>setup</var> for the new
+ * The <i>Clone</i> tab will take you to the cloning page.
+ *
+ * Links to previously cloned installations will be listed on this page.
+ *
+ * You can select a folder destination
+ * for a new cloned installation. Upon successful cloning there will be a link to <var>setup</var> for the new
  * installation. (This presumes the <var>setup</var> files are present. If not you will be told which files
  * need to be reloaded.)
+ *
  *
  * <b>Note:</b> If the destination already has a installation these files and folders will be removed by the cloning
  * process!
@@ -30,33 +35,35 @@ require_once(SERVERPATH . '/' . ZENFOLDER . '/reconfigure.php');
 if ($plugin_disable) {
 	enableExtension('cloneZenphoto', 0);
 } else {
-	zp_register_filter('admin_utilities_buttons', 'cloneZenphoto::button');
+	zp_register_filter('admin_tabs', 'cloneZenphoto::tabs');
 
 	class cloneZenphoto {
 
-		static function button($buttons) {
-			$buttons[] = array(
-							'category'		 => gettext('Admin'),
-							'enable'			 => true,
-							'button_text'	 => gettext('Clone installation'),
-							'formname'		 => 'cloneZenphoto',
-							'action'			 => FULLWEBPATH . '/' . ZENFOLDER . '/' . PLUGIN_FOLDER . '/cloneZenphoto/cloneTab.php',
-							'icon'				 => 'images/folder.png',
-							'title'				 => gettext('Create a new installation using links to the current install files.'),
-							'alt'					 => gettext('Clone'),
-							'hidden'			 => '',
-							'rights'			 => ADMIN_RIGHTS
-			);
-			return $buttons;
+		static function tabs($tabs) {
+			if (zp_loggedin(ADMIN_RIGHTS)) {
+				$oldtabs = $tabs;
+				$tabs = array();
+				foreach ($oldtabs as $tab => $data) {
+					if ($tab == 'logs') {
+						$tabs['clone'] = array('text'		 => gettext("clone"),
+										'link'		 => WEBPATH . "/" . ZENFOLDER . '/' . PLUGIN_FOLDER . '/cloneZenphoto/cloneTab.php',
+										'rights'	 => ADMIN_RIGHTS,
+										'subtabs'	 => NULL);
+					}
+					$tabs[$tab] = $data;
+				}
+			}
+
+			return $tabs;
 		}
 
-		static function setup() {
+		static function clones() {
 			global $_zp_current_admin_obj;
 			$clones = array();
 			if ($result = query('SELECT * FROM ' . prefix('plugin_storage') . ' WHERE `type`="clone"')) {
 				while ($row = db_fetch_assoc($result)) {
 					if (file_exists($row['aux'] . '/' . DATA_FOLDER . '/zenphoto.cfg.php')) {
-						$clones[$row['aux']] = $row['data'] . '/' . ZENFOLDER . '/setup/index.php?autorun';
+						$clones[$row['aux']] = $row['data'] . '/';
 						$_SESSION['admin'][bin2hex($row['aux'])] = serialize($_zp_current_admin_obj);
 					} else {
 						query('DELETE FROM ' . prefix('plugin_storage') . ' WHERE `id` = ' . $row['id']);
