3056f1bb2294d4f40df50e2b7fcf243904c2fb8c
Stephen Billard <stephen@sbillard.org>
2015-08-15 18:54:35 -0700

    Revert "release build 1.2.0.12"
    This reverts commit 78a331fa8e5640f9bbc595d993d8d0e3f87d33f5.

--- zp-core/version.php
+++ zp-core/version.php
@@ -1,5 +1,5 @@
 <?php
 // This file contains version info only and is automatically updated. DO NOT EDIT. 
-define('ZENPHOTO_VERSION', '1.2.0.12'); 
-define('ZENPHOTO_RELEASE', '5665788aa1'); 
+define('ZENPHOTO_VERSION', '1.2.0.11'); 
+define('ZENPHOTO_RELEASE', 'c4ac557a9a'); 
 ?>
