0f41fbbe343f2e816d1057622b23d199696d033c
Stephen Billard <stephen@sbillard.org>
2015-09-10 17:17:05 -0700

    typo

--- plugins/zenphotoCompatibilityPack/legacyConverter.php
+++ plugins/zenphotoCompatibilityPack/legacyConverter.php
@@ -30,7 +30,7 @@ $legacyReplacements = array(
 				'class_exists\([\'"]Zenpage[\'"]\)'	 => 'class_exists("CMS")',
 				'\$_zp_current_zenpage_article'			 => '$_zp_current_article',
 				'\$_zp_current_zenpage_page'				 => '$_zp_current_page',
-				'->getFullImage\('									 => '->getFullImageURL\('
+				'->getFullImage\('									 => '->getFullImageURL('
 );
 
 /**
