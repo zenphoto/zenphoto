8bf4983571503abf1c57f208c7699f0f4a04a243
fretzl <f.sondaar@kpnmail.nl>
2015-11-08 11:06:06 +0100

    Add some <strong> tags around numbers

--- zp-core/admin.php
+++ zp-core/admin.php
@@ -658,7 +658,7 @@ if (!zp_loggedin()) {
 								$t = $_zp_gallery->getNumImages();
 								$c = $t - $_zp_gallery->getNumImages(true);
 								if ($c > 0) {
-									printf(ngettext('<strong>%1$u</strong> Image (%2$u un-published)', '<strong>%1$u</strong> Images (%2$u un-published)', $t), $t, $c);
+									printf(ngettext('<strong>%1$u</strong> Image (%2$u un-published)', '<strong>%1$u</strong> Images (<strong>%2$u</strong> un-published)', $t), $t, $c);
 								} else {
 									printf(ngettext('<strong>%u</strong> Image', '<strong>%u</strong> Images', $t), $t);
 								}
@@ -669,7 +669,7 @@ if (!zp_loggedin()) {
 								$t = $_zp_gallery->getNumAlbums(true);
 								$c = $t - $_zp_gallery->getNumAlbums(true, true);
 								if ($c > 0) {
-									printf(ngettext('<strong>%1$u</strong> Album (%2$u un-published)', '<strong>%1$u</strong> Albums (%2$u un-published)', $t), $t, $c);
+									printf(ngettext('<strong>%1$u</strong> Album (%2$u un-published)', '<strong>%1$u</strong> Albums (<strong>%2$u</strong> un-published)', $t), $t, $c);
 								} else {
 									printf(ngettext('<strong>%u</strong> Album', '<strong>%u</strong> Albums', $t), $t);
 								}
@@ -680,7 +680,7 @@ if (!zp_loggedin()) {
 								$t = $_zp_gallery->getNumComments(true);
 								$c = $t - $_zp_gallery->getNumComments(false);
 								if ($c > 0) {
-									printf(ngettext('<strong>%1$u</strong> Comment (%2$u in moderation)', '<strong>%1$u</strong> Comments (%2$u in moderation)', $t), $t, $c);
+									printf(ngettext('<strong>%1$u</strong> Comment (%2$u in moderation)', '<strong>%1$u</strong> Comments (<strong>%2$u</strong> in moderation)', $t), $t, $c);
 								} else {
 									printf(ngettext('<strong>%u</strong> Comment', '<strong>%u</strong> Comments', $t), $t);
 								}
