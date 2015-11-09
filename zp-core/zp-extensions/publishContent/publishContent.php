2c0d69fc69c7ffcaf414f8c97091743d66db7eb5
Stephen Billard <stephen@sbillard.org>
2015-11-09 15:44:40 -0800

    fix text botched by zenphoto merge
    41064aedf1355a8cecab16d073c253046743806b

--- zp-core/zp-extensions/publishContent/publishContent.php
+++ zp-core/zp-extensions/publishContent/publishContent.php
@@ -255,7 +255,7 @@ echo '</head>';
 				$visible = $report == 'albums' || $report == 'propagate';
 				$c = count($publish_albums_list)
 				?>
-				<p class="notebox smallbox"><strong>Note: </strong><?php echo gettext('The number of un-published items does <strong>not</strong> include items that are un-published by inheritance.'); ?></p>
+				<p class="notebox smallbox"><?php echo gettext('<strong>Note:</strong> Items not published by inheritance are not included.'); ?></p>
 				<fieldset class="smallbox">
 					<legend><?php
 						if ($c > 0)
