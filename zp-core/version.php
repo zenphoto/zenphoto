26c78176cfb8d752e9e7bda4e65d69fc84b714a3
Tristan Harward <trisweb@gmail.com>
2015-12-01 09:19:47 -0500

    Zenphoto version 1.4.11

--- zp-core/version.php
+++ zp-core/version.php
@@ -1,7 +1,7 @@
 <?php
 
 // This file contains version info only and is automatically updated. DO NOT EDIT.
-define('ZENPHOTO_VERSION', '1.4.10');
+define('ZENPHOTO_VERSION', '1.4.11');
 define('ZENPHOTO_FULL_RELEASE', trim(file_get_contents(dirname(__FILE__) . '/githead')));
 define('ZENPHOTO_RELEASE', substr(ZENPHOTO_FULL_RELEASE, 0, 10));
 ?>
