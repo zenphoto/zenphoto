31353587450d5779e5a30ffe62c080ef928ab426
Stephen Billard <stephen@sbillard.org>
2016-03-08 08:32:17 -0800

    display admin notes

--- zp-core/zp-extensions/accessThreshold/admin_tab.php
+++ zp-core/zp-extensions/accessThreshold/admin_tab.php
@@ -14,7 +14,7 @@ admin_securityChecks(DEBUG_RIGHTS, $return = currentRelativeURL());
 
 
 $subtab = getSubtabs();
-printAdminHeader('overview', $subtab);
+printAdminHeader('development', $subtab);
 
 $recentIP = getSerializedArray(@file_get_contents(SERVERPATH . '/' . DATA_FOLDER . '/recentIP'));
 unset($recentIP['config']);
@@ -29,26 +29,33 @@ echo "\n</head>";
 	<div id="main">
 		<?php printTabs(); ?>
 		<div id="content">
-			<?php
-			$ct = 0;
-			foreach ($recentIP as $entity => $data) {
-				if ($data['counter'] < $noise)
-					break;
-				echo '<span style="width:25%;float:left">';
-				echo '<span style="width:40%;float:left">' . $entity . '</span>';
-				echo '<span style="width:48%;float:left">' . date('Y-m-d H:i:s', $data['accessTime']) . '</span>';
-				echo '<span style="width:3%;float:left">' . $data['counter'] . '</span>';
-				echo "</span>\n";
-				$ct++;
-				if ($ct % 4 == 0) {
-					echo '<br clear="both">';
-				}
-			}
-			if (empty($ct)) {
-				echo gettext("No entries excede the noise level");
-			}
-			?>
-
+			<?php zp_apply_filter('admin_note', 'database', ''); ?>
+			<div id="container">
+				<?php
+				$subtab = printSubtabs();
+				?>
+				<div class="tabbox">
+					<?php
+					$ct = 0;
+					foreach ($recentIP as $entity => $data) {
+						if ($data['counter'] < $noise)
+							break;
+						echo '<span style="width:25%;float:left">';
+						echo '<span style="width:40%;float:left">' . $entity . '</span>';
+						echo '<span style="width:48%;float:left">' . date('Y-m-d H:i:s', $data['accessTime']) . '</span>';
+						echo '<span style="width:3%;float:left">' . $data['counter'] . '</span>';
+						echo "</span>\n";
+						$ct++;
+						if ($ct % 4 == 0) {
+							echo '<br clear="both">';
+						}
+					}
+					if (empty($ct)) {
+						echo gettext("No entries excede the noise level");
+					}
+					?>
+				</div>
+			</div>
 		</div>
 	</div>
 	<br class = "clearall" />
