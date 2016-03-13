1046030de7bdffeb41b09ee49df064c7efcd937b
Stephen Billard <stephen@sbillard.org>
2016-03-12 17:23:53 -0800

    release build 1.2.6.21

--- zp-core/version.php
+++ zp-core/version.php
@@ -1,5 +1,5 @@
 <?php
 // This file contains version info only and is automatically updated. DO NOT EDIT. 
-define('ZENPHOTO_VERSION', '1.2.6.20'); 
-define('ZENPHOTO_RELEASE', 'ec9c642c8e'); 
+define('ZENPHOTO_VERSION', '1.2.6.21'); 
+define('ZENPHOTO_RELEASE', '7033d4540c'); 
 ?>
