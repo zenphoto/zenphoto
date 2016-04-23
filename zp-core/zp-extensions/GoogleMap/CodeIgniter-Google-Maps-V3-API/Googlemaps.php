550f402ad6dad05f5c3f4f2bfab372a4950956bf
fretzl <f.sondaar@kpnmail.nl>
2016-04-23 18:29:44 +0200

    Googlemaps.php __construct

--- zp-core/zp-extensions/GoogleMap/CodeIgniter-Google-Maps-V3-API/Googlemaps.php
+++ zp-core/zp-extensions/GoogleMap/CodeIgniter-Google-Maps-V3-API/Googlemaps.php
@@ -147,7 +147,7 @@ class Googlemaps {
 	var $placesAutocompleteBoundsMap= FALSE;					// An alternative to setting the SW and NE bounds is to use the bounds of the current viewport. If set to TRUE, the bounds will be set to the viewport of the visible map, even if dragged or zoomed
 	var $placesAutocompleteOnChange	= '';						// The JavaScript action to perform when a place is selected
 
-	function Googlemaps($config = array())
+	function __construct($config = array())
 	{
 		if (count($config) > 0)
 		{
