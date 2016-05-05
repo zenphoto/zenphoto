37006029985489e81e6496c7c1a55b567eed7bc6
Stephen <stephen@sbillard.org>
2016-05-05 14:45:37 -0700

    release build 1.3.0.3

--- zp-core/version.php
+++ zp-core/version.php
@@ -1,5 +1,5 @@
 <?php
 // This file contains version info only and is automatically updated. DO NOT EDIT. 
-define('ZENPHOTO_VERSION', '1.3.0.2'); 
-define('ZENPHOTO_RELEASE', 'c56809bc6c'); 
+define('ZENPHOTO_VERSION', '1.3.0.3'); 
+define('ZENPHOTO_RELEASE', '880300c7e5'); 
 ?>
