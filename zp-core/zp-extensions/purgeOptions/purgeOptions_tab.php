4c5122b3504ad48c64325db7cc1323aa5b29fbd5
Stephen <stephen@sbillard.org>
2017-02-18 16:24:53 -0800

    fis sort order of options and correct some Netbeans screwups

--- zp-core/zp-extensions/purgeOptions/purgeOptions_tab.php
+++ zp-core/zp-extensions/purgeOptions/purgeOptions_tab.php
@@ -24,32 +24,22 @@ if (isset($_POST['purge'])) {
 
 	if (isset($_POST['del'])) {
 		foreach ($_POST['del'] as $owner) {
-			$sql = 'DELETE FROM ' . prefix('options') . ' WHERE `creator` LIKE ' . db_quote('%' . basename($owner));
+			$sql = 'DELETE FROM ' . prefix('options') . ' WHERE `creator` LIKE ' . db_quote($owner . '%');
 			$result = query($sql);
 			if (preg_match('~^' . THEMEFOLDER . '/~', $owner)) {
-				if ($owner == THEMEFOLDER . '/') {
-					$purgedActive[] = true;
-					$where = ' WHERE `creator` = "' . THEMEFOLDER . '/"';
-				} else {
-					$where = ' WHERE `creator` LIKE ' . db_quote('%' . basename($owner) . '/themeoptions.php');
-					if (file_exists(SERVERPATH . '/' . THEMEFOLDER . '/' . basename($owner) . '/themeoptions.php')) {
-						$purgedActive[] = true;
-					}
+				if (file_exists(SERVERPATH . '/' . THEMEFOLDER . '/' . basename($owner) . '/themeoptions.php')) {
+					$purgedActive[] = true; // theme still exists, need to re-run setup
 				}
-				$sql = 'DELETE FROM ' . prefix('options') . $where;
-				$result = query($sql);
 			} else {
-				purgeOption('zp_plugin_' . stripSuffix(basename($owner)));
-				$purgedActive[basename($owner)] = true;
+				$plugin = basename($owner);
+				if (!in_array($plugin, $_POST['missingplugin'])) {
+					$purgedActive[basename($owner)] = true;
+					purgeOption('zp_plugin_' . stripSuffix(basename($owner)));
+				}
 			}
 		}
 	}
-	if (isset($_POST['missingplugin'])) {
-		foreach ($_POST['missingplugin'] as $plugin) {
-			purgeOption('zp_plugin_' . stripSuffix($plugin));
-			unset($purgedActive[$plugin]);
-		}
-	}
+
 	if (isset($_POST['missingcreator'])) {
 		foreach ($_POST['missingcreator'] as $key => $action) {
 			switch ($action) {
@@ -93,91 +83,82 @@ $orphaned = array();
 					$sql = 'SELECT `name` FROM ' . prefix('options') . ' WHERE `name` LIKE "zp_plugin_%"';
 					$result = query_full_array($sql);
 					foreach ($result as $row) {
-						$plugin = str_replace('zp_plugin_', '', $row['name']) . '.php';
-						$file = str_replace(SERVERPATH, '', getPlugin($plugin, false));
+						$plugin = str_replace('zp_plugin_', '', $row['name']);
+						$file = str_replace(SERVERPATH, '', getPlugin($plugin . '.php', false));
 						if ($file) {
 							if (strpos($file, PLUGIN_FOLDER) === false) {
-								$owners[USER_PLUGIN_FOLDER][$plugin] = $plugin;
+								$owners[USER_PLUGIN_FOLDER][strtolower($plugin)] = $plugin;
 							}
 						} else {
 							purgeOption($row['name']);
 						}
 					}
 
-					$nullCreator = $hiddenOptions = false;
 					$sql = 'SELECT `creator` FROM ' . prefix('options') . ' ORDER BY `creator`';
 					$result = query_full_array($sql);
 					foreach ($result as $owner) {
 						$structure = explode('/', preg_replace('~\[.*\]$~', '', $owner['creator']));
 						switch ($structure[0]) {
 							case NULL:
-								$nullCreator = true;
 								break;
 							case THEMEFOLDER:
-								$owners[THEMEFOLDER][] = $structure[1];
+								$owners[THEMEFOLDER][strtolower($structure[1])] = $structure[1];
 								break;
 							case USER_PLUGIN_FOLDER:
 								unset($structure[0]);
-								$owners[USER_PLUGIN_FOLDER][] = implode('/', $structure);
+								$creator = stripSuffix(implode('/', $structure));
+								$owners[USER_PLUGIN_FOLDER][strtolower($creator)] = $creator;
 								break;
 							case ZENFOLDER:
 								if ($structure[1] == PLUGIN_FOLDER) {
 									unset($structure[0], $structure[1]);
-									$owners[ZENFOLDER . '/' . PLUGIN_FOLDER][] = implode('/', $structure);
+									$creator = stripSuffix(implode('/', $structure));
+									$owners[ZENFOLDER . '/' . PLUGIN_FOLDER][strtolower($creator)] = $creator;
 								}
 								break;
 						}
 					}
-					$empty = false;
-					if ($nullCreator) {
-						$sql = 'SELECT * FROM ' . prefix('options') . ' WHERE `creator` is NULL';
-						$result = query_full_array($sql);
-						foreach ($result as $opt) {
-							if (strpos($opt['name'], 'zp_plugin_') === false) {
-								if (empty($opt['value'])) {
-									$empty = true;
+					ksort($owners[ZENFOLDER . '/' . PLUGIN_FOLDER]);
+					ksort($owners[USER_PLUGIN_FOLDER]);
+					ksort($owners[THEMEFOLDER]);
+
+					$empty = $hiddenOptions = false;
+					$sql = 'SELECT * FROM ' . prefix('options') . ' WHERE `creator` is NULL || `creator` LIKE "%purgeOptions%" ORDER BY `name`';
+					$result = query_full_array($sql);
+					foreach ($result as $opt) {
+						if (strpos($opt['name'], 'zp_plugin_') === false) {
+							if (empty($opt['value'])) {
+								$empty = true;
+								if (empty($opt['creator'])) {
 									$orpahaned[$opt['id']] = '<span class="emptyOption">' . $opt['name'] . '</span>';
 								} else {
+									$hiddenOptions = true;
+									$orpahaned[$opt['id']] = '<span class="hiddenOrphanHighlight emptyOption">' . $opt['name'] . '</span>';
+								}
+							} else {
+								if (empty($opt['creator'])) {
 									$orpahaned[$opt['id']] = $opt['name'];
+								} else {
+									$hiddenOptions = true;
+									$orpahaned[$opt['id']] = '<span class="hiddenOrphanHighlight">' . $opt['name'] . '</span>';
 								}
 							}
 						}
 					}
-					//	"owned" by purgeOptions
-					$sql = 'SELECT * FROM ' . prefix('options') . ' WHERE `creator` LIKE "%purgeOptions%";';
-					$result = query_full_array($sql);
-					foreach ($result as $opt) {
-						$hiddenOptions = true;
-						if (empty($opt['value'])) {
-							$empty = true;
-							$orpahaned[$opt['id']] = '<span class="hiddenOrphanHighlight emptyOption">' . $opt['name'] . '</span>';
-						} else {
-							$orpahaned[$opt['id']] = '<span class="hiddenOrphanHighlight">' . $opt['name'] . '</span>';
-						}
-					}
 
-					if (isset($owners[ZENFOLDER . '/' . PLUGIN_FOLDER])) {
-						$owners[ZENFOLDER . '/' . PLUGIN_FOLDER] = array_unique($owners[ZENFOLDER . '/' . PLUGIN_FOLDER]);
-					}
-					if (isset($owners[USER_PLUGIN_FOLDER])) {
-						$owners[USER_PLUGIN_FOLDER] = array_unique($owners[USER_PLUGIN_FOLDER]);
-					}
-					if (isset($owners[THEMEFOLDER])) {
-						$owners[THEMEFOLDER] = array_unique($owners[THEMEFOLDER]);
-					}
 
-					if (empty($owners) && !$nullCreator) {
+
+					if (empty($owners) && empty($orpahaned)) {
 						echo gettext('No option owners have been located.');
 					} else {
 						?>
-						<form class="dirtylistening" onReset="setClean('purge_options_form');
-									" id="purge_options_form" action="?page = options&tab = purge" method="post" >
-										<?php XSRFToken('purgeOptions'); ?>
+						<form class="dirtylistening" onReset="setClean('purge_options_form');" id="purge_options_form" action="?page=options&tab=purge" method="post" >
+							<?php XSRFToken('purgeOptions'); ?>
 							<input type="hidden" name="purge" value="1" />.
 							<p class = "buttons" >
 								<button type="submit" value="<?php echo gettext('Apply')
-										?>"> <img src="<?php echo WEBPATH . '/' . ZENFOLDER; ?>/images/pass_2.png" alt="" /> <strong><?php echo gettext("Apply"); ?> </strong></button >
-								<button type="" "reset" value="<?php echo gettext('reset') ?>"> <img src="<?php echo WEBPATH . '/' . ZENFOLDER; ?>/images/reset.png" alt="" /> <strong><?php echo gettext("Reset"); ?> </strong></button>
+							?>"> <img src="<?php echo WEBPATH . '/' . ZENFOLDER; ?>/images/pass_2.png" alt="" /> <strong><?php echo gettext("Apply"); ?> </strong></button >
+								<button type="reset" value="<?php echo gettext('reset') ?>"> <img src="<?php echo WEBPATH . '/' . ZENFOLDER; ?>/images/reset.png" alt="" /> <strong><?php echo gettext("Reset"); ?> </strong></button>
 							</p>
 							<br class="clearall" />
 
