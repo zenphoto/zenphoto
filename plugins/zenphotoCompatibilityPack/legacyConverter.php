67dbb6dec0811623979841c6293415090dd0752c
Stephen <stephen@sbillard.org>
2017-03-21 14:35:17 -0700

    fixes to legacy conversion tool

--- plugins/zenphotoCompatibilityPack/legacyConverter.php
+++ plugins/zenphotoCompatibilityPack/legacyConverter.php
@@ -22,18 +22,28 @@ $legacyReplacements = array(
 		'\$_zp_zenpage' => '$_zp_CMS',
 		'ZP_NEWS_ENABLED' => 'TRUE/* TODO:replaced ZP_NEWS_ENABLED */',
 		'ZP_PAGES_ENABLED' => 'TRUE/* TODO:replaced ZP_PAGES_ENABLED */',
-		'getAllTagsCount\(.*?\);' => 'getAllTagsUnique(NULL, 1, true);',
-		'printHeadTitle\(.*?\);' => '/* TODO:replaced printHeadTitle(); */',
+		'getAllTagsCount\(.*?\)' => 'getAllTagsUnique(NULL, 1, true)',
+		'printHeadTitle\(.*?\);?' => '/* TODO:replaced printHeadTitle() */',
 		'getSiteHomeURL\(.*?\)' => 'getGalleryIndexURL() /* TODO:replaced getSiteHomeURL() */',
-		'printSiteHomeURL\(.*?\);' => '/* TODO:replaced printSiteHomeURL(); */',
+		'printSiteHomeURL\(.*?\);?' => '/* TODO:replaced printSiteHomeURL() */',
 		'getNextPrevNews\([\'"](.*)[\'"]\)' => 'get$1News() /* TODO:replaced getNextPrevNews(\'$1\') */',
-		'zenpagePublish\((.*)\,(.*)\);' => '$1->setShow($2); /* TODO:replaced zenpagePublish() */',
+		'zenpagePublish\((.*)\,(.*)\)' => '$1->setShow($2) /* TODO:replaced zenpagePublish() */',
+		'getImageCustomData\(\)' => '($_zp_current_image)?$_zp_current_image->get("custom_data"):NULL /*TODO: Use customFieldExtender to define unique fields */',
+		'printImageCategoryCustomData\(\)' => 'echo ($_zp_current_image)?$_zp_current_image->get("custom_data"):NULL /*TODO: Use customFieldExtender to define unique fields */',
+		'getAlbumCustomData\(\)' => '($_zp_current_album)?$_zp_current_album->get("custom_data"):NULL /*TODO: Use customFieldExtender to define unique fields */',
+		'printAlbumCategoryCustomData\(\)' => 'echo ($_zp_current_album)?$_zp_current_album->get("custom_data"):NULL /*TODO: Use customFieldExtender to define unique fields */',
+		'getPageCustomData\(\)' => '($_zp_current_page)?$_zp_current_page->get("custom_data"):NULL /*TODO: Use customFieldExtender to define unique fields */',
+		'printPageCategoryCustomData\(\)' => 'echo ($_zp_current_page)?$_zp_current_page->get("custom_data"):NULL /*TODO: Use customFieldExtender to define unique fields */',
+		'getNewsCustomData\(\)' => '($_zp_current_article)?$_zp_current_article->get("custom_data"):NULL /*TODO: Use customFieldExtender to define unique fields */',
+		'printNewsCustomData\(\)' => 'echo ($_zp_current_article)?$_zp_current_article->get("custom_data"):NULL /*TODO: Use customFieldExtender to define unique fields */',
+		'getNewsCategoryCustomData\(\)' => '($_zp_current_category)?$_zp_current_category->get("custom_data"):NULL /*TODO: Use customFieldExtender to define unique fields */',
+		'printNewsCategoryCustomData\(\)' => 'echo ($_zp_current_category)?$_zp_current_category->get("custom_data"):NULL /*TODO: Use customFieldExtender to define unique fields */',
 		'class_exists\([\'"]Zenpage[\'"]\)' => 'class_exists("CMS")',
 		'\$_zp_current_zenpage_news' => '$_zp_current_article',
 		'\$_zp_current_zenpage_page' => '$_zp_current_page',
 		'->getFullImage\(' => '->getFullImageURL(',
 		'tinymce4_' => 'tinymce_',
-		'(setOptionDefault\([\'"]colorbox_.*[\'"],.*\);)' => '$1 /* TODO:replace with a call to colorbox::registerScripts(); */'
+		'(setOptionDefault\([\'"]colorbox_.*[\'"],.*\);?)' => '$1 /* TODO:replace with a call to colorbox::registerScripts(); */'
 );
 
 /**
@@ -86,6 +96,7 @@ if (isset($_GET['action'])) {
 			$body = preg_replace('~' . $match . '~im', $replace, $body);
 		}
 		$body = preg_replace('~/\* TODO:replaced .*/\* TODO:replaced(.*)\*/ \*/~', '/* TODO:replaced$1*/', $body); //in case we came here twice
+
 		if ($source != $body) {
 			file_put_contents($file, $body);
 			$counter++;
@@ -107,9 +118,9 @@ printSubtabs();
 <div class="tabbox">
 	<h1><?php echo gettext('Convert legacy Zenphoto themes and plugins'); ?></h1>
 
-	<?php
-	if (isset($counter)) {
-		?>
+<?php
+if (isset($counter)) {
+	?>
 		<div class="messagebox fade-message">
 			<h2><?php printf(ngettext('%s file updated', '%s files updated', $counter), $counter); ?></h2>
 		</div>
@@ -141,16 +152,16 @@ printSubtabs();
 	?>
 	<form class="dirtylistening" id="form_convert" action="?action=process" method="post" >
 		<ul class="page-list">
-			<?php
-			XSRFToken('saveoptions');
-			if (!empty($themes)) {
-				?>
+	<?php
+	XSRFToken('saveoptions');
+	if (!empty($themes)) {
+		?>
 				<li>
 					<ul>
-						<?php
-						echo gettext('Themes');
-						foreach ($themes as $theme) {
-							?>
+				<?php
+				echo gettext('Themes');
+				foreach ($themes as $theme) {
+					?>
 							<li><label><input type="checkbox" name="themes[]" value="<?php echo html_encode($theme); ?>" ><?php echo html_encode($theme); ?></label></li>
 							<?php
 						}
@@ -158,25 +169,25 @@ printSubtabs();
 					</ul>
 				</li>
 				<br />
-				<?php
-			}
-			if (!empty($plugins)) {
-				?>
+						<?php
+					}
+					if (!empty($plugins)) {
+						?>
 				<li>
 					<ul>
-						<?php
-						echo gettext('Plugins');
-						foreach ($plugins as $plugin) {
-							?>
+				<?php
+				echo gettext('Plugins');
+				foreach ($plugins as $plugin) {
+					?>
 							<li><label><input type="checkbox" name="plugins[]" value="<?php echo html_encode($plugin); ?>" ><?php echo html_encode($plugin); ?></label></li>
 							<?php
 						}
 						?>
 					</ul>
 				</li>
-				<?php
-			}
-			?>
+						<?php
+					}
+					?>
 		</ul>
 
 		<p class="buttons">
