4974ff244c0afe5f1a69ea12ee4626a8543e298c
Stephen <stephen@sbillard.org>
2017-03-25 15:38:00 -0700

    fix #1064
    Adds asynchronous mail queuing and sending to the user_mailing_list
    plugin

--- zp-core/zp-extensions/user_mailing_list.php
+++ zp-core/zp-extensions/user_mailing_list.php
@@ -17,16 +17,16 @@ if (defined('OFFSET_PATH')) {
 	function user_mailing_list_button($buttons) {
 		global $_zp_authority, $_zp_current_admin_obj;
 		$button = array(
-						'category'		 => gettext('Admin'),
-						'enable'			 => false,
-						'button_text'	 => gettext('User mailing list'),
-						'formname'		 => 'user_mailing_list.php',
-						'action'			 => FULLWEBPATH . '/' . ZENFOLDER . '/' . PLUGIN_FOLDER . '/user_mailing_list.php',
-						'icon'				 => 'images/icon_mail.png',
-						'title'				 => gettext('There are no other registered users who have provided an e-mail address.'),
-						'alt'					 => '',
-						'hidden'			 => '',
-						'rights'			 => ADMIN_RIGHTS
+				'category' => gettext('Admin'),
+				'enable' => false,
+				'button_text' => gettext('User mailing list'),
+				'formname' => 'user_mailing_list.php',
+				'action' => FULLWEBPATH . '/' . ZENFOLDER . '/' . PLUGIN_FOLDER . '/user_mailing_list.php',
+				'icon' => 'images/icon_mail.png',
+				'title' => gettext('There are no other registered users who have provided an e-mail address.'),
+				'alt' => '',
+				'hidden' => '',
+				'rights' => ADMIN_RIGHTS
 		);
 		$currentadminuser = $_zp_current_admin_obj->getUser();
 		$admins = $_zp_authority->getAdministrators();
@@ -49,10 +49,6 @@ if (defined('OFFSET_PATH')) {
 	require_once(dirname(dirname(__FILE__)) . '/admin-globals.php');
 
 	admin_securityChecks(NULL, currentRelativeURL());
-
-	if (isset($_GET['sendmail'])) {
-		XSRFdefender('mailing_list');
-	}
 
 	$admins = $_zp_authority->getAdministrators();
 	$zenphoto_tabs['overview']['subtabs'] = array(gettext('Mailing') => '');
@@ -83,95 +79,76 @@ if (defined('OFFSET_PATH')) {
 					} else {
 						$disabled = '';
 					}
+					?>
+					<p id="sent" class="messagebox" style="display:none;">
+						<?php echo gettext('Mail sent'); ?>
+					</p>
 
-					if (isset($_GET['sendmail'])) {
-						//form handling stuff to add...
-						$subject = NULL;
-						$message = NULL;
-						if (isset($_POST['subject'])) {
-							$subject = sanitize($_POST['subject']);
-						}
-						if (isset($_POST['message'])) {
-							$message = sanitize($_POST['message']);
-						}
-						$cc_addresses = array();
-						$admincount = count($admins);
-						foreach ($admins as $admin) {
-							if (isset($_POST["admin_" . $admin['id']])) {
-								$cc_addresses[] = $admin['email'];
-							}
-						}
-						$currentadminmail = $_zp_current_admin_obj->getEmail();
-						if (!empty($currentadminmail)) {
-							$cc_addresses[] = $currentadminmail;
-						}
-						$err_msg = zp_mail($subject, $message, array(), array(), $cc_addresses);
-						if ($err_msg) {
-							echo '<p class="errorbox">' . $err_msg . '</p>';
-						} else {
-							echo '<p class="messagebox">' . gettext('Mail sent.') . '</p>';
-							?>
-							<h3><strong><?php echo gettext('Subject:'); ?> </strong><?php echo $subject; ?></h3>
-							<p><strong><?php echo gettext('To:'); ?> </strong><?php echo implode(',', $cc_addresses); ?></p>
-							<strong><?php echo gettext('Message:'); ?> </strong><?php echo $message; ?>
-							<p class="buttons"><a href="user_mailing_list.php" title="<?php echo gettext('Send another mail'); ?>"><?php echo gettext('Send another mail'); ?></a></p>
-							<?php
-						}
-					} else {
-						?>
-						<h2><?php echo gettext('Please enter the message you want to send.'); ?></h2>
-						<form class="dirtylistening" onReset="setClean('massmail');" id="massmail" action="?sendmail" method="post" accept-charset="UTF-8" autocomplete="off">
-							<?php XSRFToken('mailing_list'); ?>
-							<table>
-								<tr>
-									<td valign="top">
-								<labelfor="subject"><?php echo gettext('Subject:'); ?></label><br />
-									<input type="text" id="subject" name="subject" value="" size="70"<?php echo $disabled; ?> /><br /><br />
-									<label for="message"><?php echo gettext('Message:'); ?></label><br />
-									<textarea id="message" name="message" value="" cols="68" rows="10"<?php echo $disabled; ?> ></textarea>
-									</td>
-									<td valign="top" align="left">
-										<?php echo gettext('Select users:'); ?>
-										<ul class="unindentedchecklist" style="height: 205px; width: 30em;">
-											<?php
-											$currentadminuser = $_zp_current_admin_obj->getUser();
-											foreach ($admins as $admin) {
-												if (!empty($admin['email']) && $currentadminuser != $admin['user']) {
-													?>
-													<li>
-														<label for="admin_<?php echo $admin['id']; ?>">
-															<input name="admin_<?php echo $admin['id']; ?>" id="admin_<?php echo $admin['id']; ?>" type="checkbox" value="<?php echo html_encode($admin['email']); ?>" checked="checked"  <?php echo $disabled; ?>/>
-															<?php
-															echo $admin['user'] . " (";
-															if (!empty($admin['name'])) {
-																echo $admin['name'] . " - ";
-															}
-															echo $admin['email'] . ")";
-															?>
-														</label>
-													</li>
-													<?php
-												}
+					<h2><?php echo gettext('Please enter the message you want to send.'); ?></h2>
+					<form class="dirtylistening" onReset="setClean('massmail');" id="massmail" action="<?php echo WEBPATH . '/' . ZENFOLDER . '/' . PLUGIN_FOLDER ?>/user_mailing_list/mail_handler.php?sendmail" method="post" accept-charset="UTF-8" autocomplete="off">
+						<?php XSRFToken('mailing_list'); ?>
+						<table>
+							<tr>
+								<td valign="top">
+							<labelfor="subject"><?php echo gettext('Subject:'); ?></label><br />
+								<input type="text" id="subject" name="subject" value="" size="70"<?php echo $disabled; ?> /><br /><br />
+								<label for="message"><?php echo gettext('Message:'); ?></label><br />
+								<textarea id="message" name="message" value="" cols="68" rows="10"<?php echo $disabled; ?> ></textarea>
+								</td>
+								<td valign="top" align="left">
+									<?php echo gettext('Select users:'); ?>
+									<ul class="unindentedchecklist" style="height: 205px; width: 30em;">
+										<?php
+										$currentadminuser = $_zp_current_admin_obj->getUser();
+										foreach ($admins as $admin) {
+											if (!empty($admin['email']) && $currentadminuser != $admin['user']) {
+												?>
+												<li>
+													<label for="admin_<?php echo $admin['id']; ?>">
+														<input name="admin_<?php echo $admin['id']; ?>" id="admin_<?php echo $admin['id']; ?>" type="checkbox" value="<?php echo html_encode($admin['email']); ?>" checked="checked"  <?php echo $disabled; ?>/>
+														<?php
+														echo $admin['user'] . " (";
+														if (!empty($admin['name'])) {
+															echo $admin['name'] . " - ";
+														}
+														echo $admin['email'] . ")";
+														?>
+													</label>
+												</li>
+												<?php
 											}
-											?>
-										</ul>
-										<br />
-									</td>
-									</tr>
-							</table>
-							<p class="buttons">
-								<button class="submitbutton" type="submit"
-												title="<?php echo gettext("Send mail"); ?>"<?php echo $disabled; ?> ><img
-										src="../images/pass.png" alt="" /><strong><?php echo gettext("Send mail"); ?></strong></button>
-							</p>
-							<p class="buttons">
-								<button class="submitbutton" type="reset"
-												title="<?php echo gettext("Reset"); ?>"><img src="../images/reset.png"
-																														 alt="" /><strong><?php echo gettext("Reset"); ?></strong></button>
-							</p>
-							<br style="clear: both" />
-						</form>
-					<?php } ?>
+										}
+										?>
+									</ul>
+									<br />
+								</td>
+								</tr>
+						</table>
+						<script type="text/javascript">
+							$('form#massmail').submit(function () {
+								$.post($(this).attr('action'), $(this).serialize(), function (res) {
+									// Do something with the response `res`
+									console.log(res);
+								});
+								$('form#massmail').trigger('reset');
+								$('#sent').show();
+								$("#sent").fadeTo(5000, 1).fadeOut(1000);
+								return false; // prevent default action
+							});
+						</script>
+						<p class="buttons">
+							<button class="submitbutton" type="submit"
+											title="<?php echo gettext("Send mail"); ?>"<?php echo $disabled; ?> ><img
+									src="../images/pass.png" alt="" /><strong><?php echo gettext("Send mail"); ?></strong></button>
+						</p>
+						<p class="buttons">
+							<button class="submitbutton" type="reset"
+											title="<?php echo gettext("Reset"); ?>"><img src="../images/reset.png"
+																													 alt="" /><strong><?php echo gettext("Reset"); ?></strong></button>
+						</p>
+						<br style="clear: both" />
+					</form>
+
 				</div>
 			</div><!-- content -->
 		</div><!-- main -->
---
+++ zp-core/zp-extensions/user_mailing_list/mail_handler.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * Handles sending the mailing list e-mails
+ */
+// UTF-8 Ø
+define('OFFSET_PATH', 4);
+require_once(dirname(dirname(dirname(__FILE__))) . '/admin-globals.php');
+require_once(SERVERPATH . '/' . ZENFOLDER . '/reconfigure.php');
+
+admin_securityChecks(NULL, currentRelativeURL());
+
+XSRFdefender('mailing_list');
+
+//form handling stuff to add...
+$subject = NULL;
+$message = NULL;
+if (isset($_POST['subject'])) {
+	$subject = sanitize($_POST['subject']);
+}
+if (isset($_POST['message'])) {
+	$message = sanitize($_POST['message']);
+}
+$toList = array();
+$admins = $_zp_authority->getAdministrators();
+$admincount = count($admins);
+foreach ($admins as $admin) {
+	if (isset($_POST["admin_" . $admin['id']])) {
+		if ($admin['name']) {
+			$toList[$admin['name']] = $admin['email'];
+		} else {
+			$toList[] = $admin['email'];
+		}
+	}
+}
+$currentadminmail = $_zp_current_admin_obj->getEmail();
+if (!empty($currentadminmail)) {
+	$name = $_zp_current_admin_obj->getName();
+	if ($name) {
+		$toList[$name] = $currentadminmail;
+	} else {
+		$toList[] = $currentadminmail;
+	}
+}
+
+foreach ($toList as $name => $email) {
+	$err_msg = zp_mail($subject, $message, array($name => $email), array(), array());
+	if ($err_msg) {
+		debugLogVar(gettext('user_mailing_list error'), $err_msg);
+	}
+	sleep(5); //	pase the mail send
+}
+?>
