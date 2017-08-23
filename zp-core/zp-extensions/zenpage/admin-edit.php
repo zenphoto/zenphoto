f3672bc96e2a9f1440b4421e4780efe6ec1de5df
Stephen <stephen@sbillard.org>
2017-08-22 18:21:29 -0700

    cancel button for zenpage copy/delete

--- zp-core/zp-extensions/zenpage/admin-edit.php
+++ zp-core/zp-extensions/zenpage/admin-edit.php
@@ -540,9 +540,9 @@ $tagsort = getTagOrder();
 																 id="show"
 																 value="1" <?php checkIfChecked($result->getShow()); ?>
 																 onclick="$('#pubdate').val('');
-																		 $('#expiredate').val('');
-																		 $('#pubdate').css('color', 'black');
-																		 $('.expire').html('');"
+																			 $('#expiredate').val('');
+																			 $('#pubdate').css('color', 'black');
+																			 $('.expire').html('');"
 																 />
 													<label for="show"><?php echo gettext("Published"); ?></label>
 												</p>
@@ -646,7 +646,7 @@ $tagsort = getTagOrder();
 																			 name="disclose_password"
 																			 id="disclose_password"
 																			 onclick="passwordClear('');
-																					 togglePassword('');"><?php echo gettext('Show password'); ?>
+																								 togglePassword('');"><?php echo gettext('Show password'); ?>
 															</label>
 															<br />
 															<span class="password_field_">
@@ -671,23 +671,32 @@ $tagsort = getTagOrder();
 												if (!$result->transient) {
 													?>
 													<label class="checkboxlabel">
-														<input type="radio" id="copy_object" name="copy_delete_object" value="copy"
-																	 onclick="$('#copyfield').show();
-																			 $('#deletemsg').hide();" />
-																	 <?php echo gettext("Copy"); ?>
+														<input type="radio" id="copy_object" name="copy_delete_object" value="copy" onclick="$('#copyfield').show(); $('#deletemsg').hide();" />
+														<?php echo gettext("Copy"); ?>
 													</label>
 													<label class="checkboxlabel">
-														<input type="radio" id="delete_object" name="copy_delete_object" value="delete"
-																	 onclick="deleteConfirm('delete_object', '', '<?php addslashes(printf(gettext('Are you sure you want to delete this %s?'), $deleteitem)); ?>');
-																			 $('#copyfield').hide();" />
-																	 <?php echo gettext('delete'); ?>
+														<input type="radio" id="delete_object" name="copy_delete_object" value="delete" onclick="deleteConfirm('delete_object', '', '<?php addslashes(printf(gettext('Are you sure you want to delete this %s?'), $deleteitem)); ?>'); $('#copyfield').hide();" />
+														<?php echo gettext('delete'); ?>
 													</label>
 													<br class="clearall">
 													<div class="copydelete" id="copyfield" style="display:none" >
 														<?php printf(gettext('copy as: %s'), '<input type="text" name="copy_object_as" value = "" />'); ?>
+														<p class="buttons">
+															<a	onclick="$('#copy_object').removeAttr('checked');$('#copyfield').hide();">
+																<?php echo CROSS_MARK_RED; ?>
+																<?php echo gettext("Cancel"); ?>
+															</a>
+														</p>
+
 													</div>
 													<div class="copydelete" id="deletemsg"	style="padding-top: .5em; padding-left: .5em; color: red; display: none">
 														<?php printf(gettext('%s will be deleted when changes are applied.'), $deleteitem); ?>
+														<p class="buttons">
+															<a	onclick="$('#delete_object').removeAttr('checked');$('#deletemsg').hide();">
+																<?php echo CROSS_MARK_RED; ?>
+																<?php echo gettext("Cancel"); ?>
+															</a>
+														</p>
 													</div>
 													<?php
 												}
