7955123e999bc5bbfe4e311602a14d764ac1edc0
Stephen <stephen@sbillard.org>
2017-10-07 19:25:35 -0700

    Revert "use object properties"
    This reverts commit 17eabe0cafc14132222729576b76501e562e7f2e.

--- zp-core/admin_options/search.php
+++ zp-core/admin_options/search.php
@@ -86,7 +86,7 @@ function saveOptions() {
 }
 
 function getOptionContent() {
-	global $_zp_gallery, $_zp_sortby, $_zp_CMS;
+	global $_zp_gallery, $_zp_sortby;
 	?>
 	<div id="tab_search" class="tabbox">
 		<form class="dirtylistening" onReset="toggle_passwords('', false);
@@ -497,7 +497,7 @@ function getOptionContent() {
 				if (extensionEnabled('zenpage')) {
 					unset($sort[gettext('Filemtime')]);
 					unset($sort[gettext('Filename')]);
-					if ($_zp_CMS->news_enabled) {
+					if (getOption('zenpage_enabled_items') & 1) {
 						?>
 						<tr>
 							<td class="option_name"><?php echo gettext("Sort articles by"); ?> </td>
@@ -551,7 +551,7 @@ function getOptionContent() {
 						</tr>
 						<?php
 					}
-					if ($_zp_CMS->pages_enabled) {
+					if (getOption('zenpage_enabled_items') & 2) {
 						$sort[gettext('Page order')] = 'sort_order';
 						?>
 						<tr>
