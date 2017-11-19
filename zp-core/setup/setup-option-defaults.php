96b8beae38bb34e3275ed888b41b4c1463d54b3a
acrylian <wartgart@gmx.net>
2017-11-19 17:31:46 +0100

    Adds and extends formerly unsupported Piwik plugin and extends it a bit; add iframe to default allowed tags for text fields

--- zp-core/setup/setup-option-defaults.php
+++ zp-core/setup/setup-option-defaults.php
@@ -155,7 +155,9 @@ $a = "a => (href =>() title =>() target=>() class=>() id=>())\n" .
 				"address=>(class=>() id=>() style=>())\n" .
 				"span=>(class=>() id=>() style=>())\n" .
 				"div=>(class=>() id=>() style=>())\n" .
-				"img=>(class=>() id=>() style=>() src=>() title=>() alt=>() width=>() height=>())\n"
+				"img=>(class=>() id=>() style=>() src=>() title=>() alt=>() width=>() height=>())" .
+				"iframe=>(class=>() id=>() style=>() src=>() title=>() width=>() height=>()) frameborder=>()" .
+			  "\n"
 ;
 setOption('allowed_tags_default', $a);
 setOptionDefault('allowed_tags', $a);
