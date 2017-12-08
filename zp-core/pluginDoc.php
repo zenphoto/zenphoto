9600711f7f279dfb6685eaa8a6619fff4637b14c
Stephen <stephen@sbillard.org>
2017-12-08 13:02:21 -0800

    fix button handling

--- zp-core/pluginDoc.php
+++ zp-core/pluginDoc.php
@@ -491,12 +491,13 @@ if (!defined('OFFSET_PATH')) {
 											<div class="moc_button tip" title="<?php echo @$button['title']; ?>" >
 												<?php
 												if (!empty($button_icon)) {
-													if (preg_match('~\&.*?\;~', $button_icon)) {
-														echo $button_icon . ' ';
-													} else {
+													if (strpos($button_icon, 'images/') === 0) {
+														// old style icon image
 														?>
 														<img src="<?php echo $button_icon; ?>" alt="<?php echo html_encode($button['alt']); ?>" />
 														<?php
+													} else {
+														echo $button_icon . ' ';
 													}
 												}
 												echo html_encode(@$button['button_text']);
