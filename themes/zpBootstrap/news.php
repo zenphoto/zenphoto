4b583376317d8eba0ea413bfbc8e1a7dbc1dc67a
Stephen <stephen@sbillard.org>
2017-12-28 13:35:42 -0800

    fix #698

--- themes/zpBootstrap/news.php
+++ themes/zpBootstrap/news.php
@@ -1,38 +1,54 @@
 <?php
-if (!$_zenpage_enabled) die();
+if (!$_zenpage_enabled)
+	die();
 include('inc_header.php');
 ?>
 
-	<!-- .container main -->
-		<!-- .page-header -->
-			<!-- .header -->
-				<h3><?php printZenpageItemsBreadcrumb(); ?><?php printCurrentNewsCategory(' | ' . gettext('Category') . ' : '); ?><?php printCurrentNewsArchive(' | '); ?></h3>
-			</div><!-- .header -->
-		</div><!-- /.page-header -->
+<!-- .container main -->
+<!-- .page-header -->
+<!-- .header -->
+<h3>
+	<?php
+	printNewsIndexURL(NULL, '');
+	printZenpageItemsBreadcrumb();
+	printCurrentNewsCategory(' | ' . gettext('Category') . ' : ');
+	printCurrentNewsArchive(' | ');
+	?>
+</h3>
+</div><!-- .header -->
+</div><!-- /.page-header -->
 
-	<?php if (is_NewsArticle()) {
-		// single news article
-		$news_class = 'post'; ?>
+<?php
+if (is_NewsArticle()) {
+	// single news article
+	$news_class = 'post';
+	?>
 
-		<?php if ((getPrevNewsURL()) || (getNextNewsURL())) { ?>
+	<?php if ((getPrevNewsURL()) || (getNextNewsURL())) { ?>
 		<nav class="row">
 			<ul class="pager margin-top-reset margin-bottom-reset">
 				<?php if (getPrevNewsURL()) { ?>
-				<li class="previous margin-bottom col-sm-6 pull-left">
-					<a href="<?php $article_url = getPrevNewsURL(); echo $article_url['link']; ?>" title="<?php echo $article_url['title']; ?>"> &larr; <?php echo shortenContent($article_url['title'], 30, ' (...)'); ?></a>
-				</li>
+					<li class="previous margin-bottom col-sm-6 pull-left">
+						<a href="<?php
+						$article_url = getPrevNewsURL();
+						echo $article_url['link'];
+						?>" title="<?php echo $article_url['title']; ?>"> &larr; <?php echo shortenContent($article_url['title'], 30, ' (...)'); ?></a>
+					</li>
 				<?php } ?>
 				<?php if (getNextNewsURL()) { ?>
-				<li class="next margin-bottom col-sm-6 pull-right">
-					<a href="<?php $article_url = getNextNewsURL(); echo $article_url['link']; ?>" title="<?php echo $article_url['title']; ?>"><?php echo shortenContent($article_url['title'], 30, ' (...)'); ?> &rarr; </a>
-				</li>
+					<li class="next margin-bottom col-sm-6 pull-right">
+						<a href="<?php
+						$article_url = getNextNewsURL();
+						echo $article_url['link'];
+						?>" title="<?php echo $article_url['title']; ?>"><?php echo shortenContent($article_url['title'], 30, ' (...)'); ?> &rarr; </a>
+					</li>
 				<?php } ?>
 			</ul>
 		</nav>
-		<?php } ?>
+	<?php } ?>
 
-		<div class="row">
-			<?php if (getNewsExtraContent()) { ?>
+	<div class="row">
+		<?php if (getNewsExtraContent()) { ?>
 			<div class="col-sm-9">
 				<?php include('inc_print_news.php'); ?>
 			</div>
@@ -41,41 +57,43 @@ include('inc_header.php');
 					<?php printNewsExtraContent(); ?>
 				</div>
 			</div>
-			<?php } else { ?>
+		<?php } else { ?>
 			<div class="col-sm-12">
 				<?php include('inc_print_news.php'); ?>
 			</div>
-			<?php } ?>
-		</div>
-
-		<?php if (extensionEnabled('comment_form')) { ?>
-			<?php include('inc_print_comment.php'); ?>
 		<?php } ?>
+	</div>
 
-	<?php } else {
-		// news article loop
-		$news_class = 'list-post'; ?>
+	<?php if (extensionEnabled('comment_form')) { ?>
+		<?php include('inc_print_comment.php'); ?>
+	<?php } ?>
 
-		<?php if ($_zp_CMS->getAllCategories()) { ?>
+	<?php
+} else {
+	// news article loop
+	$news_class = 'list-post';
+	?>
+
+	<?php if ($_zp_CMS->getAllCategories()) { ?>
 		<div class="row margin-bottom">
 			<div class="col-sm-offset-1 col-sm-10">
 				<?php printAllNewsCategories(gettext('All news'), true, 'news-cat-list', 'active'); ?>
 			</div>
 		</div>
-		<?php } ?>
-
-		<?php printNewsPageListWithNav('»', '«', true, 'pagination pagination-sm', true, 7); ?>
-
-		<div class="list-news">
-			<?php while (next_news()) { ?>
-				<?php include('inc_print_news.php'); ?>
-			<?php } ?>
-		</div>
-
-		<?php printNewsPageListWithNav('»', '«', true, 'pagination pagination-sm margin-top-reset', true, 7); ?>
-
 	<?php } ?>
 
-	</div><!-- /.container main -->
+	<?php printNewsPageListWithNav('»', '«', true, 'pagination pagination-sm', true, 7); ?>
+
+	<div class="list-news">
+		<?php while (next_news()) { ?>
+			<?php include('inc_print_news.php'); ?>
+		<?php } ?>
+	</div>
+
+	<?php printNewsPageListWithNav('»', '«', true, 'pagination pagination-sm margin-top-reset', true, 7); ?>
+
+<?php } ?>
+
+</div><!-- /.container main -->
 
 <?php include('inc_footer.php'); ?>
