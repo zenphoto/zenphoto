e4370c3e068415171d71ec00a49215d3a861ad64
Stephen <stephen@sbillard.org>
2018-03-20 18:56:50 -0700

    $version is no longer used

--- zp-core/setup/setup-option-defaults.php
+++ zp-core/setup/setup-option-defaults.php
@@ -787,20 +787,6 @@ setOptionDefault('locale_disallowed', serialize($disallow));
 
 foreach ($_languages as $language => $dirname) {
 	if (!empty($dirname) && $dirname != 'en_US') {
-		$version = '';
-		$po = file_get_contents(SERVERPATH . "/" . ZENFOLDER . "/locale/" . $dirname . '/LC_MESSAGES/zenphoto.po');
-		$i = strpos($po, 'Project-Id-Version:');
-		if ($i !== false) {
-			$j = strpos($po, '\n', $i);
-			if ($j !== false) {
-				$pversion = strtolower(substr($po, $i + 19, $j - $i - 19));
-				$vers = explode('.', trim(str_replace('zenphoto', '', $pversion)));
-				while (count($vers) < 3) {
-					$vers[] = 0;
-				}
-				$version = (int) $vers[0] . '.' . (int) $vers[1] . '.' . (int) $vers[2];
-			}
-		}
 		purgeOption('unsupported_' . $dirname);
 		if (!i18nSetLocale($dirname)) {
 			$unsupported[$dirname] = $dirname;
