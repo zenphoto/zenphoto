8187eefc719aa4f6d0a6a069532ef9acbd771842
Stephen <stephen@sbillard.org>
2018-03-23 09:21:24 -0700

    browser cache preventer for cacheImages

--- zp-core/zp-extensions/cacheManager/cacheImages.php
+++ zp-core/zp-extensions/cacheManager/cacheImages.php
@@ -96,6 +96,8 @@ function loadAlbum($album) {
 						$args = getImageParameters($args, $album->name);
 						$uri = getImageURI($args, $album->name, $_zp_current_image->filename, $_zp_current_image->filemtime);
 						if (strpos($uri, 'i.php?') !== false) {
+							$uri = str_replace('check=', '' . rand(), $uri);
+							$uri .= '&cache=' . rand();
 							if (!($count + $countit)) {
 								echo "{ ";
 							} else {
