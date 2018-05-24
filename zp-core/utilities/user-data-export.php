9116db4b2aad0fc0ce283e267afd646027c85e7f
fretzl <f.sondaar@kpnmail.nl>
2018-05-24 19:16:17 +0200

    Typo's

--- zp-core/utilities/user-data-export.php
+++ zp-core/utilities/user-data-export.php
@@ -16,7 +16,7 @@ $buttonlist[] = $mybutton = array(
 		'formname' => 'user-data-export.php',
 		'action' => 'utilities/user-data-export.php',
 		'icon' => 'images/bar_graph.png',
-		'title' => gettext('Lists and exports data stored about a specific user and mail address.'),
+		'title' => gettext('Lists and exports data stored about a specific user and email address.'),
 		'alt' => '',
 		'hidden' => '',
 		'rights' => ADMIN_RIGHTS
@@ -64,9 +64,9 @@ printAdminHeader('overview', 'User data export');
 				<h1><span id="top"><?php echo $mybutton['button_text']; ?></span></h1>
 				<p><?php echo gettext('This tool helps to export possible personal user data if requested. It does not delete any data. This covers the following data:'); ?></p>
 				<ul>
-					<li><?php echo gettext('<strong>User account</strong> (<em>user name</em> and <em>mail address</em>).'); ?></li>
+					<li><?php echo gettext('<strong>User account</strong> (<em>user name</em> and <em>email address</em>).'); ?></li>
 					<li><?php echo gettext('<strong>Securitylog</strong> (<em>user name</em>)'); ?></li>
-					<li><?php echo gettext('<strong>Comments</strong> (<em>user name</em> and <em>mail address</em>)'); ?></li>
+					<li><?php echo gettext('<strong>Comments</strong> (<em>user name</em> and <em>email address</em>)'); ?></li>
 					<li><?php echo gettext('<strong>Albums and images owner</strong> (<em>user name</em>) (excluding images of dynamic albums).'); ?></li>
 					<li><?php echo gettext('<strong>All items guest user</strong> (<em>user name</em>).'); ?></li>
 					<li><?php echo gettext('<strong>Zenpage pages and news articles author and last change author</strong> (<em>user name</em>)'); ?></li>
