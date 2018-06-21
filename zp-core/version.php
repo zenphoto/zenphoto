4daf14d0fa4cae83b6383bea950cfe307d3efeef
stephen@sbillard.org <stephen@sbillard.org>
2018-06-21 09:42:08 -0700

    release build 1.6.2.08.DEV_21

--- zp-core/version.php
+++ zp-core/version.php
@@ -1,4 +1,4 @@
 <?php
 // This file contains version info only and is automatically updated. DO NOT EDIT.
-define('ZENPHOTO_VERSION', '1.6.2.08.DEV_20');
+define('ZENPHOTO_VERSION', '1.6.2.08.DEV_21');
 ?>
