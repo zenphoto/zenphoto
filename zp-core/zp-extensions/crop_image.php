812fe4ecc2f6d140048b4a895db8d1c466214d48
stephen@sbillard.org <stephen@sbillard.org>
2018-06-27 15:07:20 -0700

    hacker protection

--- zp-core/zp-extensions/crop_image.php
+++ zp-core/zp-extensions/crop_image.php
@@ -69,8 +69,8 @@ class crop_image {
 
 }
 
-$albumname = sanitize_path($_REQUEST['a']);
-$imagename = sanitize($_REQUEST['i']);
+$albumname = sanitize_path(@$_REQUEST['a']);
+$imagename = sanitize(@$_REQUEST['i']);
 $album = newAlbum($albumname, true, true);
 if (!$album->exists || !$album->isMyItem(ALBUM_RIGHTS)) { // prevent nefarious access to this page.
 	if (!zp_apply_filter('admin_managed_albums_access', false, $return)) {
