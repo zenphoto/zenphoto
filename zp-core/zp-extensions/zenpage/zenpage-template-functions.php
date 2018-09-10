7567f4a01aa14c733e99962e9a77e576b8bc888e
acrylian <wartgart@gmx.net>
2018-09-10 20:10:00 +0200

    Align $before parameter handling of printPageTitle() to follow printNewsTitle()

--- zp-core/zp-extensions/zenpage/zenpage-template-functions.php
+++ zp-core/zp-extensions/zenpage/zenpage-template-functions.php
@@ -1800,7 +1800,12 @@ function getPageTitle() {
  * @return string
  */
 function printPageTitle($before = NULL) {
-	echo html_encodeTagged($before) . html_encode(getPageTitle());
+	if ($title = getPageTitle()) {
+		if ($before) {
+			echo '<span class="beforetext">' . html_encode($before) . '</span>';
+		}
+		echo html_encode($title);
+	}
 }
 
 /**
