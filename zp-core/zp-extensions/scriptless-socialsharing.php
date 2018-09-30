6e8418037e5e0e71db6760a42932452a95753bf5
fretzl <f.sondaar@kpnmail.nl>
2018-09-30 09:06:49 +0200

    Reddit icon wasn't showing.

--- zp-core/zp-extensions/scriptless-socialsharing.php
+++ zp-core/zp-extensions/scriptless-socialsharing.php
@@ -253,7 +253,7 @@ class scriptlessSocialsharing {
 		}
 		if (getOption('scriptless_socialsharing_reddit')) {
 			$buttons[] = array(
-					'class' => 'sharingicon-reddit',
+					'class' => 'sharingicon-reddit-alien',
 					'title' => 'Reddit',
 					'url' => 'https://reddit.com/submit?url=' . $url . '/?socialshare&amp;title=' . $title
 			);
