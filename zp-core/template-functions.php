6d9ad9d09ec453796157aec541f3ba0efffdadc0
acrylian <wartgart@gmx.net>
2022-03-26 16:30:45 +0100

    Move getSearchURL() as static method to searchengine class

--- zp-core/template-functions.php
+++ zp-core/template-functions.php
@@ -3725,6 +3725,9 @@ function isArchive() {
 
 /**
  * Returns a search URL
+ * 
+ * @since 1.1.3
+ * @deprecated ZenphotoCMS 2.0 - Use SearchEngine::getSearchURL() instead
  *
  * @param mixed $words the search words target
  * @param mixed $dates the dates that limit the search
@@ -3732,94 +3735,10 @@ function isArchive() {
  * @param int $page the page number for the URL
  * @param array $object_list the list of objects to search
  * @return string
- * @since 1.1.3
  */
 function getSearchURL($words, $dates, $fields, $page, $object_list = NULL) {
-	$urls = '';
-	$rewrite = false;
-	if (MOD_REWRITE) {
-		$rewrite = true;
-		if (is_array($object_list)) {
-			foreach ($object_list as $obj) {
-				if ($obj) {
-					$rewrite = false;
-					break;
-				}
-			}
-		}
-	}
-
-	if ($rewrite) {
-		if (empty($dates)) {
-			$url = SEO_WEBPATH . '/' . _SEARCH_ . '/';
-		} else {
-			$url = SEO_WEBPATH . '/' . _ARCHIVE_ . '/';
-		}
-	} else {
-		$url = SEO_WEBPATH . "/index.php?p=search";
-	}
-	if (!empty($fields) && empty($dates)) {
-		if (!is_array($fields)) {
-			$fields = explode(',', $fields);
-		}
-		$temp = $fields;
-		if ($rewrite && count($fields) == 1 && array_shift($temp) == 'tags') {
-			$url = SEO_WEBPATH . '/' . _TAGS_ . '/';
-		} else {
-			$search = new SearchEngine();
-			$urls = $search->getSearchFieldsText($fields, 'searchfields=');
-		}
-	}
-
-	if (!empty($words)) {
-		if (is_array($words)) {
-			foreach ($words as $key => $word) {
-				$words[$key] = SearchEngine::getSearchQuote($word);
-			}
-			$words = implode(',', $words);
-		}
-		$words = strtr($words, array('%' => '__25__', '&' => '__26__', '#' => '__23__', '/' => '__2F__'));
-		if ($rewrite) {
-			$url .= urlencode($words) . '/';
-		} else {
-			$url .= "&words=" . urlencode($words);
-		}
-	}
-	if (!empty($dates)) {
-		if (is_array($dates)) {
-			$dates = implode(',', $dates);
-		}
-		if ($rewrite) {
-			$url .= $dates . '/';
-		} else {
-			$url .= "&date=$dates";
-		}
-	}
-	if ($page > 1) {
-		if ($rewrite) {
-			$url .= "$page/";
-		} else {
-			if ($urls) {
-				$urls .= '&';
-			}
-			$urls .= "page=$page";
-		}
-	}
-	if (!empty($urls)) {
-		if ($rewrite) {
-			$url .= '?' . $urls;
-		} else {
-			$url .= '&' . $urls;
-		}
-	}
-	if (is_array($object_list)) {
-		foreach ($object_list as $key => $list) {
-			if (!empty($list)) {
-				$url .= '&in' . $key . '=' . html_encode(implode(',', $list));
-			}
-		}
-	}
-	return $url;
+	deprecationNotice(gettext('Use SearchEngine::getSearchURL() instead'));
+	return SearchEngine::getSearchURL($words, $dates, $fields, $page, $object_list);
 }
 
 /**
