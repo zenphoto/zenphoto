ca6bbab70e67b38276a273b9061fb0bb81fd05f6
fretzl <f.sondaar@kpnmail.nl>
2022-10-17 09:02:32 +0200

    Fix duplicate id's in cloned upload boxes in http uploader

--- zp-core/zp-extensions/uploader_http/httpupload.js
+++ zp-core/zp-extensions/uploader_http/httpupload.js
@@ -1,6 +1,6 @@
 function addUploadBoxes(placeholderid, copyfromid, num) {
 	for (i=0; i<num; i++) {
-		jQuery('#'+copyfromid).clone().insertBefore('#'+placeholderid);
+		jQuery('#'+copyfromid).clone().removeAttr('id').insertBefore('#'+placeholderid);
 		window.totalinputs++;
 		if (window.totalinputs >= 50) {
 			jQuery('#addUploadBoxes').toggle('slow');
--- zp-core/zp-extensions/uploader_http/upload_form.php
+++ zp-core/zp-extensions/uploader_http/upload_form.php
@@ -23,7 +23,7 @@ function upload_form($uploadlimit, $passedalbum) {
 		window.totalinputs = 5;
 		function addUploadBoxes(placeholderid, copyfromid, num) {
 			for (i = 0; i < num; i++) {
-				jQuery('#' + copyfromid).clone().insertBefore('#' + placeholderid);
+				jQuery('#' + copyfromid).clone().removeAttr('id').insertBefore('#' + placeholderid);
 				window.totalinputs++;
 				if (window.totalinputs >= 50) {
 					jQuery('#addUploadBoxes').toggle('slow');
