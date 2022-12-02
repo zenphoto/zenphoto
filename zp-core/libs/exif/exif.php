067841f76081ff415af9007905c33658ed5062be
acrylian <wartgart@gmx.net>
2022-12-02 12:24:48 +0100

    Fix possible string division in exifer library

--- zp-core/libs/exif/exif.php
+++ zp-core/libs/exif/exif.php
@@ -931,7 +931,7 @@ function formatData($type, $tag, $intel, $data) {
 				case '9000': // ExifVersion
 				case 'a000': // FlashPixVersion
 				case '0002': // InteroperabilityVersion
-					$data = gettext('version') . ' ' . ($data / 100);
+					$data = gettext('version') . ' ' . (intval($data) / 100);
 					break;
 				case 'a300': // FileSource
 					$data = bin2hex($data);
