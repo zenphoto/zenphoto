cb5ec5f738abb41614061d8c5a588ea42c4eb0ea
fretzl <f.sondaar@kpnmail.nl>
2023-10-18 09:17:49 +0200

    Fix deprecated parameter type

--- zp-core/zp-extensions/GoogleMap.php
+++ zp-core/zp-extensions/GoogleMap.php
@@ -275,7 +275,7 @@ function addGeoCoord($map, $coord) {
 		}
 
 		$marker['position'] = number_format($coord['lat'], 12, '.', '') . ", " . number_format($coord['long'], 12, '.', '');
-		$marker['title'] = addslashes($coord['title']);
+		$marker['title'] = addslashes(strval($coord['title']));
 		$marker['infowindow_content'] = $title . $thumb . $desc;
 		$map->add_marker($marker);
 		$lat_f = $coord['lat'] * M_PI / 180;
