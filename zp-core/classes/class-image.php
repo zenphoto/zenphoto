9edcfbc6397eb0b83c8d8b1d5d90602421952e92
acrylian <wartgart@gmx.net>
2024-07-23 15:20:22 +0200

    image class: extra method for pre-formatting the iptc data array.

--- zp-core/classes/class-image.php
+++ zp-core/classes/class-image.php
@@ -362,8 +362,7 @@ class Image extends MediaObject {
 						'Subfile'							 => '8#010' //	Subfile														Size:2
 		);
 		$this->set('hasMetadata', 0);
-		$result = array();
-		if (get_class($this) == 'Image') {
+		if ($this->isPhoto()) {
 			$localpath = $this->localpath;
 		} else {
 			$localpath = $this->getThumbImageFile();
@@ -388,8 +387,49 @@ class Image extends MediaObject {
 				}
 			}
 			/* check IPTC data */
-			$iptcdata = $_zp_graphics->imageIPTC($localpath);
-			if (!empty($iptcdata)) {
+			$iptc = $this->getIPTCData($localpath);
+			if ($iptc) {
+				$this->set('hasMetadata', 1);
+				$characterset = $this->getIPTCTag('1#090', $iptc);
+				if (!$characterset) {
+					$characterset = getOption('IPTC_encoding');
+				} else if (substr($characterset, 0, 1) == chr(27)) { // IPTC escape encoding
+					$characterset = substr($characterset, 1);
+					if ($characterset == '%G') {
+						$characterset = 'UTF-8';
+					} else { // we don't know, need to understand the IPTC standard here. In the mean time, default it.
+						$characterset = getOption('IPTC_encoding');
+					}
+				} else if ($characterset == 'UTF8') {
+					$characterset = 'UTF-8';
+				}
+			
+				// Extract IPTC fields of interest
+				foreach ($_zp_exifvars as $field => $exifvar) {
+					if ($exifvar[0] == 'IPTC') {
+						if (isset($iptc[$exifvar[1]])) { 
+							$datum = $iptc[$exifvar[1]]; //$this->getIPTCTag($IPTCtags[$exifvar[1]], $iptc);
+							$this->set($field, $this->prepIPTCString($datum, $characterset));
+						} else {
+							$this->set($field, NULL);
+						}
+					}
+				}
+				//iptc keywords extra handling for tags
+				if (isset($iptc['tags'])) {
+					$datum = $iptc['tags'];
+					if (is_array($datum)) {
+						$tags = array();
+						foreach ($datum as $item) {
+							$tags[] = $this->prepIPTCString(sanitize($item, 3), $characterset);
+						}
+						$this->setTags($tags);
+					}
+				}
+			}
+
+			//$iptcdata = $_zp_graphics->imageIPTC($localpath);
+			/* if (!empty($iptcdata)) {
 				$iptc = iptcparse($iptcdata);
 				if ($iptc) {
 					$this->set('hasMetadata', 1);
@@ -417,12 +457,11 @@ class Image extends MediaObject {
 							}
 						}
 					}
-					/* iptc keywords (tags) */
+					//iptc keywords (tags) 
 					if ($_zp_exifvars['IPTCKeywords'][5]) {
 						$datum = $this->getIPTCTagArray($IPTCtags['Keywords'], $iptc);
 						if (is_array($datum)) {
 							$tags = array();
-							$result['tags'] = array();
 							foreach ($datum as $item) {
 								$tags[] = $this->prepIPTCString(sanitize($item, 3), $characterset);
 							}
@@ -430,7 +469,7 @@ class Image extends MediaObject {
 						}
 					}
 				}
-			}
+			} */
 		}
 		/* "import" metadata into Zenphoto fields as makes sense */
 		zp_apply_filter('image_metadata', $this);
@@ -548,6 +587,102 @@ class Image extends MediaObject {
 			}
 		}
 	}
+	
+	/**
+	 * Parses IPTC meta data of an image and returns a preformatted array with the field names as indexes.
+	 * Meta data fields not enabled are already excluded
+	 * 
+	 * All Field data is converted to strings for later db storage
+	 * "Keywords" are also stored as original array as "tags" separately 
+	 * 
+	 * @since 1.6.5 Functionality separated from updateMetadata()
+	 * 
+	 * @return array
+	 */
+	function getIPTCData() {
+		global $_zp_graphics, $_zp_exifvars;
+		$fields = array();
+		if ($this->isPhoto()) {
+			$localpath = $this->localpath;
+		} else {
+			$localpath = $this->getThumbImageFile();
+		}
+		$IPTCtags = array(
+				'SKIP' => '2#000', //	Record Version										Size:64
+				'ObjectType' => '2#003', //	Object Type	Ref										Size:67
+				'ObjectAttr' => '2#004', //	Object Attribute Ref							Size:67
+				'ObjectName' => '2#005', //	Object name												Size:64
+				'EditStatus' => '2#007', //	Edit Status												Size:64
+				'EditorialUpdate' => '2#008', //	Editorial Update									Size:2
+				'Urgency' => '2#010', //	Urgency														Size:1
+				'SubRef' => '2#012', //	Subject	Reference									Size:236
+				'Category' => '2#015', //	Category 													Size:3
+				'SuppCategory' => '2#020', //	Supplemental category							Size:32
+				'FixtureID' => '2#022', //	Fixture	ID 												Size:32
+				'Keywords' => '2#025', //	Keywords 													Size:64
+				'ContentLocationCode' => '2#026', //	Content	Location Code							Size:3
+				'ContentLocationName' => '2#027', //	Content	Location Name							Size:64
+				'ReleaseDate' => '2#030', //	Release	Date 											Size:8
+				'ReleaseTime' => '2#035', //	Release	Time											Size:11
+				'ExpireDate' => '2#037', //	Expiration Date										Size:8
+				'ExpireTime' => '2#038', //	Expiration Time										Size:11
+				'SpecialInstru' => '2#040', //	Special Instructions							Size:256
+				'ActionAdvised' => '2#042', //	Action Advised										Size:2
+				'RefService' => '2#045', //	Reference Service									Size:10
+				'RefDate' => '2#047', //	Reference Date										Size:8
+				'RefNumber' => '2#050', //	Reference Number									Size:8
+				'DateCreated' => '2#055', //	Date created											Size:8
+				'TimeCreated' => '2#060', //	Time created											Size:11
+				'DigitizeDate' => '2#062', //	Digital Creation Date							Size:8
+				'DigitizeTime' => '2#063', //	Digital Creation Time							Size:11
+				'OriginatingProgram' => '2#065', //	Originating Program								Size:32
+				'ProgramVersion' => '2#070', //	Program version										Size:10
+				'ObjectCycle' => '2#075', //	Object Cycle											Size:1
+				'ByLine' => '2#080', //	ByLine 														Size:32
+				'ByLineTitle' => '2#085', //	ByLine Title											Size:32
+				'City' => '2#090', //	City															Size:32
+				'SubLocation' => '2#092', //	Sublocation												Size:32
+				'State' => '2#095', //	Province/State										Size:32
+				'LocationCode' => '2#100', //	Country/Primary	Location Code			Size:3
+				'LocationName' => '2#101', //	Country/Primary	Location Name			Size:64
+				'TransmissionRef' => '2#103', //	Original Transmission Reference		Size:32
+				'ImageHeadline' => '2#105', //	Image headline										Size:256
+				'ImageCredit' => '2#110', //	Image credit											Size:32
+				'Source' => '2#115', //	Source														Size:32
+				'Copyright' => '2#116', //	Copyright Notice									Size:128
+				'Contact' => '2#118', //	Contact														Size:128
+				'ImageCaption' => '2#120', //	Image caption											Size:2000
+				'ImageCaptionWriter' => '2#122', //	Image caption writer							Size:32
+				'ImageType' => '2#130', //	Image type												Size:2
+				'Orientation' => '2#131', //	Image	 rientation									Size:1
+				'LangID' => '2#135', //	Language ID												Size:3
+				'Subfile' => '8#010' //	Subfile														Size:2
+		);
+		$iptcdata = $_zp_graphics->imageIPTC($localpath);
+		if (!empty($iptcdata)) {
+			$iptc = iptcparse($iptcdata);
+			//echo "<pre>"; print_r($iptc); echo "</pre>";
+			if ($iptc) {
+				$fields = array();
+				// Extract IPTC fields of interest
+				foreach ($_zp_exifvars as $exifvar) {
+					if ($exifvar[0] == 'IPTC') {
+						if ($exifvar[5]) { // enabled field
+							$datum = $this->getIPTCTag($IPTCtags[$exifvar[1]], $iptc);
+							$fields[$exifvar[1]] = $datum; 
+						} else {
+							$fields[$exifvar[1]] = null;
+						}
+					}
+				}
+				if ($_zp_exifvars['IPTCKeywords'][5]) {
+					//setup keywords separately as original array for later tag assigment
+					$fields['tags'] = $this->getIPTCTagArray($IPTCtags['Keywords'], $iptc);
+				}
+			}
+		}
+		return $fields;
+	}
 
 	/**
 	 * Fetches a single tag from IPTC data
@@ -558,15 +693,16 @@ class Image extends MediaObject {
 	private function getIPTCTag($tag, $iptc) {
 		if (isset($iptc[$tag])) {
 			$iptcTag = $iptc[$tag];
-			$r = "";
-			$ct = count($iptcTag);
-			for ($i = 0; $i < $ct; $i++) {
+			//$r = "";
+			//$ct = count($iptcTag);
+			$r = implode(', ', $iptcTag);
+			/*for ($i = 0; $i < $ct; $i++) {
 				$w = $iptcTag[$i];
 				if (!empty($r)) {
 					$r .= ", ";
 				}
 				$r .= $w;
-			}
+			} */
 			return trim($r);
 		}
 		return '';
